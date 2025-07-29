b6369c68a1a9f900762869d4f43c87d80e465197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:18 +0000

    Add package pydaffodil/1.1.1/pydaffodil-1.1.1.tar.gz

---
+++ packages/pydaffodil/pydaffodil-1.1.1.tar.gz/pydaffodil-1.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="pydaffodil",
+    version="1.1.1",
+    description="A reusable deployment framework for Python.",
+    long_description=long_description,  # Use README.md for the long description
+    long_description_content_type="text/markdown",  # Ensure content is treated as markdown
+    author="Mark Wayne Menorca",
+    author_email="marcuwynu23@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "paramiko",
+        "tqdm",
+        "colorama"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
