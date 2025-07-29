20551fd13d9df0addcedb56292598abbd9f403bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:26 +0000

    Add package spm-python/25.1b1.post1/spm_python-25.1b1.post1-py3-none-any.whl

---
+++ packages/spm-python/spm_python-25.1b1.post1-py3-none-any.whl/spm/__toolbox/__DAiSS/bf_regularise_tichonov_rankdef.py
@@ -0,0 +1,14 @@
+from mpython import Runtime
+
+
+def bf_regularise_tichonov_rankdef(*args, **kwargs):
+    """
+      Tichonov regularisation for rank deficient matrices based on the function
+       __________________________________________________________________________
+
+
+    [Matlab code]( https://github.com/spm/spm/blob/main/toolbox/DAiSS/bf_regularise_tichonov_rankdef.m )
+
+    Copyright (C) 1995-2025 Functional Imaging Laboratory, Department of Imaging Neuroscience, UCL
+    """
+    return Runtime.call("bf_regularise_tichonov_rankdef", *args, **kwargs)
