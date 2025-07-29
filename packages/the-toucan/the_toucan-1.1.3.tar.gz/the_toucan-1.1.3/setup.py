8db02bbfda5ef1f3fbb412929beb6ecab83f9069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:05 +0000

    Add package the-toucan/1.1.3/the_toucan-1.1.3.tar.gz

---
+++ packages/the-toucan/the_toucan-1.1.3.tar.gz/the_toucan-1.1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="the-toucan",  
+    version="1.1.3",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "torch",
+        "torchvision",
+        "albumentations",
+        "Pillow"
+    ],
+    python_requires=">=3.9",
+)
