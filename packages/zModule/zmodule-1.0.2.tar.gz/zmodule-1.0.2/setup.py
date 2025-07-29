2c2280d73814d3b7e541a1443a29f17751dcc3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:12:09 +0000

    Add package zModule/1.0.2/zmodule-1.0.2.tar.gz

---
+++ packages/zModule/zmodule-1.0.2.tar.gz/zmodule-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+
+    name = "zModule",
+    version = "1.0.2",
+    description = "Just some Modules.",
+    author = "zisia13",
+    github = "zisia13",
+    author_email = "nothing@nothing.com",
+    packages = ["zModules", 
+                "zModules.zBanner",
+                "zModules.zOs",
+                "zModules.zCryptography"
+               ],
+    install_requires = [],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
