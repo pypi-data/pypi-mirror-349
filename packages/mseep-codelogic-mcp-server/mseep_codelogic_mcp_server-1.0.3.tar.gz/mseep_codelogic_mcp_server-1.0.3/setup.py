5e09911245f5c03273c90be8a2e384f06300ab90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:16 +0000

    Add package mseep-codelogic-mcp-server/1.0.3/mseep_codelogic_mcp_server-1.0.3.tar.gz

---
+++ packages/mseep-codelogic-mcp-server/mseep_codelogic_mcp_server-1.0.3.tar.gz/mseep_codelogic_mcp_server-1.0.3/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-codelogic-mcp-server",
+    version="1.0.2",
+    description="Integrates CodeLogic's powerful codebase knowledge graphs with a Model Context Protocol (MCP) server",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['debugpy>=1.8.12', 'httpx>=0.28.1', 'mcp[cli]>=1.3.0', 'pip-licenses>=5.0.0', 'python-dotenv>=1.0.1', 'tenacity>=9.0.0', 'toml>=0.10.2'],
+    keywords=["mseep"] + ['codelogic', 'mcp', 'code-analysis', 'knowledge-graph', 'static-analysis'],
+)
