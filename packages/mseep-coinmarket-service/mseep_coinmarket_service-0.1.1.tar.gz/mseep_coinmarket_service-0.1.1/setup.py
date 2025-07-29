97d7ac316db7be5b095a9d2bd0aee782dd2b907a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:37 +0000

    Add package mseep-coinmarket-service/0.1.1/mseep_coinmarket_service-0.1.1.tar.gz

---
+++ packages/mseep-coinmarket-service/mseep_coinmarket_service-0.1.1.tar.gz/mseep_coinmarket_service-0.1.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-coinmarket-service",
+    version="0.1.0",
+    description="Coinmarket MCP Server",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp>=1.0.0', 'python-dotenv>=1.0.1', 'requests>=2.32.3', 'ruff>=0.8.1'],
+    keywords=["mseep"] + [],
+)
