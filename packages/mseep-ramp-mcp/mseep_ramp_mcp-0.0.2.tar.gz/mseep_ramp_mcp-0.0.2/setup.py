1acb4a0b053f1a066a8d8ffd9753b80ec25a11ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:30 +0000

    Add package mseep-ramp-mcp/0.0.2/mseep_ramp_mcp-0.0.2.tar.gz

---
+++ packages/mseep-ramp-mcp/mseep_ramp_mcp-0.0.2.tar.gz/mseep_ramp_mcp-0.0.2/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-ramp-mcp",
+    version="0.0.1",
+    description="Ramp MCP Demo",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['httpx>=0.28.1', 'mcp[cli]>=1.3.0', 'uuid>=1.30.0', 'iso4217>=1.12.20240625', 'flatten-json>=0.1.14'],
+    keywords=["mseep"] + [],
+)
