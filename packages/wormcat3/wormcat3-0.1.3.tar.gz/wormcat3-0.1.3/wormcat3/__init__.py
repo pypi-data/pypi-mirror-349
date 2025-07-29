8600aa25c5b93581065e261241873e86b284c8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:33 +0000

    Add package wormcat3/0.1.3/wormcat3-0.1.3.tar.gz

---
+++ packages/wormcat3/wormcat3-0.1.3.tar.gz/wormcat3-0.1.3/wormcat3/__init__.py
@@ -0,0 +1,7 @@
+
+__version__ = "0.1.3"
+
+from .wormcat import Wormcat
+from .annotations_manger import AnnotationsManager
+from .constants import PAdjustMethod
+from .wormcat3_error import Wormcat3Error, ErrorCode
