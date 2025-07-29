792716545f3157baf50f7602c9ff74bb72e2fd2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:30 +0000

    Add package fwdi/0.1.28/fwdi-0.1.28.tar.gz

---
+++ packages/fwdi/fwdi-0.1.28.tar.gz/fwdi-0.1.28/fwdi/Application/dependency_injection.py
@@ -0,0 +1,19 @@
+from .Usecase.user_repository import UserRepositoryFWDI
+
+from ..Application.Abstractions.base_task_callback_handler import BaseTaskCallbackHandler
+from ..Application.Abstractions.base_task_queue_manager import BaseTaskQueueManager
+from ..Application.Abstractions.base_user_repository import BaseUserRepositoryFWDI
+from ..Application.Abstractions.base_service_collection import BaseServiceCollectionFWDI
+
+from ..Application.TaskHandler.task_handler import TaskHandler
+from ..Application.TaskManager.task_queue_manager import TaskQueueManager
+from ..Application.TaskCallback.task_callback_handler import TaskCallbackHandler
+
+class DependencyInjection():
+
+    @staticmethod
+    def AddApplication(services:BaseServiceCollectionFWDI)->None:
+        services.AddTransient(BaseUserRepositoryFWDI, UserRepositoryFWDI)
+        services.AddSingleton(BaseTaskQueueManager, TaskQueueManager)
+        services.AddTransient(BaseTaskCallbackHandler, TaskCallbackHandler)
+        services.AddSingleton(TaskHandler)
