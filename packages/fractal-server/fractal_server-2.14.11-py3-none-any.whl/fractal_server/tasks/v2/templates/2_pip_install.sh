5349f776d0a51c817512124b4e17a95d9fe717a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:19 +0000

    Add package fractal-server/2.14.11/fractal_server-2.14.11-py3-none-any.whl

---
+++ packages/fractal-server/fractal_server-2.14.11-py3-none-any.whl/fractal_server/tasks/v2/templates/2_pip_install.sh
@@ -0,0 +1,37 @@
+set -e
+
+write_log(){
+    TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+    echo "[collect-task, $TIMESTAMP] $1"
+}
+
+# Variables to be filled within fractal-server
+PACKAGE_ENV_DIR=__PACKAGE_ENV_DIR__
+INSTALL_STRING="__INSTALL_STRING__"
+PINNED_PACKAGE_LIST="__PINNED_PACKAGE_LIST__"
+FRACTAL_MAX_PIP_VERSION="__FRACTAL_MAX_PIP_VERSION__"
+FRACTAL_PIP_CACHE_DIR_ARG="__FRACTAL_PIP_CACHE_DIR_ARG__"
+
+TIME_START=$(date +%s)
+
+VENVPYTHON=${PACKAGE_ENV_DIR}/bin/python
+
+# Upgrade `pip` and install `setuptools`
+write_log "START upgrade pip and install setuptools"
+"$VENVPYTHON" -m pip install ${FRACTAL_PIP_CACHE_DIR_ARG} "pip<=${FRACTAL_MAX_PIP_VERSION}" --upgrade
+"$VENVPYTHON" -m pip install ${FRACTAL_PIP_CACHE_DIR_ARG} setuptools
+write_log "END   upgrade pip and install setuptools"
+echo
+
+# Install package
+write_log "START install with INSTALL_STRING=${INSTALL_STRING} and PINNED_PACKAGE_LIST=${PINNED_PACKAGE_LIST}"
+"$VENVPYTHON" -m pip install ${FRACTAL_PIP_CACHE_DIR_ARG} "$INSTALL_STRING" $PINNED_PACKAGE_LIST
+write_log "END   install with INSTALL_STRING=${INSTALL_STRING} and PINNED_PACKAGE_LIST=${PINNED_PACKAGE_LIST}"
+echo
+
+# End
+TIME_END=$(date +%s)
+write_log "All good up to here."
+write_log "Elapsed: $((TIME_END - TIME_START)) seconds"
+write_log "Exit."
+echo
