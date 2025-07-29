059fbc803fd8e9b89bab834f9e11c43ecc9fdfc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:09 +0000

    Add package pineboo/0.99.91.51/pineboo-0.99.91.51.tar.gz

---
+++ packages/pineboo/pineboo-0.99.91.51.tar.gz/pineboo-0.99.91.51/pineboolib/core/__init__.py
@@ -0,0 +1,9 @@
+"""
+Core package for utilities.
+
+This package holds all functions and classes that are like side utilities that don't require
+any dependency from other folders. So they're safe to import.
+"""
+
+DISABLE_CHECK_MEMORY_LEAKS: bool = True  # Disabled memory leaks checking.
+PROXY_ACTIONS_DICT = {}
