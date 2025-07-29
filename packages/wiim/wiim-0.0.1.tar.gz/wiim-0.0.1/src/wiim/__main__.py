8c82cd0348a4530eb2c7e9de205bec7ed207213a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package wiim/0.0.1/wiim-0.0.1.tar.gz

---
+++ packages/wiim/wiim-0.0.1.tar.gz/wiim-0.0.1/src/wiim/__main__.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from wiim.controller import LinkPlayController
+from wiim.utils import async_create_unverified_client_session
+
+
+async def main():
+    async with await async_create_unverified_client_session() as session:
+        controller = LinkPlayController(session)
+
+        await controller.discover_bridges()
+        for bridge in controller.bridges:
+            print(bridge)
+
+        await controller.discover_multirooms()
+        for multiroom in controller.multirooms:
+            print(multiroom.followers)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
