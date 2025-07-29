67095f45753a767840a55c6a47ca9aac56a1299d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:21 +0000

    Add package mkpipe-extractor-postgres/0.2.1/mkpipe_extractor_postgres-0.2.1.tar.gz

---
+++ packages/mkpipe-extractor-postgres/mkpipe_extractor_postgres-0.2.1.tar.gz/mkpipe_extractor_postgres-0.2.1/mkpipe_extractor_postgres/__init__.py
@@ -0,0 +1,15 @@
+from mkpipe.functions_spark.base_extractor import BaseExtractor
+
+
+class PostgresExtractor(BaseExtractor):
+    def __init__(self, config, settings):
+        super().__init__(
+            config,
+            settings,
+            driver_name='postgresql',
+            driver_jdbc='org.postgresql.Driver',
+        )
+        self.schema = self.connection_params['schema']
+
+    def build_jdbc_url(self):
+        return f'jdbc:{self.driver_name}://{self.host}:{self.port}/{self.database}?user={self.username}&password={self.password}&currentSchema={self.schema}'
