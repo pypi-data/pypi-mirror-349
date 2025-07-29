8254192455dac09741a5e0ce6367445763f29571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:31 +0000

    Add package rara-tools/0.6.2/rara_tools-0.6.2-py3-none-any.whl

---
+++ packages/rara-tools/rara_tools-0.6.2-py3-none-any.whl/rara_tools/constants/general.py
@@ -0,0 +1,18 @@
+class Status:
+    SKIPPED = "SKIPPED"
+    FAILED = "FAILED"
+    PENDING = "PENDING"
+    RUNNING = "RUNNING"
+    COMPLETED = "COMPLETED"
+    RETRYING = "RETRYING"
+
+
+class Queue:
+    CORE = "core"
+
+
+class Tasks:
+    SEND_VERSION = "send_version_to_core"
+    UPDATE_TASK_STATUS = "update_task_status"
+    UPDATE_TASK_VALUES = "update_task_values"
+    MODEL_UPDATE = "component_model_update"
