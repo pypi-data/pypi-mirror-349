62ae529287bee2156f9bc9aee8c8ac79c3d41326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:18 +0000

    Add package mseep-jenkins-mcp/0.1.3/mseep_jenkins_mcp-0.1.3.tar.gz

---
+++ packages/mseep-jenkins-mcp/mseep_jenkins_mcp-0.1.3.tar.gz/mseep_jenkins_mcp-0.1.3/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-jenkins-mcp",
+    version="0.1.1",
+    description="MCP server for managing Jenkins operations",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    keywords=["mseep"] + [],
+)
