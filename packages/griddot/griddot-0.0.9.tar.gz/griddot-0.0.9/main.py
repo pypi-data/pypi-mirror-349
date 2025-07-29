b0f4a46f22381237476478e62e14d2fc43ccb333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:32 +0000

    Add package griddot/0.0.9/griddot-0.0.9.tar.gz

---
+++ packages/griddot/griddot-0.0.9.tar.gz/griddot-0.0.9/main.py
@@ -0,0 +1,4 @@
+﻿from griddot.provision_keycloak import provision_keycloak
+
+if __name__ == "__main__":
+    provision_keycloak("../containers/keycloak/realms/master-realm.json", "qZEsKCyCWNwqphZmoUrRF6BtyIZ7fN")
