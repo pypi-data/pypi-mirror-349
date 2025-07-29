433ae975057d258025c7b24c38deea20160b3d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:18 +0000

    Add package mseep-elevenlabs-mcp/0.3.2/mseep_elevenlabs_mcp-0.3.2.tar.gz

---
+++ packages/mseep-elevenlabs-mcp/mseep_elevenlabs_mcp-0.3.2.tar.gz/mseep_elevenlabs_mcp-0.3.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-elevenlabs-mcp",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    maintainer="mseep",
+    maintainer_email="support@skydeck.ai",
+                    version="0.1.2",
+        packages=find_packages(),
+    include_package_data=True,
+)
