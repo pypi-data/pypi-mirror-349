76dab33531e345173b64d10dd11904e0da5743f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:54 +0000

    Add package pyams-lib/0.2.2/pyams_lib-0.2.2.tar.gz

---
+++ packages/pyams-lib/pyams_lib-0.2.2.tar.gz/pyams_lib-0.2.2/pyams/models/Digital/Source/Clk.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        Clk
+# Author:
+# Created:     05/05/2025 at 08:22:04
+# Modified:    05/05/2025
+# Copyright:   (c)PyAMS 2025
+#-------------------------------------------------------------------------------
+
+from pyams.lib import model, signal, param
+from pyams.lib import voltage, current
+
+# Clk model----------------------------------------------------------------------
+class Clk (model):
+     pass
