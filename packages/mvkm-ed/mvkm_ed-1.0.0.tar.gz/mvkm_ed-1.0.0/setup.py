c187a3bf2894b55e09d0a65a372876aa51b2caaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:42 +0000

    Add package mvkm-ed/1.0.0/mvkm_ed-1.0.0.tar.gz

---
+++ packages/mvkm-ed/mvkm_ed-1.0.0.tar.gz/mvkm_ed-1.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mvkm-ed",
+    version="1.0.0",
+    author="Kristina P. Sinaga",
+    author_email="kristinasinaga41@gmail.com",
+    description="Rectified Gaussian Kernel Multi-View K-Means Clustering Algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/Fed-MVKM",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy>=1.19.0",
+        "scipy>=1.6.0",
+        "scikit-learn>=0.24.0",
+    ],
+)
