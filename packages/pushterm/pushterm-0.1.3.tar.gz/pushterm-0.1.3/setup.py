5dcba984f50762864de9c94b4b8188b2b02f2643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:25 +0000

    Add package pushterm/0.1.3/pushterm-0.1.3.tar.gz

---
+++ packages/pushterm/pushterm-0.1.3.tar.gz/pushterm-0.1.3/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pushterm",
+    version="0.1.3",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    entry_points=
+    {
+        'console_scripts': 
+        [
+            'pushterm=pushterm.__main__:main',
+        ],
+    },
+
+)
