54fe1c1b8bd187d81c71a1acc8d91e4dbbda0386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:12 +0000

    Add package pigreads/0.4.0/pigreads-0.4.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pigreads/pigreads-0.4.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pigreads/core1.pyi
@@ -0,0 +1,15 @@
+"""
+Type annotations for single-precision implementation
+----------------------------------------------------
+"""
+
+from __future__ import annotations
+
+from pigreads.core import Models  # pylint: disable=import-self
+
+# pylint: disable=missing-class-docstring
+# pylint: disable=abstract-method
+
+class Models1(Models): ...
+
+__all__ = ["Models1"]
---
+++ packages/pigreads/pigreads-0.4.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pigreads/core2.pyi
@@ -0,0 +1,15 @@
+"""
+Type annotations for double-precision implementation
+----------------------------------------------------
+"""
+
+from __future__ import annotations
+
+from pigreads.core import Models  # pylint: disable=import-self
+
+# pylint: disable=missing-class-docstring
+# pylint: disable=abstract-method
+
+class Models2(Models): ...
+
+__all__ = ["Models2"]
