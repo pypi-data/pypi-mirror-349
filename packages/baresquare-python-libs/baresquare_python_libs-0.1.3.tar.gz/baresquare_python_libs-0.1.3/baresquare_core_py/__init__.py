8a1ef2df63932ca4afe8f0a0ce92dfe3e341960a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:18 +0000

    Add package baresquare-python-libs/0.1.3/baresquare_python_libs-0.1.3.tar.gz

---
+++ packages/baresquare-python-libs/baresquare_python_libs-0.1.3.tar.gz/baresquare_python_libs-0.1.3/baresquare_core_py/__init__.py
@@ -0,0 +1,11 @@
+"""
+Baresquare Core Python utilities.
+
+This package provides core utilities and functionality used across Baresquare Python services.
+"""
+
+# Import and expose logger module
+from . import logger
+
+# Import middleware modules and submodules
+from . import middleware
