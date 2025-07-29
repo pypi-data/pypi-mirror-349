224e8d7ff37c68bbfaef5b46e88e791061cc2052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:34 +0000

    Add package bio-showcase/2025.5.20.1/bio_showcase-2025.5.20.1-py3-none-any.whl

---
+++ packages/bio-showcase/bio_showcase-2025.5.20.1-py3-none-any.whl/bio_showcase/main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from sodatools import CD
+from pathlib import Path
+
+
+def main():
+    cr = Path(__file__).resolve().parent
+    with CD(cr):
+        try:
+            uvicorn.run("bio_showcase.asgi:application", port=8000, log_level="info")
+        except KeyboardInterrupt:
+            pass
+
+
+if __name__ == "__main__":
+    main()
