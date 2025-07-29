15518cfdc5bfe77a00b1cb2c6ec0897f71360461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:13 +0000

    Add package aish-os/0.0.0/aish_os-0.0.0.tar.gz

---
+++ packages/aish-os/aish_os-0.0.0.tar.gz/aish_os-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aish-os',
+    version='0.0.0',
+    author='Patryk Zdunowski',
+    author_email='zdunekere@gmail.com',
+    description='AI Kernel',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
