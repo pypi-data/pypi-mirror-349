61b7748f17057c0d8df929e73fe2dcb40fe5a865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:50 +0000

    Add package pyfcg/0.1b4/pyfcg-0.1b4.tar.gz

---
+++ packages/pyfcg/pyfcg-0.1b4.tar.gz/pyfcg-0.1b4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='pyfcg',
+    version='0.1b4',
+    packages=find_packages(), # automatically includes all packages through their __init__.py files
+    install_requires=[
+        "wget==3.2",          # used for downloading FCG Go
+        "requests==2.32.3",   # used for HTTP requests to FCG Go
+        "platformdirs",     # used to store FCG Go in user-specific data dir
+        "penman",
+    ],
+
+    # Sets README.md as the project description on PyPI
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
