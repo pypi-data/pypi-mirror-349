22b04094dc99d9cc14693cdad12868c650cee2fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:58 +0000

    Add package mseep-itmcp/0.1.2/mseep_itmcp-0.1.2.tar.gz

---
+++ packages/mseep-itmcp/mseep_itmcp-0.1.2.tar.gz/mseep_itmcp-0.1.2/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-itmcp",
+    version="0.1.0",
+    description="MCP server enabling AI assistants to execute terminal commands through a secure shell interface",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp>=1.0.0'],
+    keywords=["mseep"] + [],
+)
