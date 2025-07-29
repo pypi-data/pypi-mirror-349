7d689f79e89ba6366036155cf60c7748924f132b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:13 +0000

    Add package my-custom-function/0.1/my_custom_function-0.1.tar.gz

---
+++ packages/my-custom-function/my_custom_function-0.1.tar.gz/my_custom_function-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_custom_function',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+)
