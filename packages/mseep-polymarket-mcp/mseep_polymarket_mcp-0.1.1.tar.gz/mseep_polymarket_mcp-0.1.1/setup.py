92cc627245e3df35c9fbc80de850b4f47d65f526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:58 +0000

    Add package mseep-polymarket-mcp/0.1.1/mseep_polymarket_mcp-0.1.1.tar.gz

---
+++ packages/mseep-polymarket-mcp/mseep_polymarket_mcp-0.1.1.tar.gz/mseep_polymarket_mcp-0.1.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-polymarket_mcp",
+    version="0.1.0",
+    description="MCP Server for PolyMarket API",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp>=0.1.0', 'httpx>=0.24.0', 'python-dotenv>=1.0.0', 'py-clob-client'],
+    keywords=["mseep"] + [],
+)
