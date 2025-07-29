ae05d6a25463fd6d055562175fb104c09b9d876d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:27 +0000

    Add package teehr/0.4.11/teehr-0.4.11-py3-none-any.whl

---
+++ packages/teehr/teehr-0.4.11-py3-none-any.whl/teehr/evaluation/tables/secondary_timeseries_table.py
@@ -0,0 +1,124 @@
+"""Secondary timeseries table class."""
+import teehr.const as const
+from teehr.evaluation.tables.timeseries_table import TimeseriesTable
+from teehr.loading.timeseries import convert_timeseries
+import teehr.models.pandera_dataframe_schemas as schemas
+from teehr.models.table_enums import TimeseriesFields
+from pathlib import Path
+from typing import Union
+import logging
+from teehr.utils.utils import to_path_or_s3path, remove_dir_if_exists
+from teehr.models.table_enums import TableWriteEnum
+from teehr.loading.utils import add_or_replace_sdf_column_prefix
+from teehr.const import MAX_CPUS
+
+
+logger = logging.getLogger(__name__)
+
+
+class SecondaryTimeseriesTable(TimeseriesTable):
+    """Access methods to secondary timeseries table."""
+
+    def __init__(self, ev):
+        """Initialize class."""
+        super().__init__(ev)
+        self.name = "secondary_timeseries"
+        self.dir = to_path_or_s3path(ev.dataset_dir, self.name)
+        self.schema_func = schemas.secondary_timeseries_schema
+        self.unique_column_set = [
+            "location_id",
+            "value_time",
+            "reference_time",
+            "variable_name",
+            "unit_name",
+            "member",
+            "configuration_name",
+        ]
+
+    def field_enum(self) -> TimeseriesFields:
+        """Get the timeseries fields enum."""
+        fields = self._get_schema("pandas").columns.keys()
+        return TimeseriesFields(
+            "TimeseriesFields",
+            {field: field for field in fields}
+        )
+
+    def _get_schema(self, type: str = "pyspark"):
+        """Get the primary timeseries schema."""
+        if type == "pandas":
+            return self.schema_func(type="pandas")
+
+        location_ids = self.ev.location_crosswalks.distinct_values("secondary_location_id")  # noqa
+        variable_names = self.ev.variables.distinct_values("name")
+        configuration_names = self.ev.configurations.distinct_values("name")
+        unit_names = self.ev.units.distinct_values("name")
+        return self.schema_func(
+            location_ids=location_ids,
+            variable_names=variable_names,
+            configuration_names=configuration_names,
+            unit_names=unit_names
+        )
+
+    def _load(
+        self,
+        in_path: Union[Path, str],
+        pattern="**/*.parquet",
+        field_mapping: dict = None,
+        constant_field_values: dict = None,
+        location_id_prefix: str = None,
+        write_mode: TableWriteEnum = "append",
+        max_workers: Union[int, None] = MAX_CPUS,
+        persist_dataframe: bool = False,
+        drop_duplicates: bool = True,
+        **kwargs
+    ):
+        """Import timeseries helper."""
+        cache_dir = Path(
+            self.ev.dir_path,
+            const.CACHE_DIR,
+            const.LOADING_CACHE_DIR,
+            const.SECONDARY_TIMESERIES_DIR
+        )
+        # Clear the cache directory if it exists.
+        remove_dir_if_exists(cache_dir)
+
+        convert_timeseries(
+            in_path=in_path,
+            out_path=cache_dir,
+            field_mapping=field_mapping,
+            constant_field_values=constant_field_values,
+            timeseries_type="secondary",
+            pattern=pattern,
+            max_workers=max_workers,
+            **kwargs
+        )
+
+        # Read the converted files to Spark DataFrame
+        df = self._read_files(cache_dir)
+
+        if persist_dataframe:
+            df = df.repartition(*self.partition_by)
+            df = df.persist()
+
+        # Add or replace location_id prefix if provided
+        if location_id_prefix:
+            df = add_or_replace_sdf_column_prefix(
+                sdf=df,
+                column_name="location_id",
+                prefix=location_id_prefix,
+            )
+
+        # Validate using the _validate() method
+        validated_df = self._validate(df)
+
+        # Write to the table
+        self._write_spark_df(
+            validated_df,
+            write_mode=write_mode,
+            drop_duplicates=drop_duplicates,
+        )
+
+        # Reload the table
+        self._load_table()
+
+        df.unpersist()
