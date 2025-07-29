59b2a4f40638dfa3c0e4331213cba07442cc979d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:20 +0000

    Add package mseep-image-gen/0.1.2/mseep_image_gen-0.1.2.tar.gz

---
+++ packages/mseep-image-gen/mseep_image_gen-0.1.2.tar.gz/mseep_image_gen-0.1.2/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-image-gen",
+    version="0.1.1",
+    description="MCP server enabling Image Generation for LLMs, built in Python and integrated with Together AI.",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['httpx>=0.28.1', 'mcp>=1.1.2'],
+    keywords=["mseep"] + [],
+)
