b3265d84789c6376a799825a60bac5636a614475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:52 +0000

    Add package yaml-resume-builder/0.1.3/yaml_resume_builder-0.1.3.tar.gz

---
+++ packages/yaml-resume-builder/yaml_resume_builder-0.1.3.tar.gz/yaml_resume_builder-0.1.3/yaml_resume_builder/__init__.py
@@ -0,0 +1,9 @@
+"""YAML Resume Builder Package.
+
+A package to generate PDF resumes from YAML files using LaTeX templates.
+"""
+
+from yaml_resume_builder.builder import build_resume
+
+__version__ = "0.1.3"
+__all__ = ["build_resume"]
