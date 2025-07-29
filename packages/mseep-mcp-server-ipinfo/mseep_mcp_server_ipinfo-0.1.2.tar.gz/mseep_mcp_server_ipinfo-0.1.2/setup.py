16d917aa06401afc00ec170d13ff960292656b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:48 +0000

    Add package mseep-mcp-server-ipinfo/0.1.2/mseep_mcp_server_ipinfo-0.1.2.tar.gz

---
+++ packages/mseep-mcp-server-ipinfo/mseep_mcp_server_ipinfo-0.1.2.tar.gz/mseep_mcp_server_ipinfo-0.1.2/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-mcp-server-ipinfo",
+    version="0.1.0",
+    description="IP Geolocation Server for MCP",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['ipinfo>=5.1.1', 'mcp>=1.2.1', 'pydantic>=2.10.6'],
+    keywords=["mseep"] + [],
+)
