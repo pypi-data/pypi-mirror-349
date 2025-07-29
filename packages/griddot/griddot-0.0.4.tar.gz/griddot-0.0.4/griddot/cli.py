326e37aecbdeef551e10ba26e644723dad580da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:52 +0000

    Add package griddot/0.0.4/griddot-0.0.4.tar.gz

---
+++ packages/griddot/griddot-0.0.4.tar.gz/griddot-0.0.4/griddot/cli.py
@@ -0,0 +1,15 @@
+﻿import click
+from griddot.provision_keycloak import provision_kc
+
+
+@click.group()
+def cli():
+    """CLI for GridDot platform tools"""
+    pass
+
+
+@cli.command("provision-keycloak")
+@click.option('--url', help='Keycloak server URL', default='https://localhost:8443')
+def provision(url="https://localhost:8443"):
+    """Provision Keycloak with the given URL."""
+    provision_kc(url)
---
+++ packages/griddot/griddot-0.0.4.tar.gz/griddot-0.0.4/griddot/provision_keycloak/main.py
@@ -0,0 +1,31 @@
+﻿import sys
+import time
+import requests
+import urllib3
+
+# Disable warnings for self-signed certs
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def provision_kc(url):
+    print(f'Provisioning Keycloak at {url}...')
+    wait_for_keycloak_to_start(url)
+
+
+def wait_for_keycloak_to_start(url):
+    retries = 10
+    while retries > 0:
+        try:
+            response = requests.get(url, verify=False)
+            if response.status_code == 200:
+                print("Keycloak is up and running.", flush=True)
+                break
+            else:
+                print(f"Keycloak is not ready yet. Status code: {response.status_code}")
+        except requests.exceptions.RequestException:
+            print(f"Keycloak is not ready yet ...", flush=True)
+        time.sleep(5)
+        retries -= 1
+    else:
+        print("Keycloak did not start in time.", flush=True)
+        sys.exit(1)
