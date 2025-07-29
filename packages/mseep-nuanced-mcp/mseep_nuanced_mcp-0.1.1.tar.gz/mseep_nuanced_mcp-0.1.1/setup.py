00603908de985f906cff14ab9dfe7e9494167006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:11 +0000

    Add package mseep-nuanced-mcp/0.1.1/mseep_nuanced_mcp-0.1.1.tar.gz

---
+++ packages/mseep-nuanced-mcp/mseep_nuanced_mcp-0.1.1.tar.gz/mseep_nuanced_mcp-0.1.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-nuanced-mcp",
+    version="0.1.0",
+    description="MCP server expose nuanced.dev functionality",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp[cli]>=1.3.0', 'nuanced>=0.1.4'],
+    keywords=["mseep"] + [],
+)
