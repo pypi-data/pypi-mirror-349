00b76849c92ef6f5e68184288225c6052f24e8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:24 +0000

    Add package mseep-meilisearch-mcp/0.4.2/mseep_meilisearch_mcp-0.4.2.tar.gz

---
+++ packages/mseep-meilisearch-mcp/mseep_meilisearch_mcp-0.4.2.tar.gz/mseep_meilisearch_mcp-0.4.2/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-meilisearch-mcp",
+    version="0.4.0",
+    description="MCP server for Meilisearch",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['meilisearch>=0.33.0', 'mcp>=0.1.0', 'httpx>=0.24.0', 'pydantic>=2.0.0'],
+    keywords=["mseep"] + [],
+)
