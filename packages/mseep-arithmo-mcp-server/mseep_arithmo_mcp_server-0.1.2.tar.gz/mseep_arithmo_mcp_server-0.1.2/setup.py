bf182416b0badf052eabac9ec05468987ea970bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:46 +0000

    Add package mseep-arithmo-mcp-server/0.1.2/mseep_arithmo_mcp_server-0.1.2.tar.gz

---
+++ packages/mseep-arithmo-mcp-server/mseep_arithmo_mcp_server-0.1.2.tar.gz/mseep_arithmo_mcp_server-0.1.2/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-arithmo_mcp_server",
+    version="0.1.0",
+    description="An MCP server providing fast arithmetic operations.",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp'],
+    keywords=["mseep"] + [],
+)
