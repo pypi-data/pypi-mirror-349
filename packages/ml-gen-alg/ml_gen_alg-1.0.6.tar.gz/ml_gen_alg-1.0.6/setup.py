d9bbcdfe1dc027535a867fb31529534240956ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:00 +0000

    Add package ml-gen-alg/1.0.6/ml_gen_alg-1.0.6.tar.gz

---
+++ packages/ml-gen-alg/ml_gen_alg-1.0.6.tar.gz/ml_gen_alg-1.0.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="ml-gen-alg",
+    version="1.0.6",
+    author="Nicolas Torres & Yamid Quiroga",
+    author_email="nicolasrodriguezt@ucundinamarca.edu.co, yfquiroga@ucundinamarca.edu.co",
+    description="Una librería de algoritmos genéticos en Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/YFQG/ml-gen-alg",  # Reemplaza con tu URL real
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "setuptools",
+        "wheel",
+        "twine"],  # Agrega dependencias si es necesario
+)
+
