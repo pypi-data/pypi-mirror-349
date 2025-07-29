8b185a537d11ddc6722e9f1568140ebb25f2d210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:25 +0000

    Add package tl-code/0.2/tl_code-0.2.tar.gz

---
+++ packages/tl-code/tl_code-0.2.tar.gz/tl_code-0.2/tl_code/__init__.py
@@ -0,0 +1,9 @@
+from .exp import experiment
+
+def tl_code(n):
+    code = experiment.get(n)
+    if code:
+        print(code)
+    else:
+        print("Sorry Dude wrong exp number. Choose 1-10.")
+
