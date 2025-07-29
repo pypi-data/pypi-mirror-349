1e77102a6b0c4b809c030db926c98ed103c773af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:14 +0000

    Add package trajectopy/3.0.5/trajectopy-3.0.5.tar.gz

---
+++ packages/trajectopy/trajectopy-3.0.5.tar.gz/trajectopy-3.0.5/trajectopy/__init__.py
@@ -0,0 +1,25 @@
+import logging
+
+from rich.logging import RichHandler
+
+logging.basicConfig(
+    format="%(message)s",
+    level=logging.INFO,
+    handlers=[RichHandler(omit_repeated_times=False, log_time_format="%Y-%m-%d %H:%M:%S")],
+)
+
+
+__version__ = "3.0.5"
+
+
+from trajectopy.alignment import *
+from trajectopy.evaluation import *
+from trajectopy.matching import *
+from trajectopy.merging import *
+from trajectopy.plotting import *
+from trajectopy.pointset import PointSet
+from trajectopy.report import *
+from trajectopy.rotationset import RotationSet
+from trajectopy.settings import *
+from trajectopy.sorting import *
+from trajectopy.trajectory import Trajectory
