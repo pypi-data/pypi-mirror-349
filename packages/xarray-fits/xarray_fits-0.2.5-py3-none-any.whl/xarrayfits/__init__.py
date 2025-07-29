f85de12d86aadb78d04d417fbf3714fae081d907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:00 +0000

    Add package xarray-fits/0.2.5/xarray_fits-0.2.5-py3-none-any.whl

---
+++ packages/xarray-fits/xarray_fits-0.2.5-py3-none-any.whl/xarrayfits/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for xarray-fits."""
+
+__author__ = """Simon Perkins"""
+__email__ = "simon.perkins@gmail.com"
+__version__ = "0.2.5"
+
+from xarrayfits.fits import xds_from_fits  # noqa
---
+++ packages/xarray-fits/xarray_fits-0.2.5-py3-none-any.whl/xarrayfits/fits_proxy.py
@@ -0,0 +1,69 @@
+from threading import Lock
+import weakref
+
+from astropy.io import fits  # type: ignore
+
+TABLE_CACHE_LOCK = Lock()
+TABLE_CACHE: weakref.WeakValueDictionary = weakref.WeakValueDictionary()
+
+
+class FitsProxyMetaClass(type):
+    """https://en.wikipedia.org/wiki/Multiton_pattern"""
+
+    def __call__(cls, *args, **kwargs):
+        key = (cls,) + args + tuple(set(kwargs.items()))
+
+        with TABLE_CACHE_LOCK:
+            try:
+                return TABLE_CACHE[key]
+            except KeyError:
+                instance = type.__call__(cls, *args, **kwargs)
+                TABLE_CACHE[key] = instance
+                return instance
+
+
+class FitsProxy(metaclass=FitsProxyMetaClass):
+    """
+    Picklable object proxying a :class:`astropy.io.fits` class
+    """
+
+    def __init__(self, filename, **kwargs):
+        """
+        Parameters
+        ----------
+        filename : str
+            fits filename
+        **kwargs (optional) : dict
+            Extra keywords arguments passed in to the
+            :class:`astropy.iofits` constructor.
+        """
+        self._filename = filename
+        self._kwargs = kwargs
+        self._lock = Lock()
+
+    @staticmethod
+    def from_reduce_args(filename, kw):
+        return FitsProxy(filename, **kw)
+
+    @property
+    def is_memory_mapped(self):
+        return self._kwargs.get("memmap") in {None, True}
+
+    @property
+    def hdu_list(self):
+        try:
+            return self._hdul
+        except AttributeError:
+            with self._lock:
+                try:
+                    return self._hdul
+                except AttributeError:
+                    self._hdul = hdul = fits.open(self._filename, **self._kwargs)
+                    weakref.finalize(self, hdul.close)
+                    return hdul
+
+    def __hash__(self):
+        return hash((self._filename, tuple(set(self._kwargs.items()))))
+
+    def __reduce__(self):
+        return (FitsProxy.from_reduce_args, (self._filename, self._kwargs))
