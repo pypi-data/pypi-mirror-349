f19e67d519b2d3cd0e55855b1dc39d716e3c7f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:11 +0000

    Add package speedy-utils/1.0.11/speedy_utils-1.0.11.tar.gz

---
+++ packages/speedy-utils/speedy_utils-1.0.11.tar.gz/speedy_utils-1.0.11/src/llm_utils/__init__.py
@@ -0,0 +1,31 @@
+from .chat_format import (
+    transform_messages,
+    transform_messages_to_chatml,
+    show_chat,
+    get_conversation_one_turn,
+    show_string_diff,
+    display_conversations,
+    build_chatml_input,
+    format_msgs,
+    display_chat_messages_as_html,
+)
+from .lm import LM
+from .group_messages import (
+    split_indices_by_length,
+    group_messages_by_len,
+)
+
+__all__ = [
+    "transform_messages",
+    "transform_messages_to_chatml",
+    "show_chat",
+    "get_conversation_one_turn",
+    "show_string_diff",
+    "display_conversations",
+    "build_chatml_input",
+    "format_msgs",
+    "split_indices_by_length",
+    "group_messages_by_len",
+    "LM",
+    "display_chat_messages_as_html",
+]
