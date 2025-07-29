c35b3b333fda37b5362635097b8b4f4fb11af589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:18 +0000

    Add package prompted/0.0.4/prompted-0.0.4.tar.gz

---
+++ packages/prompted/prompted-0.0.4.tar.gz/prompted-0.0.4/prompted/__init__.py
@@ -0,0 +1,19 @@
+"""
+## 💭 prompted
+
+[`hammad saeed`](https://github.com/hsaeed3) | 2025
+"""
+
+from __future__ import annotations
+
+from ._main import (
+    create,
+    types,
+    utils
+)
+
+__all__ = [
+    "create",
+    "types",
+    "utils"
+]
