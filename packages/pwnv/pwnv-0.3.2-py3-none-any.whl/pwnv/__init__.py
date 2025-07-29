69b075a70b9c1b8245deccb45947cc021756d006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:32 +0000

    Add package pwnv/0.3.2/pwnv-0.3.2-py3-none-any.whl

---
+++ packages/pwnv/pwnv-0.3.2-py3-none-any.whl/pwnv/__init__.py
@@ -0,0 +1,13 @@
+import typer
+
+from pwnv.cli import challenge_app, ctf_app, init_app, reset_app, solve_app
+
+
+def main():
+    app = typer.Typer(no_args_is_help=True)
+    app.add_typer(challenge_app, name="challenge")
+    app.add_typer(ctf_app, name="ctf")
+    app.add_typer(init_app)
+    app.add_typer(reset_app)
+    app.add_typer(solve_app)
+    app()
