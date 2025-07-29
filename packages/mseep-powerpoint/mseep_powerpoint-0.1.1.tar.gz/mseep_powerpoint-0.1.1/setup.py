10ff40412f95ce3e1a3185011ed3acf8a9e101fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package mseep-powerpoint/0.1.1/mseep_powerpoint-0.1.1.tar.gz

---
+++ packages/mseep-powerpoint/mseep_powerpoint-0.1.1.tar.gz/mseep_powerpoint-0.1.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-powerpoint",
+    version="0.1.0",
+    description="A MCP server project that creates powerpoint presentations",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp>=1.3.0', 'pillow>=11.1.0', 'python-pptx>=1.0.2', 'requests>=2.32.3', 'together>=1.4.1'],
+    keywords=["mseep"] + [],
+)
