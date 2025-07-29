7ea8cdc4890c027403a8308059d8373563f11748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:54 +0000

    Add package baresquare-core-py/0.1.4/baresquare_core_py-0.1.4.tar.gz

---
+++ packages/baresquare-core-py/baresquare_core_py-0.1.4.tar.gz/baresquare_core_py-0.1.4/baresquare_core_py/middleware/__init__.py
@@ -0,0 +1,6 @@
+"""Middleware components for Baresquare applications.
+
+This package provides reusable middleware components for web applications.
+"""
+
+# Import and expose all middleware modules
