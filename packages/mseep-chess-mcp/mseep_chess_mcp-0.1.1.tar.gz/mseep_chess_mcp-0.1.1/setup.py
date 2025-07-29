2638ec10b01ecc40370489f3af46c18f78b78cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:04 +0000

    Add package mseep-chess-mcp/0.1.1/mseep_chess_mcp-0.1.1.tar.gz

---
+++ packages/mseep-chess-mcp/mseep_chess_mcp-0.1.1.tar.gz/mseep_chess_mcp-0.1.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-chess_mcp",
+    version="0.1.0",
+    description="MCP server for Chess.com API integration",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp[cli]', 'httpx'],
+    keywords=["mseep"] + [],
+)
