f80bcd93d5c5f58209de1bb5251b684a6d7d4fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:24 +0000

    Add package rohith/0.1.1/rohith-0.1.1.tar.gz

---
+++ packages/rohith/rohith-0.1.1.tar.gz/rohith-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rohith",
+    version="0.1.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "questionary",
+        "rich",
+        "requests"
+    ],
+    entry_points={
+        "console_scripts": [
+            "rohith=rohith.cli:main"
+        ]
+    },
+    author="Rohit Gowda R",
+    description="CLI card for Rohit Gowda with contact, resume and meeting options.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rohithgowdax/cli-card",  # Optional
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
