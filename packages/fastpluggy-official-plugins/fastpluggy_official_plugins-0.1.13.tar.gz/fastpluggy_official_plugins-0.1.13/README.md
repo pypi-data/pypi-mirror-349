12354d7cac9dc80c1d4bd8a62c930c7d75900e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:11 +0000

    Add package fastpluggy-official-plugins/0.1.13/fastpluggy_official_plugins-0.1.13.tar.gz

---
+++ packages/fastpluggy-official-plugins/fastpluggy_official_plugins-0.1.13.tar.gz/fastpluggy_official_plugins-0.1.13/README.md
@@ -0,0 +1,10 @@
+# FastPluggy Official Plugins
+
+This bundle includes pre-approved plugins.
+
+# Included plugins 
+
+- tasks_worker
+- ui_tools
+- crud_tools
+- redis_tools
---
+++ packages/fastpluggy-official-plugins/fastpluggy_official_plugins-0.1.13.tar.gz/fastpluggy_official_plugins-0.1.13/fastpluggy_official_plugins/bootstrap.py
@@ -0,0 +1,33 @@
+import importlib.resources
+import logging
+import os.path
+import shutil
+from pathlib import Path
+
+from fastpluggy.core.tools.system import trigger_reload
+
+def init_plugins_if_needed(plugins_dir: str, enabled_plugins: list):
+    plugins_dir = Path(plugins_dir)
+
+    from fastpluggy_official_plugins import plugins as official_plugins
+    plugins_embedded = importlib.resources.files(official_plugins)
+    logging.debug(f"Plugins embedded: {plugins_embedded}")
+
+    for plugin_name in enabled_plugins:
+        src = plugins_embedded / plugin_name
+        dest = Path(os.path.join(plugins_dir, plugin_name))
+
+        logging.debug(f"Copying {src} -> {dest}")
+        logging.debug(f"src: {src.exists()}, dest: {dest.exists()}")
+
+        if not dest.exists() and src.is_dir():
+            shutil.copytree(src, dest)
+            logging.debug(f"Copied {src} -> {dest}")
+
+        # Try installing requirements
+        from fastpluggy.core.tools.install import install_requirements
+        req_file = dest / "requirements.txt"
+        logging.debug(f"Installing requirements from {req_file}")
+        install_requirements(str(req_file))
+
+    trigger_reload(plugins_dir, create_file=True)
