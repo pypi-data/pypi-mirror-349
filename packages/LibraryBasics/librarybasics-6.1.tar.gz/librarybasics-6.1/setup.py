d0f7a7f7d204daabf678857a64de5d19a1701093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:13 +0000

    Add package LibraryBasics/6.1/librarybasics-6.1.tar.gz

---
+++ packages/LibraryBasics/librarybasics-6.1.tar.gz/librarybasics-6.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="LibraryBasics",
+      version="6.1",
+      packages=['printslow','passcrack','hangmansolver'],#called uGauss because it's a super small AND scalable version to ANY variable amount (e.g: 3 term elim, 4 term, 5 term, 150003 terms...)
+      author="YusufA442",
+      author_email="yusuf365820@gmail.com",
+      license='Creative Commons Attribution-Noncommercial-Share Alike license',
+      long_description=open('readme.txt').read())
