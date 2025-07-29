27246c335dce9be0d99fac11a72d3e7e77f82c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:58 +0000

    Add package invenio-override/0.0.6/invenio_override-0.0.6-py2.py3-none-any.whl

---
+++ packages/invenio-override/invenio_override-0.0.6-py2.py3-none-any.whl/invenio_override/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2023 Graz University of Technology.
+# Copyright (C) 2024 Shared RDM.
+#
+# invenio-override is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""invenio module for sharedRDM theme."""
+
+from .ext import InvenioOverride
+
+__version__ = "0.0.6"
+
+__all__ = ("__version__", "InvenioOverride")
