e78fd6e13079adc131ae4dc58b94b11cb7525e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:30 +0000

    Add package mseep-beeper-mcp/0.1.2/mseep_beeper_mcp-0.1.2.tar.gz

---
+++ packages/mseep-beeper-mcp/mseep_beeper_mcp-0.1.2.tar.gz/mseep_beeper_mcp-0.1.2/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-beeper-mcp",
+    version="0.1.0",
+    description="a mcp server for interacting with chain",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['dotenv>=0.9.9', 'eth-account>=0.13.5', 'mcp[cli]>=1.4.1', 'pytest>=8.3.5', 'python-dotenv>=1.0.1', 'web3>=7.9.0', 'pyyaml>=6.0.1'],
+    keywords=["mseep"] + [],
+)
