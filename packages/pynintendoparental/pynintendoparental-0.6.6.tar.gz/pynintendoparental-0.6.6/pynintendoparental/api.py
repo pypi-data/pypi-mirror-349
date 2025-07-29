c586828877098f527f6b857056ecffa4e78ff97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:11 +0000

    Add package pynintendoparental/0.6.6/pynintendoparental-0.6.6.tar.gz

---
+++ packages/pynintendoparental/pynintendoparental-0.6.6.tar.gz/pynintendoparental-0.6.6/pynintendoparental/api.py
@@ -0,0 +1,96 @@
+"""API handler."""
+
+from datetime import datetime, timedelta
+
+import aiohttp
+
+from .authenticator import Authenticator
+from .const import (
+    ENDPOINTS,
+    BASE_URL,
+    USER_AGENT,
+    MOBILE_APP_PKG,
+    MOBILE_APP_BUILD,
+    MOBILE_APP_VERSION,
+    OS_VERSION,
+    OS_NAME,
+    DEVICE_MODEL,
+    _LOGGER
+)
+from .exceptions import HttpException
+
+def _check_http_success(status: int) -> bool:
+    return status >= 200 and status < 300
+
+class Api:
+    """Nintendo Parental Controls API."""
+
+    def __init__(self, auth, tz, lang):
+        """INIT"""
+        self._auth: Authenticator = auth
+        self._tz = tz
+        self._language = lang
+
+    @property
+    def _auth_token(self) -> str:
+        """Returns the auth token."""
+        return f"Bearer {self._auth.access_token}"
+
+    @property
+    def account_id(self):
+        """Return the account id."""
+        return self._auth.account_id
+
+    async def send_request(self, endpoint: str, body: object=None, **kwargs):
+        """Sends a request to a given endpoint."""
+        _LOGGER.debug("Sending request to %s", endpoint)
+        # Get the endpoint from the endpoints map
+        e_point = ENDPOINTS.get(endpoint, None)
+        if e_point is None:
+            raise ValueError("Endpoint does not exist")
+        # refresh the token if it has expired.
+        if self._auth.expires < (datetime.now()+timedelta(seconds=30)):
+            _LOGGER.debug("Access token expired, requesting refresh.")
+            await self._auth.perform_refresh()
+        # format the URL using the kwargs
+        url = e_point.get("url").format(BASE_URL=BASE_URL, **kwargs)
+        _LOGGER.debug("Built URL %s", url)
+        # now send the HTTP request
+        resp: dict = {
+            "status": 0,
+            "text": "",
+            "json": "",
+            "headers": ""
+        }
+        async with aiohttp.ClientSession() as session:
+            # Add auth header
+            session.headers.add("Authorization", self._auth_token)
+            session.headers.add("User-Agent", USER_AGENT)
+            session.headers.add("X-Moon-App-Id", MOBILE_APP_PKG)
+            session.headers.add("X-Moon-Os", OS_NAME)
+            session.headers.add("X-Moon-Os-Version", OS_VERSION)
+            session.headers.add("X-Moon-Model", DEVICE_MODEL)
+            session.headers.add("X-Moon-TimeZone", self._tz)
+            session.headers.add("X-Moon-Os-Language", self._language)
+            session.headers.add("X-Moon-App-Language", self._language)
+            session.headers.add("X-Moon-App-Display-Version", MOBILE_APP_VERSION)
+            session.headers.add("X-Moon-App-Internal-Version", MOBILE_APP_BUILD)
+            async with session.request(
+                method=e_point.get("method"),
+                url=url,
+                json=body
+            ) as response:
+                _LOGGER.debug("%s request to %s status code %s",
+                              e_point.get("method"),
+                              url,
+                              response.status)
+                if _check_http_success(response.status):
+                    resp["status"] = response.status
+                    resp["text"] = await response.text()
+                    resp["json"] = await response.json()
+                    resp["headers"] = response.headers
+                else:
+                    raise HttpException("HTTP Error", response.status, await response.text())
+
+        # now return the resp dict
+        return resp
---
+++ packages/pynintendoparental/pynintendoparental-0.6.6.tar.gz/pynintendoparental-0.6.6/pynintendoparental/const.py
@@ -0,0 +1,68 @@
+# pylint: disable=line-too-long
+"""pynintendoparental"""
+
+import logging
+
+_LOGGER = logging.getLogger(__package__)
+MOBILE_APP_PKG = "com.nintendo.znma"
+MOBILE_APP_VERSION = "1.23.0"
+MOBILE_APP_BUILD = "293"
+OS_NAME = "ANDROID"
+OS_VERSION = "33"
+OS_STR = f"{OS_NAME} {OS_VERSION}"
+DEVICE_MODEL = "Pixel 4 XL"
+BASE_URL = "https://api-lp1.pctl.srv.nintendo.net/moon/v1"
+USER_AGENT = f"moon_ANDROID/{MOBILE_APP_VERSION} ({MOBILE_APP_PKG}; build:{MOBILE_APP_BUILD}; {OS_STR})"
+
+DAYS_OF_WEEK = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+ENDPOINTS = {
+    "get_account_details": {
+        "url": "{BASE_URL}/users/{ACCOUNT_ID}",
+        "method": "GET"
+    },
+    "get_account_devices": {
+        "url": "{BASE_URL}/users/{ACCOUNT_ID}/devices?filter.device.activated.$eq=true",
+        "method": "GET"
+    },
+    "get_account_device": {
+        "url": "{BASE_URL}/users/{ACCOUNT_ID}/devices/{DEVICE_ID}",
+        "method": "GET"
+    },
+    "get_device_daily_summaries": {
+        "url": "{BASE_URL}/devices/{DEVICE_ID}/daily_summaries",
+        "method": "GET"
+    },
+    "get_device_monthly_summaries": {
+        "url": "{BASE_URL}/devices/{DEVICE_ID}/monthly_summaries",
+        "method": "GET"
+    },
+    "get_device_parental_control_setting": {
+        "url": "{BASE_URL}/devices/{DEVICE_ID}/parental_control_setting",
+        "method": "GET"
+    },
+    "update_device_parental_control_setting": {
+        "url": "{BASE_URL}/devices/{DEVICE_ID}/parental_control_setting",
+        "method": "POST"
+    },
+    "update_device_whitelisted_applications": {
+        "url": "{BASE_URL}/devices/{DEVICE_ID}/parental_control_setting/whitelisted_applications",
+        "method": "POST"
+    },
+    "get_device_parental_control_setting_state": {
+        "url": "{BASE_URL}/devices/{DEVICE_ID}/parental_control_setting_state",
+        "method": "GET"
+    },
+    "update_device_alarm_setting_state": {
+        "url": "{BASE_URL}/devices/{DEVICE_ID}/alarm_setting_state",
+        "method": "POST"
+    },
+    "get_device_alarm_setting_state": {
+        "url": "{BASE_URL}/devices/{DEVICE_ID}/alarm_setting_state",
+        "method": "GET"
+    },
+    "get_device_monthly_summary": {
+        "url": "{BASE_URL}/devices/{DEVICE_ID}/monthly_summaries/{YEAR}-{MONTH}",
+        "method": "GET"
+    }
+}
