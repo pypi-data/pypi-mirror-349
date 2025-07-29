defeab5213e7bed5e3859888ef07d6d47076a822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:56 +0000

    Add package mseep-moodle-mcp/0.1.1/mseep_moodle_mcp-0.1.1.tar.gz

---
+++ packages/mseep-moodle-mcp/mseep_moodle_mcp-0.1.1.tar.gz/mseep_moodle_mcp-0.1.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-moodle-mcp",
+    version="0.1.0",
+    description="A Model Context Protocol (MCP) server implementation that provides capabilities to interact with Moodle LMS.",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['glom>=24.11.0', 'mcp[cli]>=1.6.0', 'python-dotenv>=1.1.0', 'requests>=2.32.3'],
+    keywords=["mseep"] + [],
+)
