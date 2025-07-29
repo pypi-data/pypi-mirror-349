833b0502b629fd3b43358990231d500e8c034bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:15 +0000

    Add package mseep-beanquery-mcp/0.1.1/mseep_beanquery_mcp-0.1.1.tar.gz

---
+++ packages/mseep-beanquery-mcp/mseep_beanquery_mcp-0.1.1.tar.gz/mseep_beanquery_mcp-0.1.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-beanquery-mcp",
+    version="0.1.0",
+    description="The Beancount MCP Server is an experimental implementation that utilizes the Model Context Protocol (MCP) to enable AI assistants to query and analyze Beancount ledger files using Beancount Query Language (BQL) and the beanquery tool.",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['beancount>=3.1.0', 'beanquery>=0.2.0', 'mcp[cli]>=1.5.0'],
+    keywords=["mseep"] + [],
+)
