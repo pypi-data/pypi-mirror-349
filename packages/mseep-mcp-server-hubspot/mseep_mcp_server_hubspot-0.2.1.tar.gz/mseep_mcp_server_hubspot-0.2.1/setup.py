afa35395f68776e82c7feca1244e16d1dfffef15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:39 +0000

    Add package mseep-mcp-server-hubspot/0.2.1/mseep_mcp_server_hubspot-0.2.1.tar.gz

---
+++ packages/mseep-mcp-server-hubspot/mseep_mcp_server_hubspot-0.2.1.tar.gz/mseep_mcp_server_hubspot-0.2.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-mcp-server-hubspot",
+    version="0.2.0",
+    description="A simple Hubspot MCP server",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp>=1.4.1', 'hubspot-api-client>=11.1.0', 'python-dotenv>=1.0.1', 'faiss-cpu>=1.7.4', 'numpy>=1.24.0', 'sentence-transformers>=2.2.2', 'huggingface-hub==0.14.1'],
+    keywords=["mseep"] + [],
+)
