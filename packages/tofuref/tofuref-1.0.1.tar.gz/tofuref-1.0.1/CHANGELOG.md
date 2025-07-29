7b590f6851cd9ac130e4213a94ff84f5346189b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:28 +0000

    Add package tofuref/1.0.1/tofuref-1.0.1.tar.gz

---
+++ packages/tofuref/tofuref-1.0.1.tar.gz/tofuref-1.0.1/CHANGELOG.md
@@ -0,0 +1,52 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [1.0.1] - 2025-05-20
+
+### Fixed
+
+- Use provider configuration had `v` prefix in the version number 
+- Use provider configuration was missing a quote at the beginning of source
+- Use provider configuration has a proper format (aligned `=`)
+
+## [1.0.0] - 2025-05-19
+
+### Added 
+
+- Use command (`u` or `y`) to copy provider configuration for `required_providers` block
+- End-to-end tests have been added
+
+## [0.4.0] - 2025-05-02
+
+### Added
+
+- Providers now have a version picker, when one is selected, press `v`
+- Loading indicators
+- Fullscreen mode
+- Content now reacts to page up, page down, home and end
+
+## [0.3.0] - 2025-05-01
+
+### Added
+
+- Content is now scrollable with arrows
+- Added keybindings for focusing Providers/Resources/Content respectively
+- Search now has alternative keybind `/`
+- Added version to the welcome screen headline
+
+### Changed
+
+- Search now works per window, separately for providers and resources
+- Log is now toggle-able, off by default
+
+## [0.2.0] - 2025-05-01
+
+### Added
+
+- Whatever was before :)
---
+++ packages/tofuref/tofuref-1.0.1.tar.gz/tofuref-1.0.1/tofuref/data/providers.py
@@ -0,0 +1,142 @@
+from dataclasses import dataclass, field
+from typing import Dict, Optional, List, Any
+from rich.json import JSON as RICH_JSON
+import json
+import logging
+
+from tofuref.data.resources import Resource, ResourceType
+from tofuref.data.helpers import (
+    get_registry_api,
+    header_markdown_split,
+)
+
+LOGGER = logging.getLogger(__name__)
+
+
+@dataclass
+class Provider:
+    organization: str
+    name: str
+    description: str
+    fork_count: int
+    blocked: bool
+    popularity: int
+    _overview: Optional[str] = None
+    _active_version: Optional[str] = None
+    versions: List[Dict[str, str]] = field(default_factory=list)
+    fork_of: Optional[str] = None
+    raw_json: Optional[dict] = None
+    resources: List[Resource] = field(default_factory=list)
+    datasources: List[Resource] = field(default_factory=list)
+    functions: List[Resource] = field(default_factory=list)
+    guides: List[Resource] = field(default_factory=list)
+    log_widget: Optional[Any] = None
+
+    @classmethod
+    def from_json(cls, data: dict) -> "Provider":
+        return cls(
+            organization=data["addr"]["namespace"],
+            name=data["addr"]["name"],
+            description=data["description"],
+            fork_count=data["fork_count"],
+            blocked=data["is_blocked"],
+            popularity=data["popularity"],
+            versions=data["versions"],
+            fork_of=data.get("fork_of", {}).get("display"),
+            raw_json=data,
+        )
+
+    @property
+    def display_name(self) -> str:
+        return f"{self.organization}/{self.name}"
+
+    @property
+    def active_version(self) -> str:
+        if self._active_version is None:
+            self._active_version = self.versions[0]["id"]
+        return self._active_version
+
+    @active_version.setter
+    def active_version(self, value: str) -> None:
+        self._active_version = value
+        self.resources = []
+        self._overview = None
+
+    @property
+    def _endpoint(self) -> str:
+        return f"{self.organization}/{self.name}/{self.active_version}"
+
+    @property
+    def use_configuration(self) -> str:
+        return f"""    {self.name} = {{
+      source  = "{self.organization}/{self.name}"
+      version = "{self.active_version.lstrip('v')}"
+    }}"""
+
+    async def overview(self) -> str:
+        if self._overview is None:
+            self._overview = await get_registry_api(
+                f"{self._endpoint}/index.md", json=False, log_widget=self.log_widget
+            )
+            _, self._overview = header_markdown_split(self._overview)
+        return self._overview
+
+    async def load_resources(self) -> None:
+        if self.resources:
+            return
+        resource_data = await get_registry_api(
+            f"{self.organization}/{self.name}/{self.active_version}/index.json",
+            log_widget=self.log_widget,
+        )
+        for g in sorted(resource_data["docs"]["guides"], key=lambda x: x["name"]):
+            self.resources.append(Resource(g["name"], self, type=ResourceType.GUIDE))
+
+        for r in sorted(resource_data["docs"]["resources"], key=lambda x: x["name"]):
+            self.resources.append(Resource(r["name"], self, type=ResourceType.RESOURCE))
+        for d in sorted(resource_data["docs"]["datasources"], key=lambda x: x["name"]):
+            self.resources.append(
+                Resource(d["name"], self, type=ResourceType.DATASOURCE)
+            )
+        for f in sorted(resource_data["docs"]["functions"], key=lambda x: x["name"]):
+            self.resources.append(Resource(f["name"], self, type=ResourceType.FUNCTION))
+
+    def __rich__(self):
+        return RICH_JSON(
+            json.dumps(
+                {
+                    k: v
+                    for k, v in self.__dict__.items()
+                    if k not in ["raw_json", "versions"]
+                }
+            )
+        )
+
+
+async def populate_providers(log_widget: Optional[Any] = None) -> Dict[str, Provider]:
+    LOGGER.debug("Populating providers")
+    providers = {}
+
+    data = await get_registry_api("index.json", log_widget=log_widget)
+
+    LOGGER.debug("Got API response")
+
+    for provider_json in data["providers"]:
+        provider = Provider.from_json(provider_json)
+        provider.log_widget = log_widget
+        filter_in = (
+            provider.versions,
+            not provider.blocked,
+            (not provider.fork_of or provider.organization == "opentofu"),
+            provider.organization not in ["terraform-providers"],
+        )
+        if all(filter_in):
+            providers[provider.display_name] = provider
+
+    providers = {
+        k: v
+        for k, v in sorted(
+            providers.items(), key=lambda p: p[1].popularity, reverse=True
+        )
+    }
+
+    return providers
