4026c9f1a141194e2a50bc9a7bd0c91d2608eded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:26 +0000

    Add package explaineval/0.1.6/explaineval-0.1.6.tar.gz

---
+++ packages/explaineval/explaineval-0.1.6.tar.gz/explaineval-0.1.6/explaineval/explainers/shap_explainer.py
@@ -0,0 +1,26 @@
+import shap
+import matplotlib.pyplot as plt
+
+class ShapExplainer:
+    def __init__(self, model, background_data):
+        self.model = model
+        self.explainer = shap.Explainer(model, background_data)
+
+    def explain(self, X_sample):
+        shap_values = self.explainer(X_sample, check_additivity=False)
+
+        # Handle multi-class case (only relevant for classifiers)
+        if len(shap_values.values.shape) == 3:
+            print("⚠️ Detected multi-class SHAP output. Showing class 0")
+            shap.plots.beeswarm(shap.Explanation(
+                values=shap_values.values[:, :, 0],
+                base_values=shap_values.base_values[:, 0],
+                data=shap_values.data,
+                feature_names=shap_values.feature_names
+            ))
+        else:
+            shap.plots.beeswarm(shap_values)
+
+        plt.show()
+
+
