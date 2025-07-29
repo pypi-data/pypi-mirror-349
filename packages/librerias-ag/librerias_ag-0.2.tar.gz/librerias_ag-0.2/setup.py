7f7e839dd943ad4fd51ee7d8687961dca8dcea0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:50 +0000

    Add package librerias-ag/0.2/librerias_ag-0.2.tar.gz

---
+++ packages/librerias-ag/librerias_ag-0.2.tar.gz/librerias_ag-0.2/setup.py
@@ -0,0 +1,29 @@
+#Setup.py
+
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name= 'librerias_ag',
+    version='0.2',
+    author="Jorge Salazar",
+    author_email="jisalazar@ucundinamarca.edu.co",
+    description="Una libreria para Algoritmos Geneticos",
+    long_description=long_description,  # Long description from README.md
+    long_description_content_type="text/markdown",
+    url="https://github.com/JSalazar17/librerias_ag",   
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+
+    "numpy"
+
+    ],
+)
