fe6dccc161a03b3c0fc21480aa77124d5f8e929f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:32 +0000

    Add package moddb/0.13.0/moddb-0.13.0.tar.gz

---
+++ packages/moddb/moddb-0.13.0.tar.gz/moddb-0.13.0/moddb/__init__.py
@@ -0,0 +1,31 @@
+import requests
+
+from .base import front_page, login, logout, parse_page, parse_results, rss, search, search_tags
+from .client import Client, Thread
+from .enums import *
+from .pages import *
+from .utils import BASE_URL, LOGGER, Object, get_page, request, soup, SSLAdapter
+
+SESSION = requests.Session()
+SESSION.mount("https://", SSLAdapter())
+
+__version__ = "0.13.0"
+
+__all__ = [
+    "front_page",
+    "login",
+    "logout",
+    "parse_page",
+    "parse_results",
+    "rss",
+    "search",
+    "search_tags",
+    "Client",
+    "Thread",
+    "BASE_URL",
+    "LOGGER",
+    "Object",
+    "get_page",
+    "request",
+    "soup",
+]
