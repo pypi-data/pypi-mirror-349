2fd4b68e0a514df28b915f6cf3f53fa8accb9d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:45 +0000

    Add package dktshell/0.0.1/dktshell-0.0.1.tar.gz

---
+++ packages/dktshell/dktshell-0.0.1.tar.gz/dktshell-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(    
+     name="dktshell",     
+     version="0.0.1",
+     install_requires=[
+         "numpy>=1.26",
+         "scipy>=1.15",
+         "numba>=0.61",
+     ],
+     python_requires=">=3.12", 
+     author="Kazuki Hayashi and Romain Mesnil",
+     description="Matrix-based structural analysis module for triangulated thin shells using discrete Kirchhoff triangle (DKT) elements.",
+     packages=["dktshell"],
+)
