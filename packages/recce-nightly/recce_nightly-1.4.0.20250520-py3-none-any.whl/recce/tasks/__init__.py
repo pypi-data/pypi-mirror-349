a0059f24dd67a6ee15a39c9bca0f81e71bf300b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:22 +0000

    Add package recce-nightly/1.4.0.20250520/recce_nightly-1.4.0.20250520-py3-none-any.whl

---
+++ packages/recce-nightly/recce_nightly-1.4.0.20250520-py3-none-any.whl/recce/tasks/__init__.py
@@ -0,0 +1,7 @@
+from .core import Task
+from .histogram import HistogramDiffTask
+from .profile import ProfileDiffTask, ProfileTask
+from .query import QueryBaseTask, QueryDiffTask, QueryTask
+from .rowcount import RowCountDiffTask, RowCountTask
+from .top_k import TopKDiffTask
+from .valuediff import ValueDiffDetailTask, ValueDiffTask
