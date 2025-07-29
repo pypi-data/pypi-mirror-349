3221629db3e45aaea7f90283d9768833f31accac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:13:42 +0000

    Add package dq-suite-amsterdam/0.11.19/dq_suite_amsterdam-0.11.19-py3-none-any.whl

---
+++ packages/dq-suite-amsterdam/dq_suite_amsterdam-0.11.19-py3-none-any.whl/dq_suite/schemas/regel.py
@@ -0,0 +1,10 @@
+from pyspark.sql.types import StructType
+
+SCHEMA = (
+    StructType()
+    .add("regelNaam", "string")
+    .add("regelParameters", "string")
+    .add("norm", "integer")
+    .add("bronTabelId", "string")
+    .add("attribuut", "string")
+)
