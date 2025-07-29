deebe93c5626442e970d2d382a93bebd1b719991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:09 +0000

    Add package aiagents4pharma/1.38.0/aiagents4pharma-1.38.0.tar.gz

---
+++ packages/aiagents4pharma/aiagents4pharma-1.38.0.tar.gz/aiagents4pharma-1.38.0/aiagents4pharma/talk2scholars/tools/paper_download/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This package provides modules for fetching and downloading academic papers from arXiv.
+"""
+
+# Import modules
+from . import download_arxiv_input
+
+__all__ = [
+    "download_arxiv_input",
+]
