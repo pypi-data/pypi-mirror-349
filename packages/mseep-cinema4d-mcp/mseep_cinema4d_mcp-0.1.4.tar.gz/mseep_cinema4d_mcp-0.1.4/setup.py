b1d28e6e76cac4ae802a8b4aeb5e253f6769f644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:15 +0000

    Add package mseep-cinema4d-mcp/0.1.4/mseep_cinema4d_mcp-0.1.4.tar.gz

---
+++ packages/mseep-cinema4d-mcp/mseep_cinema4d_mcp-0.1.4.tar.gz/mseep_cinema4d_mcp-0.1.4/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+    name="mseep-cinema4d-mcp",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    maintainer="mseep",
+    maintainer_email="support@skydeck.ai",
+                    version="0.1.2",
+        )
