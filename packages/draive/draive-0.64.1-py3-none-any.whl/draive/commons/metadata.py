7bbd8008f6caee737b4aaabe13b319adb4465136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:27 +0000

    Add package draive/0.64.1/draive-0.64.1-py3-none-any.whl

---
+++ packages/draive/draive-0.64.1-py3-none-any.whl/draive/commons/metadata.py
@@ -0,0 +1,21 @@
+from collections.abc import Mapping, Sequence
+from types import MappingProxyType
+from typing import Final
+
+from haiway import AttributePath
+
+__all__ = (
+    "META_EMPTY",
+    "Meta",
+    "MetaPath",
+    "MetaValue",
+)
+
+type MetaValue = Mapping[str, MetaValue] | Sequence[MetaValue] | str | float | int | bool | None
+
+type Meta = Mapping[str, MetaValue]
+
+MetaPath: Meta = AttributePath(Meta, attribute=Meta)  # pyright: ignore[reportArgumentType, reportAssignmentType, reportCallIssue]
+
+# using mapping proxy to make sure it won't be mutated as there is no frozendict
+META_EMPTY: Final[Meta] = MappingProxyType({})
