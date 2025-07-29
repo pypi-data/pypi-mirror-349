7d4b7ac2f29f5b2a9370d99974ffa5ab8daa00d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:24 +0000

    Add package hello-pkg-janvi/0.1.1/hello_pkg_janvi-0.1.1.tar.gz

---
+++ packages/hello-pkg-janvi/hello_pkg_janvi-0.1.1.tar.gz/hello_pkg_janvi-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hello_pkg_janvi",
+    version="0.1.1",
+    packages=find_packages(),
+    author="Janvi",
+    author_email="janvi@dataprowess.com.au",
+    description="A simple hello world package",
+    url="https://github.com/Janvi-05/hello_pkg",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
