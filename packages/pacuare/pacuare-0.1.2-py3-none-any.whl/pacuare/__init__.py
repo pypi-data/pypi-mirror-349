6f46f4a580d5aed069323047794ea5b796876321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:36 +0000

    Add package pacuare/0.1.2/pacuare-0.1.2-py3-none-any.whl

---
+++ packages/pacuare/pacuare-0.1.2-py3-none-any.whl/pacuare/__init__.py
@@ -0,0 +1,25 @@
+import requests
+import pandas as pd
+
+class Client:
+    """A client for the Pacuare API."""
+    def __init__(self, api_key: str, base: str = 'https://app.pacuare.dev'):
+        """
+        Create a new client.
+        An API key must be generated in your account settings to access the API.
+        `base` is for development purposes.
+        """
+        self.api_key = api_key
+        self.base = base
+    
+    def query(self, sql: str) -> pd.DataFrame:
+        """Query your database using SQL, returning a Pandas DataFrame of the results."""
+        res = requests.post(self.base + '/api/query?language=sql',
+                            headers={
+                                'Authorization': 'Bearer ' + self.api_key,
+                                'Content-Type': 'text/sql',
+                                'Accept': 'application/json'
+                            },
+                            data=sql)
+        
+        return pd.DataFrame.from_records(res.json())
