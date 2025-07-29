5d29a47b3484a2264b6ceb94aed79ced0aa9c0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:03 +0000

    Add package talentro-definitions/0.4.3/talentro_definitions-0.4.3.tar.gz

---
+++ packages/talentro-definitions/talentro_definitions-0.4.3.tar.gz/talentro_definitions-0.4.3/src/talentro/billing/models.py
@@ -0,0 +1,34 @@
+import enum
+import uuid
+from datetime import datetime, timezone
+from typing import Optional
+from sqlalchemy import Enum as SqlEnum
+
+from sqlalchemy import Column
+from sqlmodel import SQLModel, Field
+
+from src.talentro.general.models import BillingModel
+
+
+class CurrencyEnum(str, enum.Enum):
+    USD = "USD"
+    EUR = "EUR"
+    GBP = "GBP"
+    YPJ = "YPJ"
+
+
+class BillingEvent(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    organization: uuid.UUID = Field(index=True)
+    event_time: Optional[datetime] = Field(
+        default_factory=lambda: datetime.now(timezone.utc)
+    )
+    sku: int = Field(foreign_key="sku.id")
+    amount: float
+
+
+class SKU(BillingModel, table=True):
+    amount: float
+    currency: CurrencyEnum = Field(sa_column=Column(SqlEnum(CurrencyEnum)))
+
+
