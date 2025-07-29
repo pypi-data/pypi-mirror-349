74e82d94b5c7559eb97b1bfa8f77291ab9e5c319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:50 +0000

    Add package spm-python/25.1b1.post2/spm_python-25.1b1.post2.tar.gz

---
+++ packages/spm-python/spm_python-25.1b1.post2.tar.gz/spm_python-25.1b1.post2/spm/__external/__fieldtrip/__fileio/_db_close.py
@@ -0,0 +1,16 @@
+from mpython import Runtime
+
+
+def _db_close(*args, **kwargs):
+    """
+      DB_CLOSE closes the connection to the database
+
+        Use as
+          db_close
+
+
+    [Matlab code]( https://github.com/spm/spm/blob/main/external/fieldtrip/fileio/private/db_close.m )
+
+    Copyright (C) 1995-2025 Functional Imaging Laboratory, Department of Imaging Neuroscience, UCL
+    """
+    return Runtime.call("db_close", *args, **kwargs, nargout=0)
