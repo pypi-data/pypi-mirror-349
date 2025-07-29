575107b677d1ded698626ea3d294a0f734587df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:59 +0000

    Add package libbr/0.1.0/libbr-0.1.0.tar.gz

---
+++ packages/libbr/libbr-0.1.0.tar.gz/libbr-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='libbr',
+    version='0.1.0',
+    author='Renata Machado Barreto Braga',
+    description='Uma biblioteca gráfica simples e brasileira, estilo PySimpleGUI.',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
