0f25f352c7ef1de4be194a272451ecfbc25cbaf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:27 +0000

    Add package django-polymorphic/4.1.0/django_polymorphic-4.1.0-py3-none-any.whl

---
+++ packages/django-polymorphic/django_polymorphic-4.1.0-py3-none-any.whl/polymorphic/__init__.py
@@ -0,0 +1,12 @@
+"""
+Seamless Polymorphic Inheritance for Django Models
+
+Copyright:
+This code and affiliated files are (C) by Bert Constantin and individual contributors.
+Please see LICENSE and AUTHORS for more information.
+"""
+
+VERSION = "4.1.0"
+
+# version synonym for backwards compatibility
+__version__ = VERSION
