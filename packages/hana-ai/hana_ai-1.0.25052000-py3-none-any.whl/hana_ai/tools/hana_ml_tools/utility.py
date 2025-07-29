6068dbd3e830ab7758b24b9beefe2f175e114830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:40 +0000

    Add package hana-ai/1.0.25052000/hana_ai-1.0.25052000-py3-none-any.whl

---
+++ packages/hana-ai/hana_ai-1.0.25052000-py3-none-any.whl/hana_ai/tools/hana_ml_tools/utility.py
@@ -0,0 +1,21 @@
+"""
+Utility functions for the HANA ML tools.
+"""
+import json
+from datetime import datetime, date
+from pandas import Timestamp
+from numpy import int64
+
+class _CustomEncoder(json.JSONEncoder):
+    """
+    This class is used to encode the model attributes into JSON string.
+    """
+    def default(self, obj): #pylint: disable=arguments-renamed
+        if isinstance(obj, (Timestamp, datetime, date)):
+            # Convert Timestamp, datetime or date to ISO string
+            return obj.isoformat()
+        elif isinstance(obj, (int64, int)):
+            # Convert numpy int64 or Python int to Python int
+            return int(obj)
+        # Let other types use the default handler
+        return super().default(obj)
