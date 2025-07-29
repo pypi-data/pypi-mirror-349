907651811e88688754b2184d2527a7567015bb7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:45 +0000

    Add package DecoKit/0.1.4/decokit-0.1.4-py3-none-any.whl

---
+++ packages/DecoKit/decokit-0.1.4-py3-none-any.whl/DecoKit/__init__.py
@@ -0,0 +1,3 @@
+from .DecoKit import setTimeout, setInterval, timeIt, catch, catchAll, callLimit, retry, memoize, rateLimit, memoize
+
+__all__ = ["setTimeout", "setInterval", "timeIt", "catch", "catchAll", "callLimit", "retry", "memoize", "rateLimit", "memoize"]
