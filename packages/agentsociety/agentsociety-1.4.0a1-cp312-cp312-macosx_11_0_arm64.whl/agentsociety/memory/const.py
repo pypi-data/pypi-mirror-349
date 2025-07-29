ce180bb538c97ec96da72659403aa7df28b5bace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:08 +0000

    Add package agentsociety/1.4.0a1/agentsociety-1.4.0a1-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/agentsociety/agentsociety-1.4.0a1-cp312-cp312-macosx_11_0_arm64.whl/agentsociety/memory/const.py
@@ -0,0 +1,44 @@
+from pycityproto.city.person.v2.motion_pb2 import Status
+
+PROFILE_ATTRIBUTES = {
+    "name": str(),
+    "gender": str(),
+    "age": float(),
+    "education": str(),
+    "skill": str(),
+    "occupation": str(),
+    "family_consumption": str(),
+    "consumption": str(),
+    "personality": str(),
+    "income": float(),
+    "currency": float(),
+    "residence": str(),
+    "race": str(),
+    "city": str(),
+    "religion": str(),
+    "marital_status": str(),
+}
+
+STATE_ATTRIBUTES = {
+    # base
+    "id": -1,
+    "attribute": dict(),
+    "home": dict(),
+    "work": dict(),
+    "schedules": [],
+    "vehicle_attribute": dict(),
+    "bus_attribute": dict(),
+    "pedestrian_attribute": dict(),
+    "bike_attribute": dict(),
+    # motion
+    "status": Status.STATUS_UNSPECIFIED,
+    "position": dict(),
+    "v": float(),
+    "direction": float(),
+    "activity": str(),
+    "l": float(),
+}
+
+SELF_DEFINE_PREFIX = "self_define_"
+
+TIME_STAMP_KEY = "_timestamp"
