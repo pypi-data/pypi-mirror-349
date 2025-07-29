87e44474366bde95d399bf28b6f0df3123777595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:19 +0000

    Add package prompted/0.0.7/prompted-0.0.7-py3-none-any.whl

---
+++ packages/prompted/prompted-0.0.7-py3-none-any.whl/prompted/_create/__init__.py
@@ -0,0 +1,20 @@
+"""
+💬 prompted._create
+
+Contains the `Create` module, which is used for creating
+chat completions / structured outputs and other LLM related
+operations quickly & easily using the `prompted` package.
+
+This module requires `litellm` and `instructor` to be installed &
+can be installed using either:
+
+```bash
+pip install litellm instructor
+```
+
+or
+
+```bash
+pip install prompted[create]
+```
+"""
