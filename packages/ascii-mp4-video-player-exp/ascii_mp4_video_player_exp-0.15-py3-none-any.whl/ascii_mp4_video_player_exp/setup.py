43b2f6694d31414ba0e685cee8088aa661b7595b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:25 +0000

    Add package ascii-mp4-video-player-exp/0.15/ascii_mp4_video_player_exp-0.15-py3-none-any.whl

---
+++ packages/ascii-mp4-video-player-exp/ascii_mp4_video_player_exp-0.15-py3-none-any.whl/ascii_mp4_video_player_exp/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='ascii_mp4_video_player_exp',
+    version='0.15',
+    author='paul',
+    author_email='paul@kfsoft.info',
+    description='ASCII mp4 video player experimental',
+    packages=['ascii_mp4_video_player_exp'],  
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        'opencv-python',
+        'numpy',
+        'windows-curses ; platform_system == "Windows"'
+    ],
+)
