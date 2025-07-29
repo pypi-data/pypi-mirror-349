82541729039375e0a3a8c78559c6ee54676a8708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:44 +0000

    Add package financial-reports-generated-client/0.3.4/financial_reports_generated_client-0.3.4.tar.gz

---
+++ packages/financial-reports-generated-client/financial_reports_generated_client-0.3.4.tar.gz/financial_reports_generated_client-0.3.4/financial_reports_generated_client/api/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+
+# import apis into api package
+from financial_reports_generated_client.api.companies_api import CompaniesApi
+from financial_reports_generated_client.api.filing_types_api import FilingTypesApi
+from financial_reports_generated_client.api.filings_api import FilingsApi
+from financial_reports_generated_client.api.industries_api import IndustriesApi
+from financial_reports_generated_client.api.industry_groups_api import IndustryGroupsApi
+from financial_reports_generated_client.api.processed_filings_api import ProcessedFilingsApi
+from financial_reports_generated_client.api.schema_api import SchemaApi
+from financial_reports_generated_client.api.sectors_api import SectorsApi
+from financial_reports_generated_client.api.sources_api import SourcesApi
+from financial_reports_generated_client.api.sub_industries_api import SubIndustriesApi
+from financial_reports_generated_client.api.watchlist_api import WatchlistApi
+
