c62c980a48deae05a1a6a5622ddf31ab746b4cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:06 +0000

    Add package aceti/0.0.1/aceti-0.0.1.tar.gz

---
+++ packages/aceti/aceti-0.0.1.tar.gz/aceti-0.0.1/src/aceti/__init__.py
@@ -0,0 +1,2 @@
+# from .maps_utils import png_to_csv, csv_to_png, csv_to_npy, invert_map, get_output_name, csv_shrink, csv_downsize
+from .map_importer import import_map
---
+++ packages/aceti/aceti-0.0.1.tar.gz/aceti-0.0.1/tests/test.py
@@ -0,0 +1,3 @@
+from aceti_maps import import_map
+
+base_matrix, lat_lon_map  = import_map("alamillo11x15")
