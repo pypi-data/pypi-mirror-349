7dad18a38be78f97b39b0d7a5d692be213aa3444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:04:46 +0000

    Add package giftool2/0.16.0/giftool2-0.16.0.tar.gz

---
+++ packages/giftool2/giftool2-0.16.0.tar.gz/giftool2-0.16.0/tests/test_api.py
@@ -0,0 +1,8 @@
+import pytest
+from giftool2.app import app
+
+def test_create_app():
+    """Test the creation of the Flask app."""
+    # app_instance = app.run()
+    # assert app_instance is not None
+    assert '1' == '1'
