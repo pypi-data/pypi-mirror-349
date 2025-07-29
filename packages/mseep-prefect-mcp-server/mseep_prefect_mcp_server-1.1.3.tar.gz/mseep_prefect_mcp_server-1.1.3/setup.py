eafcad51c5a26ef07ebaf6fb456b429947b2d602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:21 +0000

    Add package mseep-prefect-mcp-server/1.1.3/mseep_prefect_mcp_server-1.1.3.tar.gz

---
+++ packages/mseep-prefect-mcp-server/mseep_prefect_mcp_server-1.1.3.tar.gz/mseep_prefect_mcp_server-1.1.3/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-prefect-mcp-server",
+    version="1.1.2",
+    description="MCP server for interacting with the Prefect API",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp[cli]', 'httpx>=0.24.0', 'prefect>=3.0.0'],
+    keywords=["mseep"] + [],
+)
