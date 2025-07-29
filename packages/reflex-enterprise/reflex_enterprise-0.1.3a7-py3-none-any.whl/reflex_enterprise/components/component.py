ee30f3f0e32950948957b02fbdce8fab27468c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:23 +0000

    Add package reflex-enterprise/0.1.3a7/reflex_enterprise-0.1.3a7-py3-none-any.whl

---
+++ packages/reflex-enterprise/reflex_enterprise-0.1.3a7-py3-none-any.whl/reflex_enterprise/components/component.py
@@ -0,0 +1,25 @@
+"""The Reflex Enterprise component."""
+
+from reflex.components.component import Component
+
+from reflex_enterprise.app import AppEnterprise
+
+
+class ComponentEnterprise(Component):
+    """The Reflex Enterprise component."""
+
+    @classmethod
+    def create(cls, *children, **props) -> Component:
+        """Create the Reflex Enterprise component."""
+        return super().create(*children, **props)
+
+    def render(self, *children, **props) -> dict:
+        """Render the Reflex Enterprise component."""
+        from reflex.utils.prerequisites import get_app
+
+        if not isinstance(get_app().app, AppEnterprise):
+            raise TypeError(
+                "Reflex Enterprise components can only be used in an Enterprise app. Use rxe.App() instead of rx.App()."
+            )
+
+        return super().render(*children, **props)
