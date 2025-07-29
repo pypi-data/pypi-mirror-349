0c5d3c041bb0deca7951a986b8aa38b888577022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:49 +0000

    Add package strictprotocol/0.1.11/strictprotocol-0.1.11.tar.gz

---
+++ packages/strictprotocol/strictprotocol-0.1.11.tar.gz/strictprotocol-0.1.11/strictprotocol/__init__.py
@@ -0,0 +1,13 @@
+from .core import StrictProtocol, is_signature_compatible, CheckMode, ProtocolError, get_best_effort_annotations
+from .safe_typing import safe_subtype, safe_isinstance, get_callable_signature, is_type_object, is_service_class
+__all__ = ["StrictProtocol", 
+           "is_signature_compatible", 
+           "CheckMode", 
+           "safe_subtype", 
+           "safe_isinstance", 
+           'ProtocolError',
+           'get_callable_signature',
+           "get_best_effort_annotations",
+           'is_type_object',
+           'is_service_class',
+           ]
