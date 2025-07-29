19da53f9a44ede816e1121d9abd86a16dee55510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:40 +0000

    Add package genetic-algorithm-library/0.1.0/genetic_algorithm_library-0.1.0-py3-none-any.whl

---
+++ packages/genetic-algorithm-library/genetic_algorithm_library-0.1.0-py3-none-any.whl/genetic_algorithm/__init__.py
@@ -0,0 +1,11 @@
+# Importar funciones principales para que estén disponibles directamente
+from .functions import (
+    create_population,
+    fitness_function,
+    selection,
+    crossover,
+    mutation,
+    run_genetic_algorithm
+)
+
+__version__ = '0.1.0'
