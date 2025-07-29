900682f8775f0f5c041b2f08d02e047336c9fa91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:19 +0000

    Add package mseep-runbook-mcp-server/0.1.1/mseep_runbook_mcp_server-0.1.1.tar.gz

---
+++ packages/mseep-runbook-mcp-server/mseep_runbook_mcp_server-0.1.1.tar.gz/mseep_runbook_mcp_server-0.1.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-runbook-mcp-server",
+    version="0.1.0",
+    description="Runbook MCP server",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['httpx>=0.28.1', 'mcp[cli]>=1.6.0', 'pyyaml>=6.0.2', 'whoosh>=2.7.4'],
+    keywords=["mseep"] + [],
+)
