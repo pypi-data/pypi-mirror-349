da703bc4d1ca30e6a19366f91d400edf28fc80f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:19 +0000

    Add package spotpython/0.29.18/spotpython-0.29.18-py3-none-any.whl

---
+++ packages/spotpython/spotpython-0.29.18-py3-none-any.whl/spotpython/light/regression/__init__.py
@@ -0,0 +1,25 @@
+"""
+This module implements pytorch lightning neural networks for handling regression tasks.
+
+"""
+
+from .nn_resnet_regressor import NNResNetRegressor
+from .nn_transformer_regressor import NNTransformerRegressor
+from .nn_linear_regressor import NNLinearRegressor
+from .netlightregression import NetLightRegression
+from .nn_condnet_regressor import NNCondNetRegressor
+from .nn_many_to_many_rnn_regressor import ManyToManyRNNRegressor, ManyToManyRNN
+from .nn_many_to_many_gru_regressor import ManyToManyGRURegressor
+from .nn_many_to_many_lstm_regressor import ManyToManyLSTMRegressor
+
+__all__ = [
+    "NNLinearRegressor",
+    "NetLightRegression",
+    "NNResNetRegressor",
+    "NNTransformerRegressor",
+    "NNCondNetRegressor",
+    "ManyToManyRNNRegressor",
+    "ManyToManyRNN",
+    "ManyToManyGRURegressor",
+    "ManyToManyLSTMRegressor",
+]
