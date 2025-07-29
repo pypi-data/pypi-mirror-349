8d7cebb3dde37883f171e59edeb66565e2e3b498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:58 +0000

    Add package mseep-gossiphs/0.11.7/mseep_gossiphs-0.11.7.tar.gz

---
+++ packages/mseep-gossiphs/mseep_gossiphs-0.11.7.tar.gz/mseep_gossiphs-0.11.7/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-gossiphs",
+    version="0.11.6",
+    description="",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    keywords=["mseep"] + [],
+)
