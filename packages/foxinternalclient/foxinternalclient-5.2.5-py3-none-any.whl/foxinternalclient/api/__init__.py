e26be33f365ba59687b9d11911dcf444c42bc746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:59 +0000

    Add package foxinternalclient/5.2.5/foxinternalclient-5.2.5-py3-none-any.whl

---
+++ packages/foxinternalclient/foxinternalclient-5.2.5-py3-none-any.whl/foxinternalclient/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from foxinternalclient.api.device_api import DeviceApi
+from foxinternalclient.api.fox_factory_api import FoxFactoryApi
+from foxinternalclient.api.gitlab_api import GitlabApi
+from foxinternalclient.api.internal_api import InternalApi
+from foxinternalclient.api.release_api import ReleaseApi
+from foxinternalclient.api.stats_api import StatsApi
+
