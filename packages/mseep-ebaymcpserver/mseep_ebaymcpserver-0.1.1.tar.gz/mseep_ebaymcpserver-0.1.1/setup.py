0a0b38f0dd3fe58068be9c2deff61d5a5d576736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:19 +0000

    Add package mseep-ebaymcpserver/0.1.1/mseep_ebaymcpserver-0.1.1.tar.gz

---
+++ packages/mseep-ebaymcpserver/mseep_ebaymcpserver-0.1.1.tar.gz/mseep_ebaymcpserver-0.1.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-ebaymcpserver",
+    version="0.1.0",
+    description="Add your description here",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp>=1.3.0', 'pydantic'],
+    keywords=["mseep"] + [],
+)
