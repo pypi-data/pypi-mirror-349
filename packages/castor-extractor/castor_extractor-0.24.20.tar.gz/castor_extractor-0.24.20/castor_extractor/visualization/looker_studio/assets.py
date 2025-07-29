8393aed3fe3d4ef1a7d96b3510f661c9a4eda7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:41 +0000

    Add package castor-extractor/0.24.20/castor_extractor-0.24.20.tar.gz

---
+++ packages/castor-extractor/castor_extractor-0.24.20.tar.gz/castor_extractor-0.24.20/castor_extractor/visualization/looker_studio/assets.py
@@ -0,0 +1,7 @@
+from ...types import ExternalAsset
+
+
+class LookerStudioAsset(ExternalAsset):
+    ASSETS = "assets"
+    SOURCE_QUERIES = "source_queries"
+    VIEW_ACTIVITY = "view_activity"
---
+++ packages/castor-extractor/castor_extractor-0.24.20.tar.gz/castor_extractor-0.24.20/castor_extractor/visualization/looker_studio/client/scopes.py
@@ -0,0 +1,6 @@
+SCOPES = (
+    "https://www.googleapis.com/auth/datastudio",
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/admin.reports.audit.readonly",
+    "https://www.googleapis.com/auth/admin.directory.user.readonly",
+)
