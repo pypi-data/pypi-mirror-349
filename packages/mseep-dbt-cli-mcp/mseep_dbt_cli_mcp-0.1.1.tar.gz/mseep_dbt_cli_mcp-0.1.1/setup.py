a5168327e98654deff3dbc0c0fe194f27df2c2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:20 +0000

    Add package mseep-dbt-cli-mcp/0.1.1/mseep_dbt_cli_mcp-0.1.1.tar.gz

---
+++ packages/mseep-dbt-cli-mcp/mseep_dbt_cli_mcp-0.1.1.tar.gz/mseep_dbt_cli_mcp-0.1.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-dbt-cli-mcp",
+    version="0.1.0",
+    description="A Model Context Protocol server for dbt CLI",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp[cli]>=1.2.0', 'python-dotenv>=1.0.0'],
+    keywords=["mseep"] + [],
+)
