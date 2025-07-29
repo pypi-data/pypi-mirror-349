5d07815dbfbe204326a5c76f6e53cca2ecd2fbfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:43 +0000

    Add package utilsx/0.0.0a3/utilsx-0.0.0a3.tar.gz

---
+++ packages/utilsx/utilsx-0.0.0a3.tar.gz/utilsx-0.0.0a3/docs/index.md
@@ -0,0 +1,13 @@
+# UtilsX
+
+Generic utilities for Python projects.
+
+[Collections](collections.md)
+[Constants](constants.md)
+[Dictionaries](dictionaries.md)
+[Exceptions](exceptions.md)
+[Math](math.md)
+[Text](text.md)
+[Typevars](typevars.md)
+
+:::utilsx
---
+++ packages/utilsx/utilsx-0.0.0a3.tar.gz/utilsx-0.0.0a3/src/utilsx/__init__.py
@@ -0,0 +1,9 @@
+"""UtilsX - a collection of generic Python utility functions and types."""
+
+from .collections import *
+from .constants import *
+from .dicts import *
+from .exceptions import *
+from .math import *
+from .text import *
+from .typevars import *
