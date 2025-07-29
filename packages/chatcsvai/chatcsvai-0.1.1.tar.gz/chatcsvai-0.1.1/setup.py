cafcd1183afce60d47b24dcff38fb742688b3775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:55 +0000

    Add package chatcsvai/0.1.1/chatcsvai-0.1.1.tar.gz

---
+++ packages/chatcsvai/chatcsvai-0.1.1.tar.gz/chatcsvai-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chatcsvai",
+    version="0.1.0",
+    author="Your Name",
+    author_email="luckykandpal091@gmail.com",
+    description="A chatbot for interacting with CSV files using AI.",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "duckdb",
+        "langchain-core",
+        "sentence-transformers",
+        "langchain",
+        "langchain-huggingface",
+        "langchain-community",
+        "langchain-chains",
+        "langchain-groq",
+        "langchain-agents",
+        "langchain-experimental",
+        "langchain-memory",
+        
+    ],
+    entry_points={
+        "console_scripts": [
+            "chatcsvai=main:main",
+        ],
+    },
+)
