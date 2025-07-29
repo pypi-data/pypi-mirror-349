13623b72b48957a76bf1d2538f8378314371b003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:48 +0000

    Add package OptimalAgents-power/0.1.4/optimalagents_power-0.1.4.tar.gz

---
+++ packages/OptimalAgents-power/optimalagents_power-0.1.4.tar.gz/optimalagents_power-0.1.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OptimalAgents-power',
+    version='0.1.4',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'fastapi',
+        'flask',
+        'requests',
+        'uvicorn'
+    ],
+    entry_points={
+        'console_scripts': [
+            'create-superpower=optimalagents.cli:create_project'
+        ],
+    },
+    author='Sandilya Kishlay',
+    author_email='sandilyakishlay@gmail.com',
+    description='A project generator for web superheroes using FastAPI, Flask, or REST.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/sandilyaKishlay',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.11',
+)
