c88d840b228cfd85669ae8a1138d1c76248b893e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:00 +0000

    Add package mseep-mcp-finnhub/0.1.2/mseep_mcp_finnhub-0.1.2.tar.gz

---
+++ packages/mseep-mcp-finnhub/mseep_mcp_finnhub-0.1.2.tar.gz/mseep_mcp_finnhub-0.1.2/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-mcp-finnhub",
+    version="0.1.0",
+    description="An MCP server to connect with Finnhub financial API.",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp>=1.0.0', 'python-dotenv>=1.0.1', 'fastmcp>=0.4.0', 'finnhub-python'],
+    keywords=["mseep"] + [],
+)
