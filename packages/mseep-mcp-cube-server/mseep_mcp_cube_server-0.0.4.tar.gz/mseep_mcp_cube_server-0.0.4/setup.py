9712d6ced60210c5dede1fd0596ef05af2c30c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:50 +0000

    Add package mseep-mcp-cube-server/0.0.4/mseep_mcp_cube_server-0.0.4.tar.gz

---
+++ packages/mseep-mcp-cube-server/mseep_mcp_cube_server-0.0.4.tar.gz/mseep_mcp_cube_server-0.0.4/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-mcp_cube_server",
+    version="0.0.2",
+    description="MCP server for interfacing with Cube.dev REST API",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp>=1.2.1', 'pandas', 'pyjwt>=2.10.1', 'python-dotenv', 'pyyaml>=6.0.2', 'requests>=2.32.3'],
+    keywords=["mseep"] + [],
+)
