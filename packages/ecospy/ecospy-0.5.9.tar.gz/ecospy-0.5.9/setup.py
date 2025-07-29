fb3d10f48078fdb8b7d077595fe612f8b00b5fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:06 +0000

    Add package ecospy/0.5.9/ecospy-0.5.9.tar.gz

---
+++ packages/ecospy/ecospy-0.5.9.tar.gz/ecospy-0.5.9/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, find_packages
+
+WINDOWS = (os.name == 'nt')
+
+
+def version():
+    with open("version.txt", "r") as f:
+        return f.readline().strip()
+
+
+def binary_suffix():
+    return ".exe" if WINDOWS else ""
+
+
+setup(name="ecospy",
+      version=version(),
+      data_files=[
+          ("Scripts", [f"ecospy/binaries/proxyfmu{binary_suffix()}", f"ecospy/binaries/ecos{binary_suffix()}"])
+      ]
+      )
