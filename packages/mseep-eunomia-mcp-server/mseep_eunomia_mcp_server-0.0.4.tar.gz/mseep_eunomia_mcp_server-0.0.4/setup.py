47663b6a15d1be6491be7b10d228e9dbb5fae21f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:16 +0000

    Add package mseep-eunomia-mcp-server/0.0.4/mseep_eunomia_mcp_server-0.0.4.tar.gz

---
+++ packages/mseep-eunomia-mcp-server/mseep_eunomia_mcp_server-0.0.4.tar.gz/mseep_eunomia_mcp_server-0.0.4/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-eunomia-mcp-server",
+    version="0.0.2",
+    description="",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['pip', 'uv', 'pydantic>=2.8.0', 'python-dotenv==1.0.0', 'pydantic-settings>=2.1.0', 'mcp>=1.1.2', 'eunomia-ai'],
+    keywords=["mseep"] + [],
+)
