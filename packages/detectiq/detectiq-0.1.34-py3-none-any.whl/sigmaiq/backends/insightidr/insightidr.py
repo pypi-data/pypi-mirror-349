a9dfa62ad9090f617580606dc42e52e191a30d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:46 +0000

    Add package detectiq/0.1.34/detectiq-0.1.34-py3-none-any.whl

---
+++ packages/detectiq/detectiq-0.1.34-py3-none-any.whl/sigmaiq/backends/insightidr/insightidr.py
@@ -0,0 +1,9 @@
+from sigma.backends.insight_idr import InsightIDRBackend
+
+from sigmaiq.backends.sigmaiq_abstract_backend import AbstractGenericSigmAIQBackendClass
+
+
+class SigmAIQInsightIDRBackend(AbstractGenericSigmAIQBackendClass, InsightIDRBackend):
+    custom_formats = {}
+    associated_pipelines = ["insightidr"]
+    default_pipeline = "insightidr"
