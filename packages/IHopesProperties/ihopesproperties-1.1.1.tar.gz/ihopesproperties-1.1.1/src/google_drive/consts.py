b9b9d20d71f9925d58f6f0356cf6e392e0bc2796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:57 +0000

    Add package IHopesProperties/1.1.1/ihopesproperties-1.1.1.tar.gz

---
+++ packages/IHopesProperties/ihopesproperties-1.1.1.tar.gz/ihopesproperties-1.1.1/src/google_drive/consts.py
@@ -0,0 +1,7 @@
+GOOGLE_DRIVE_PROJECT_NAME: str = 'IHopesPropertiesGProject'
+
+CLIENT_ID: str = '1040077980830-j5tkof0du3okp54iq8as0og4s5k263ja.apps.googleusercontent.com'
+WEB_APP_CLIENT_ID: str = '1040077980830-hs7lpar7pel7tgcgcc8bc7tsrnm03l68.apps.googleusercontent.com'
+WEB_APP_CLIENT_SECRET: str = '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' # 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
+
+ARV_TEMPLATE_FILE_ID: str = '1etDxXHDevTsYqj6_s3T4B-CJYEoGtXh8pTf2xqu3kRE'
