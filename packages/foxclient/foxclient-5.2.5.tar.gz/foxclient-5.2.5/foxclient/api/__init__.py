61601bbdb784ea641969c8a3641adeedd28a16bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:01 +0000

    Add package foxclient/5.2.5/foxclient-5.2.5.tar.gz

---
+++ packages/foxclient/foxclient-5.2.5.tar.gz/foxclient-5.2.5/foxclient/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from foxclient.api.device_api import DeviceApi
+from foxclient.api.release_api import ReleaseApi
+from foxclient.api.stats_api import StatsApi
+
