c32e578bcfed82de64b70b1167f2e72271b68665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:51 +0000

    Add package octopus32/0.1.0/octopus32-0.1.0.tar.gz

---
+++ packages/octopus32/octopus32-0.1.0.tar.gz/octopus32-0.1.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="octopus32",
+    version="0.1.0",
+    description="A utility package for compatibility helpers",
+    author="Fedya",
+    author_email="fedya@example.com",
+    url="https://github.com/fedya/octopus32",
+    packages=["octopus32"],
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+    license="MIT",
+)
