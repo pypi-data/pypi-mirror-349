577a8d6d11594b398acb1aaeff906ce863071076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:11 +0000

    Add package mseep-eclass-mcp-server/0.1.1/mseep_eclass_mcp_server-0.1.1.tar.gz

---
+++ packages/mseep-eclass-mcp-server/mseep_eclass_mcp_server-0.1.1.tar.gz/mseep_eclass_mcp_server-0.1.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-eclass-mcp-server",
+    version="0.1.0",
+    description="An MCP server for the Open eClass platform by GUnet",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp>=1.5.0', 'requests>=2.31.0', 'beautifulsoup4>=4.12.2', 'python-dotenv>=1.0.0'],
+    keywords=["mseep"] + [],
+)
