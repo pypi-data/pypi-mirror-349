db1670e00f36eff5b533400ba3b9809243177685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:11 +0000

    Add package addition-simple-package/0.1.0/addition_simple_package-0.1.0.tar.gz

---
+++ packages/addition-simple-package/addition_simple_package-0.1.0.tar.gz/addition_simple_package-0.1.0/addition_package/addition.py
@@ -0,0 +1,14 @@
+def add(x: int, y: int) -> int:
+    """Adds two integers together.
+
+    This is a simple example function used to demonstrate how to create
+    and structure a Python package.
+
+    Args:
+        x (int): The first number to add.
+        y (int): The second number to add.
+
+    Returns:
+        int: The sum of x and y.
+    """
+    return x + y
