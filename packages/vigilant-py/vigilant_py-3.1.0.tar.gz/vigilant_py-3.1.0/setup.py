dbda1d6feacb3d67d10db40eedaaa7cda05eb14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:28 +0000

    Add package vigilant-py/3.1.0/vigilant_py-3.1.0.tar.gz

---
+++ packages/vigilant-py/vigilant_py-3.1.0.tar.gz/vigilant_py-3.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vigilant-py",
+    version="3.1.0",
+    author="Vigilant Team",
+    author_email="support@vigilant.run",
+    url="https://vigilant.run",
+    description="Python SDK for Vigilant (https://vigilant.run)",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        "requests>=2.28.0",
+    ],
+    python_requires=">=3.7",
+)
