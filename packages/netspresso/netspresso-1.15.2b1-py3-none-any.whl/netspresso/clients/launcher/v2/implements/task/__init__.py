ff909e0458290fa85e7bc01951f8abfac4cca55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:30 +0000

    Add package netspresso/1.15.2b1/netspresso-1.15.2b1-py3-none-any.whl

---
+++ packages/netspresso/netspresso-1.15.2b1-py3-none-any.whl/netspresso/clients/launcher/v2/implements/task/__init__.py
@@ -0,0 +1,5 @@
+from .benchmark_task import BenchmarkTaskAPI
+from .convert_task import ConvertTaskAPI
+from .quantize_task import QuantizeTaskAPI
+
+__all__ = [BenchmarkTaskAPI, ConvertTaskAPI, QuantizeTaskAPI]
