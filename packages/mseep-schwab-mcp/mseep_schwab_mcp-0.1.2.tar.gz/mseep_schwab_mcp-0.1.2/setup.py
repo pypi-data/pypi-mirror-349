86eea0813b2755f23f11f16b3d7cee6aa815bf36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:36 +0000

    Add package mseep-schwab-mcp/0.1.2/mseep_schwab_mcp-0.1.2.tar.gz

---
+++ packages/mseep-schwab-mcp/mseep_schwab_mcp-0.1.2.tar.gz/mseep_schwab_mcp-0.1.2/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-schwab-mcp",
+    version="0.1.0",
+    description="Schwab Model Context Protocol (MCP) for the Schwab API",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['anyio>=4.9.0', 'click>=8.1.8', 'mcp>=1.4.1', 'platformdirs>=4.3.7', 'pyyaml>=6.0.2'],
+    keywords=["mseep"] + [],
+)
