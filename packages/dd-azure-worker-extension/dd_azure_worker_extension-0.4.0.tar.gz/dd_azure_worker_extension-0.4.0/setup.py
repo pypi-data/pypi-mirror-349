88703e5fb2907d4346d0beecd6808bc89fa3b933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:01 +0000

    Add package dd-azure-worker-extension/0.4.0/dd_azure_worker_extension-0.4.0.tar.gz

---
+++ packages/dd-azure-worker-extension/dd_azure_worker_extension-0.4.0.tar.gz/dd_azure_worker_extension-0.4.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dd-azure-worker-extension",
+    version="0.4.0",
+    author="Datadog",
+    description="Python Worker Extension for starting Datadog Tracer and a top level span to enable auto-instrumenting of Azure Function Apps",
+    include_package_data=True,
+    long_description=open("README.md").read(),
+    install_requires=[
+        "azure-functions >= 1.7.0, < 2.0.0",
+        "ddtrace",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    license="License: Apache 2.0",
+    packages=find_packages(where="."),
+    zip_safe=False,
+)
