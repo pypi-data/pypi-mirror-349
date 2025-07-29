4b4b0d214ee8f2d2204d302e45fb9c673aa3c458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:42 +0000

    Add package ascii-mp4-video-player-exp/0.1/ascii_mp4_video_player_exp-0.1.tar.gz

---
+++ packages/ascii-mp4-video-player-exp/ascii_mp4_video_player_exp-0.1.tar.gz/ascii_mp4_video_player_exp-0.1/ascii_mp4_video_player_exp/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='ascii_mp4_video_player_exp',
+    version='0.1',
+    author='paul',
+    author_email='paul@kfsoft.info',
+    description='ASCII mp4 video player experimental',
+    packages=['ascii_mp4_video_player_exp'],  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
