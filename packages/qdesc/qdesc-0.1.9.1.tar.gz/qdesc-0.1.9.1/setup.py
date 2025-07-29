12f371c2afbf641e9ab8afadab9da1224d71ea71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:54 +0000

    Add package qdesc/0.1.9.1/qdesc-0.1.9.1.tar.gz

---
+++ packages/qdesc/qdesc-0.1.9.1.tar.gz/qdesc-0.1.9.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages 
+from pathlib import Path 
+
+# Read the contents of the README file 
+this_directory = Path(__file__).parent 
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='qdesc',
+    version='0.1.9.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here, e.g., pandas if your function requires it
+    ],
+    author='Paolo Hilado',
+    author_email='datasciencepgh@proton.me',
+    description= 'Quick and Easy way to do descriptive analysis.',
+    long_description=long_description, 
+    long_description_content_type='text/markdown', # or 'text/x-rst' for reStructuredText # other metadata fields... )
+)
