a118668eaef46c95bc6a469c2d4eb4e65aa89b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:08 +0000

    Add package encryptext/1.1/encryptext-1.1.tar.gz

---
+++ packages/encryptext/encryptext-1.1.tar.gz/encryptext-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="encryptext",
+      version='1.1',
+      packages=['uCipher'],
+      install_requires=['LibraryBasics>=5.0'],
+      author="YusufA442",
+      author_email="yusuf365820@gmail.com",
+      license='Creative Commons Attribution-Noncommercial-Share Alike license',
+      long_description=open('readme.txt').read())
