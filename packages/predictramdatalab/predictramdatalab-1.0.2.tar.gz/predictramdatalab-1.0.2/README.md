815a535f66a86ef2f870880994be760bb415a8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:16 +0000

    Add package predictramdatalab/1.0.2/predictramdatalab-1.0.2.tar.gz

---
+++ packages/predictramdatalab/predictramdatalab-1.0.2.tar.gz/predictramdatalab-1.0.2/README.md
@@ -0,0 +1,8 @@
+# Predictram DataLab Stock API
+
+A Python package for accessing and analyzing stock data.
+
+## Installation
+
+```bash
+pip install predictramdatalab
---
+++ packages/predictramdatalab/predictramdatalab-1.0.2.tar.gz/predictramdatalab-1.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="predictramdatalab",
+    version="1.0.2",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "predictramdatalab": ["data/*.json"],
+    },
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "openpyxl",  # For Excel export
+    ],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A Python API for accessing and analyzing stock data",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/predictramdatalab",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
