97c473ad7575b55d67b45f38e2b339fd023c9573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:12 +0000

    Add package openalgo/1.0.13/openalgo-1.0.13-py3-none-any.whl

---
+++ packages/openalgo/openalgo-1.0.13-py3-none-any.whl/openalgo/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+OpenAlgo Python Library
+"""
+
+from .orders import OrderAPI
+from .data import DataAPI
+from .account import AccountAPI
+from .strategy import Strategy
+from .feed import FeedAPI
+
+class api(OrderAPI, DataAPI, AccountAPI, FeedAPI):
+    """
+    OpenAlgo API client class
+    """
+    pass
+
+__version__ = "1.0.13"
