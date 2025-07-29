65471043c6d0408c992270b279af9a387fa7c9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:13 +0000

    Add package mseep-mcp-multilspy/0.1.2/mseep_mcp_multilspy-0.1.2.tar.gz

---
+++ packages/mseep-mcp-multilspy/mseep_mcp_multilspy-0.1.2.tar.gz/mseep_mcp_multilspy-0.1.2/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-mcp-multilspy",
+    version="0.1.0",
+    description="MCP server that exposes Language Server Protocol functionality via multilspy",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp[cli]>=1.4.1', 'multilspy>=0.0.14'],
+    keywords=["mseep"] + [],
+)
