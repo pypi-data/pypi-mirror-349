523761831c2d6fc462119828755b3abfbaa58ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:48 +0000

    Add package pylecular/0.1.5/pylecular-0.1.5.tar.gz

---
+++ packages/pylecular/pylecular-0.1.5.tar.gz/pylecular-0.1.5/pylecular/settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    def __init__(self, transporter="nats://localhost:4222", serializer="JSON", log_level="INFO", log_format="PLAIN"):
+        self.transporter = transporter
+        self.serializer = serializer
+        self.log_level = log_level
+        self.log_format = log_format
