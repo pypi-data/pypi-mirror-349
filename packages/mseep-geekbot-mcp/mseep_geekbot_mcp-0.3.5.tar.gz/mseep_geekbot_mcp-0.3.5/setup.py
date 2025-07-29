a899d34476ebb66a81481e6c575277bdb0e7f46f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:18 +0000

    Add package mseep-geekbot-mcp/0.3.5/mseep_geekbot_mcp-0.3.5.tar.gz

---
+++ packages/mseep-geekbot-mcp/mseep_geekbot_mcp-0.3.5.tar.gz/mseep_geekbot_mcp-0.3.5/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-geekbot-mcp",
+    version="0.3.4",
+    description="Model Context Protocol (MCP) server integrating Geekbot data and tools to Claude AI",
+    author="mseep",
+    author_email="support@skydeck.ai",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['aiohttp>=3.11.14', 'jinja2>=3.1.6', 'mcp[cli]>=1.5.0', 'python-dotenv>=1.1.0'],
+    keywords=["mseep"] + ['geekbot', 'claude', 'ai', 'llm', 'mcp', 'api', 'integration', 'chatbot', 'anthropic'],
+)
