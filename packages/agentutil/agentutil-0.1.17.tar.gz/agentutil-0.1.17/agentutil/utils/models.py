8905739dc9d2f63ff36320b765b769146b3fe0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:29 +0000

    Add package agentutil/0.1.17/agentutil-0.1.17.tar.gz

---
+++ packages/agentutil/agentutil-0.1.17.tar.gz/agentutil-0.1.17/agentutil/utils/models.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel, model_validator
+from enum import Enum
+from sqlalchemy import Column, Integer, String, Enum as SAEnum
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class NewsStatus(str, Enum):
+    PUBLISHED = "published"
+    NEW = "new"
+    FAILED = "failed"
+
+
+Base = declarative_base()
+
+class NewsORM(Base):
+    __tablename__ = "news"
+
+    id = Column(String, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=False)
+    summary = Column(String, default="")
+    content = Column(String, default="")
+    status = Column(SAEnum(NewsStatus), default=NewsStatus.NEW, nullable=False)
+
+class News(BaseModel):
+    title: str
+    summary: str = ""
+    content: str = ""
+    status: NewsStatus = NewsStatus.NEW
+
+    @model_validator(mode='before')
+    @classmethod
+    def remove_non_bmp(cls, values):
+        for field in ['title', 'summary', 'content']:
+            if field in values:
+                values[field] = ''.join(
+                    c for c in values[field] if ord(c) <= 0xFFFF
+                )
+        return values
+
+    class Config:
+        from_attributes = True
