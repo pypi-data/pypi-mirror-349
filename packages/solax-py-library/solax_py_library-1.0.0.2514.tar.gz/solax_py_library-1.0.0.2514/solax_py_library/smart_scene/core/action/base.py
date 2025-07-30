c657ffb3c05849e0b72ab2484ff9d98bffc6d720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:41 +0000

    Add package solax-py-library/1.0.0.2514/solax_py_library-1.0.0.2514.tar.gz

---
+++ packages/solax-py-library/solax_py_library-1.0.0.2514.tar.gz/solax_py_library-1.0.0.2514/solax_py_library/smart_scene/core/action/base.py
@@ -0,0 +1,9 @@
+# class BaseAction(object):
+#     def __init__(self, smart_scene_service):
+#         self.smart_scene_service = smart_scene_service
+#         self.device_service = DeviceService()
+#         self.ems_tcp_service = EMSTcpService()
+#
+#
+#
+#
