01c39f85addcde35ec429cf239dfb65b7ff285bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:32 +0000

    Add package tdsbrondata/0.3.0/tdsbrondata-0.3.0.tar.gz

---
+++ packages/tdsbrondata/tdsbrondata-0.3.0.tar.gz/tdsbrondata-0.3.0/src/tdsbrondata/check.py
@@ -0,0 +1,22 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import tdsbrondata
+from pyspark.sql import functions as F
+from pyspark.sql.types import *
+
+def getCurrentData(workspaceName, lakehouseName, schemaName, tableName, usesScd):
+    
+    abfss = f"abfss://{workspaceName}@onelake.dfs.fabric.microsoft.com/{lakehouseName}.Lakehouse/Tables/{schemaName}/{tableName}"
+
+    data = tdsbrondata._spark.read.format("delta").load(abfss)
+    
+    if(usesScd):
+        data = data.filter(
+            (F.col("CurrentFlag") == 1) & 
+            (F.col("ScdEndDate").isNull())
+        )
+
+    return data
