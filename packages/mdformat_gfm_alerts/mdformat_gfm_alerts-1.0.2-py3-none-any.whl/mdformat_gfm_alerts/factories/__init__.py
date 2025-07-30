bd4fe6fbdf4222fc4b718d10a1690d4270d10bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:23 +0000

    Add package mdformat_gfm_alerts/1.0.2/mdformat_gfm_alerts-1.0.2-py3-none-any.whl

---
+++ packages/mdformat_gfm_alerts/mdformat_gfm_alerts-1.0.2-py3-none-any.whl/mdformat_gfm_alerts/factories/__init__.py
@@ -0,0 +1,13 @@
+from ._gfm_blockquote_factories import (
+    AlertData,
+    gfm_alerts_plugin_factory,
+    new_token,
+    parse_possible_blockquote_admon_factory,
+)
+
+__all__ = (
+    "AlertData",
+    "gfm_alerts_plugin_factory",
+    "new_token",
+    "parse_possible_blockquote_admon_factory",
+)
