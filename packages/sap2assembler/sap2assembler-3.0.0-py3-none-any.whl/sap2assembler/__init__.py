92afb035f9ca948676f2751a764ca58e9d0000b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:42 +0000

    Add package sap2assembler/3.0.0/sap2assembler-3.0.0-py3-none-any.whl

---
+++ packages/sap2assembler/sap2assembler-3.0.0-py3-none-any.whl/sap2assembler/__init__.py
@@ -0,0 +1,7 @@
+from .assembler import SAP2Assembler
+from .__main__ import main
+
+__version__ = "3.0.0"
+__author__ = "Samarth Javagal"
+__author_info__ = "literally not human"
+__version__info = "improved bugs"
