ea47b78a8c03ca2fd29ae07ed393c6c368efdf61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:56 +0000

    Add package mapbiomas-downloader/2.2.0/mapbiomas_downloader-2.2.0-py3-none-any.whl

---
+++ packages/mapbiomas-downloader/mapbiomas_downloader-2.2.0-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,10 @@
+"""
+MapBiomas Downloader - Pacote principal.
+
+Autor: Ricardo Malnati
+"""
+
+# Sem importações aqui para evitar o warning de ordem de carregamento
+# As importações são feitas diretamente nos módulos que precisam das funções
+
+__all__ = ["verificar_cd_mun", "extrair_cd_mun", "baixar_e_recortar_por_shapefile"] 
