13dfe379d172598d60b447af16d032f8dc2622d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:24 +0000

    Add package pangea-sdk/6.2.0b2/pangea_sdk-6.2.0b2.tar.gz

---
+++ packages/pangea-sdk/pangea_sdk-6.2.0b2.tar.gz/pangea_sdk-6.2.0b2/pangea/asyncio/services/ai_guard.py
@@ -0,0 +1,261 @@
+from __future__ import annotations
+
+from collections.abc import Mapping
+from typing import overload
+
+from typing_extensions import Literal, TypeVar
+
+from pangea.asyncio.services.base import ServiceBaseAsync
+from pangea.config import PangeaConfig
+from pangea.response import PangeaResponse
+from pangea.services.ai_guard import (
+    AuditDataActivityConfig,
+    ConnectionsConfig,
+    LogFields,
+    Overrides,
+    RecipeConfig,
+    ServiceConfig,
+    ServiceConfigFilter,
+    ServiceConfigsPage,
+    TextGuardResult,
+)
+
+_T = TypeVar("_T")
+
+
+class AIGuardAsync(ServiceBaseAsync):
+    """AI Guard service client.
+
+    Provides methods to interact with Pangea's AI Guard service.
+
+    Examples:
+        from pangea import PangeaConfig
+        from pangea.asyncio.services import AIGuardAsync
+
+        config = PangeaConfig(domain="aws.us.pangea.cloud")
+        ai_guard = AIGuardAsync(token="pangea_token", config=config)
+    """
+
+    service_name = "ai-guard"
+
+    def __init__(
+        self, token: str, config: PangeaConfig | None = None, logger_name: str = "pangea", config_id: str | None = None
+    ) -> None:
+        """
+        AI Guard service client.
+
+        Initializes a new AI Guard client.
+
+        Args:
+            token: Pangea API token.
+            config: Pangea service configuration.
+            logger_name: Logger name.
+            config_id: Configuration ID.
+
+        Examples:
+            from pangea import PangeaConfig
+            from pangea.asyncio.services import AIGuardAsync
+
+            config = PangeaConfig(domain="aws.us.pangea.cloud")
+            ai_guard = AIGuardAsync(token="pangea_token", config=config)
+        """
+
+        super().__init__(token, config, logger_name, config_id)
+
+    @overload
+    async def guard_text(
+        self,
+        text: str,
+        *,
+        recipe: str | None = None,
+        debug: bool | None = None,
+        overrides: Overrides | None = None,
+        log_fields: LogFields | None = None,
+    ) -> PangeaResponse[TextGuardResult[None]]:
+        """
+        Text Guard for scanning LLM inputs and outputs
+
+        Analyze and redact text to avoid manipulation of the model, addition of
+        malicious content, and other undesirable data transfers.
+
+        OperationId: ai_guard_post_v1_text_guard
+
+        Args:
+            text: Text to be scanned by AI Guard for PII, sensitive data,
+                malicious content, and other data types defined by the
+                configuration. Supports processing up to 10KB of text.
+            recipe: Recipe key of a configuration of data types and settings
+                defined in the Pangea User Console. It specifies the rules that
+                are to be applied to the text, such as defang malicious URLs.
+            debug: Setting this value to true will provide a detailed analysis
+                of the text data
+            log_field: Additional fields to include in activity log
+
+        Examples:
+            response = await ai_guard.guard_text("text")
+        """
+
+    @overload
+    async def guard_text(
+        self,
+        *,
+        messages: _T,
+        recipe: str | None = None,
+        debug: bool | None = None,
+        overrides: Overrides | None = None,
+        log_fields: LogFields | None = None,
+    ) -> PangeaResponse[TextGuardResult[_T]]:
+        """
+        Text Guard for scanning LLM inputs and outputs
+
+        Analyze and redact text to avoid manipulation of the model, addition of
+        malicious content, and other undesirable data transfers.
+
+        OperationId: ai_guard_post_v1_text_guard
+
+        Args:
+            messages: Structured messages data to be scanned by AI Guard for
+                PII, sensitive data, malicious content, and other data types
+                defined by the configuration. Supports processing up to 10KB of
+                JSON text
+            recipe: Recipe key of a configuration of data types and settings
+                defined in the Pangea User Console. It specifies the rules that
+                are to be applied to the text, such as defang malicious URLs.
+            debug: Setting this value to true will provide a detailed analysis
+                of the text data
+            log_field: Additional fields to include in activity log
+
+        Examples:
+            response = await ai_guard.guard_text(messages=[{"role": "user", "content": "hello world"}])
+        """
+
+    async def guard_text(  # type: ignore[misc]
+        self,
+        text: str | None = None,
+        *,
+        messages: _T | None = None,
+        recipe: str | None = None,
+        debug: bool | None = None,
+        overrides: Overrides | None = None,
+        log_fields: LogFields | None = None,
+    ) -> PangeaResponse[TextGuardResult[None]]:
+        """
+        Text Guard for scanning LLM inputs and outputs
+
+        Analyze and redact text to avoid manipulation of the model, addition of
+        malicious content, and other undesirable data transfers.
+
+        OperationId: ai_guard_post_v1_text_guard
+
+        Args:
+            text: Text to be scanned by AI Guard for PII, sensitive data,
+                malicious content, and other data types defined by the
+                configuration. Supports processing up to 10KB of text.
+            messages: Structured messages data to be scanned by AI Guard for
+                PII, sensitive data, malicious content, and other data types
+                defined by the configuration. Supports processing up to 10KB of
+                JSON text
+            recipe: Recipe key of a configuration of data types and settings
+                defined in the Pangea User Console. It specifies the rules that
+                are to be applied to the text, such as defang malicious URLs.
+            debug: Setting this value to true will provide a detailed analysis
+                of the text data
+            log_field: Additional fields to include in activity log
+
+        Examples:
+            response = await ai_guard.guard_text("text")
+        """
+
+        if text is not None and messages is not None:
+            raise ValueError("Exactly one of `text` or `messages` must be given")
+
+        return await self.request.post(
+            "v1/text/guard",
+            TextGuardResult,
+            data={
+                "text": text,
+                "messages": messages,
+                "recipe": recipe,
+                "debug": debug,
+                "overrides": overrides,
+                "log_fields": log_fields,
+            },
+        )
+
+    async def get_service_config(self, id: str) -> PangeaResponse[ServiceConfig]:
+        """
+        OperationId: ai_guard_post_v1beta_config
+        """
+        return await self.request.post("v1beta/config", data={"id": id}, result_class=ServiceConfig)
+
+    async def create_service_config(
+        self,
+        name: str,
+        *,
+        id: str | None = None,
+        audit_data_activity: AuditDataActivityConfig | None = None,
+        connections: ConnectionsConfig | None = None,
+        recipes: Mapping[str, RecipeConfig] | None = None,
+    ) -> PangeaResponse[ServiceConfig]:
+        """
+        OperationId: ai_guard_post_v1beta_config_create
+        """
+        return await self.request.post(
+            "v1beta/config/create",
+            data={
+                "name": name,
+                "id": id,
+                "audit_data_activity": audit_data_activity,
+                "connections": connections,
+                "recipes": recipes,
+            },
+            result_class=ServiceConfig,
+        )
+
+    async def update_service_config(
+        self,
+        id: str,
+        name: str,
+        *,
+        audit_data_activity: AuditDataActivityConfig | None = None,
+        connections: ConnectionsConfig | None = None,
+        recipes: Mapping[str, RecipeConfig] | None = None,
+    ) -> PangeaResponse[ServiceConfig]:
+        """
+        OperationId: ai_guard_post_v1beta_config_update
+        """
+        return await self.request.post(
+            "v1beta/config/update",
+            data={
+                "id": id,
+                "name": name,
+                "audit_data_activity": audit_data_activity,
+                "connections": connections,
+                "recipes": recipes,
+            },
+            result_class=ServiceConfig,
+        )
+
+    async def delete_service_config(self, id: str) -> PangeaResponse[ServiceConfig]:
+        """
+        OperationId: ai_guard_post_v1beta_config_delete
+        """
+        return await self.request.post("v1beta/config/delete", data={"id": id}, result_class=ServiceConfig)
+
+    async def list_service_configs(
+        self,
+        *,
+        filter: ServiceConfigFilter | None = None,
+        last: str | None = None,
+        order: Literal["asc", "desc"] | None = None,
+        order_by: Literal["id", "created_at", "updated_at"] | None = None,
+        size: int | None = None,
+    ) -> PangeaResponse[ServiceConfigsPage]:
+        """
+        OperationId: ai_guard_post_v1beta_config_list
+        """
+        return await self.request.post(
+            "v1beta/config/list",
+            data={"filter": filter, "last": last, "order": order, "order_by": order_by, "size": size},
+            result_class=ServiceConfigsPage,
+        )
