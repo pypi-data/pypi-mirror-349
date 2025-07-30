11cc2abaef9f1c6dd3539c1490ba5093a9684054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:15 +0000

    Add package kg-gen/0.2.0/kg_gen-0.2.0.tar.gz

---
+++ packages/kg-gen/kg_gen-0.2.0.tar.gz/kg_gen-0.2.0/tests/test_basic.py
@@ -0,0 +1,24 @@
+from src.kg_gen import KGGen
+import os
+from dotenv import load_dotenv
+
+from tests.utils.visualize_kg import visualize
+
+if __name__ == "__main__":
+  # Load environment variables
+  load_dotenv()
+  
+  # Example usage
+  kg = KGGen(api_key=os.getenv("OPENAI_API_KEY"))
+  
+  # Generate a simple graph
+  text = "Harry has two parents - his dad James Potter and his mom Lily Potter. Harry and his wife Ginny have three kids together: their oldest son James Sirius, their other son Albus, and their daughter Lily Luna."
+  
+  graph = kg.generate(
+    input_data=text,
+    model="openai/gpt-4o"
+  )
+
+  visualize(graph, "tests/test_basic.html")
+
+  print(graph)
