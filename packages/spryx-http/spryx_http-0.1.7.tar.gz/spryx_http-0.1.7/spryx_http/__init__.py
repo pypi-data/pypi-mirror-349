16320324dddf8c25432eb24e6c1cbf588432b219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:40 +0000

    Add package spryx-http/0.1.7/spryx_http-0.1.7.tar.gz

---
+++ packages/spryx-http/spryx_http-0.1.7.tar.gz/spryx_http-0.1.7/spryx_http/__init__.py
@@ -0,0 +1,33 @@
+__version__ = "0.1.1"
+
+from spryx_http.auth import AuthStrategy, HmacAuth, JwtAuth, NoAuth
+from spryx_http.base import SpryxAsyncClient, T
+from spryx_http.exceptions import (
+    AuthenticationError,
+    ClientError,
+    ForbiddenError,
+    HttpError,
+    NotFoundError,
+    RateLimitError,
+    ServerError,
+    raise_for_status,
+)
+from spryx_http.pagination import AsyncPaginator
+
+__all__ = [
+    "SpryxAsyncClient",
+    "T",  # TypeVar for generic type hints
+    "AuthStrategy",
+    "JwtAuth",
+    "HmacAuth",
+    "NoAuth",
+    "HttpError",
+    "ClientError",
+    "ServerError",
+    "RateLimitError",
+    "AuthenticationError",
+    "ForbiddenError",
+    "NotFoundError",
+    "raise_for_status",
+    "AsyncPaginator",
+]
