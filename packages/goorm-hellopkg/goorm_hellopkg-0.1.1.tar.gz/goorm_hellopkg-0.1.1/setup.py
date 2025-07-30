535b4406ff92cae002e7965098e6dde77d4613b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:09:05 +0000

    Add package goorm-hellopkg/0.1.1/goorm_hellopkg-0.1.1.tar.gz

---
+++ packages/goorm-hellopkg/goorm_hellopkg-0.1.1.tar.gz/goorm_hellopkg-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="goorm_hellopkg",
+    version="0.1.1",
+    description="A simple Hello World library",
+    author="Your Name",
+    packages=find_packages(),
+    install_requires=[],
+)
