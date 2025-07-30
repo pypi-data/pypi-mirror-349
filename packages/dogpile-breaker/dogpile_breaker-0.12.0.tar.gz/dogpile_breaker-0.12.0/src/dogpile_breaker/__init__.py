27c5f398453cb6191752c2f48a86dd7f8c8e07ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:06 +0000

    Add package dogpile-breaker/0.12.0/dogpile_breaker-0.12.0.tar.gz

---
+++ packages/dogpile-breaker/dogpile_breaker-0.12.0.tar.gz/dogpile_breaker-0.12.0/src/dogpile_breaker/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "0.12.0"
+
+from .api import CacheRegion, ShouldCacheFunc, StorageBackend
+from .backends.memory_backend import MemoryBackendLRU
+from .backends.redis_backend import RedisSentinelBackend, RedisStorageBackend
+from .middlewares.base_middleware import StorageBackendMiddleware
+from .middlewares.circut_breaker_fallback_middleware import CircuitBreakerFallbackMiddleware
+from .middlewares.prometheus_middleware import PrometheusMiddleware
+
+__all__ = [
+    "CacheRegion",
+    "CircuitBreakerFallbackMiddleware",
+    "MemoryBackendLRU",
+    "PrometheusMiddleware",
+    "RedisSentinelBackend",
+    "RedisStorageBackend",
+    "ShouldCacheFunc",
+    "StorageBackend",
+    "StorageBackendMiddleware",
+]
