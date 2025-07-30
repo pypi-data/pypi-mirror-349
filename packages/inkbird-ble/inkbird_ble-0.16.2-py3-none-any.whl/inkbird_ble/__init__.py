2876b66bdef9bd7a767ef6e39a1a9f6437ce7c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:16:31 +0000

    Add package inkbird-ble/0.16.2/inkbird_ble-0.16.2-py3-none-any.whl

---
+++ packages/inkbird-ble/inkbird_ble-0.16.2-py3-none-any.whl/inkbird_ble/__init__.py
@@ -0,0 +1,29 @@
+"""Parser for Inkbird BLE advertisements."""
+
+from __future__ import annotations
+
+from sensor_state_data import (
+    DeviceClass,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .parser import INKBIRDBluetoothDeviceData
+
+__version__ = "0.16.2"
+
+__all__ = [
+    "DeviceClass",
+    "DeviceKey",
+    "INKBIRDBluetoothDeviceData",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "SensorDeviceInfo",
+    "SensorUpdate",
+    "SensorValue",
+    "Units",
+]
