839f292a4de1b7cae5fa6b9f1ce5c2b26c6f87fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:19 +0000

    Add package abs-integration-core/0.1.2/abs_integration_core-0.1.2.tar.gz

---
+++ packages/abs-integration-core/abs_integration_core-0.1.2.tar.gz/abs_integration_core-0.1.2/abs_integration_core/service/__init__.py
@@ -0,0 +1,4 @@
+from .sql_integration_service import IntegrationBaseService
+from .base_service import AbstractIntegrationBaseService
+
+__all__ = ["IntegrationBaseService", "AbstractIntegrationBaseService"]
