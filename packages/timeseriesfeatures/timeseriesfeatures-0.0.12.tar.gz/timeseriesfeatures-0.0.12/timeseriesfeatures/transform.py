93e4463cc97080677f8665fd109fe9d40449ca51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:09 +0000

    Add package timeseriesfeatures/0.0.12/timeseriesfeatures-0.0.12.tar.gz

---
+++ packages/timeseriesfeatures/timeseriesfeatures-0.0.12.tar.gz/timeseriesfeatures-0.0.12/timeseriesfeatures/transform.py
@@ -0,0 +1,12 @@
+"""Transform enumeration."""
+
+from enum import StrEnum, auto
+
+
+class Transform(StrEnum):
+    """Transform enumeration."""
+
+    NONE = auto()
+    VELOCITY = auto()
+    LOG = auto()
+    ACCELERATION = auto()
