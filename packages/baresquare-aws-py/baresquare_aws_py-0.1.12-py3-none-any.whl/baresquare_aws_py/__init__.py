509a9ab5f7ffd33483b2bf5386121aeb0a66d173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:34 +0000

    Add package baresquare-aws-py/0.1.12/baresquare_aws_py-0.1.12-py3-none-any.whl

---
+++ packages/baresquare-aws-py/baresquare_aws_py-0.1.12-py3-none-any.whl/baresquare_aws_py/__init__.py
@@ -0,0 +1,8 @@
+"""Baresquare AWS Python utilities.
+
+This package provides AWS-specific utilities for Baresquare services.
+"""
+
+from . import authentication
+from . import s3
+from . import ssm
