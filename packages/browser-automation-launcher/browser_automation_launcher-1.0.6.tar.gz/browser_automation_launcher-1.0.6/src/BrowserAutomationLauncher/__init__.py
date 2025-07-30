abce353b2bf87738b02b6cbcd8a011c49e8deb6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:36 +0000

    Add package browser-automation-launcher/1.0.6/browser_automation_launcher-1.0.6.tar.gz

---
+++ packages/browser-automation-launcher/browser_automation_launcher-1.0.6.tar.gz/browser_automation_launcher-1.0.6/src/BrowserAutomationLauncher/__init__.py
@@ -0,0 +1,5 @@
+from ._browser import BrowserInitOptions
+from ._utils.data_packet import DataPacketProcessor
+from .launcher import Launcher
+
+__all__ = ['Launcher', 'BrowserInitOptions', 'DataPacketProcessor']
