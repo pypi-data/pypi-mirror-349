5b5ef420c6f7f83aeed96f2f8e6b945f98c3b5b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:49 +0000

    Add package dg-ignition-selenium/0.1.3/dg_ignition_selenium-0.1.3.tar.gz

---
+++ packages/dg-ignition-selenium/dg_ignition_selenium-0.1.3.tar.gz/dg_ignition_selenium-0.1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dg-ignition-selenium",
+    version="0.1.3",
+    packages=find_packages(
+        where="src",
+        include=["ignition_automation_tools", "ignition_automation_tools.*"],
+    ),
+    package_dir={"": "src"},
+    install_requires=[
+        "selenium>=4.0.0",
+    ],
+    python_requires=">=3.8",
+)
