c4f36088b082aebbe42973f56c146c713a6ec304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:50 +0000

    Add package bcf-api-xml/0.4.18/bcf_api_xml-0.4.18.tar.gz

---
+++ packages/bcf-api-xml/bcf_api_xml-0.4.18.tar.gz/bcf_api_xml-0.4.18/bcf_api_xml/__init__.py
@@ -0,0 +1,7 @@
+from bcf_api_xml.errors import InvalidBCF  # noqa: F401
+from bcf_api_xml.errors import UnsupportedBCFVersion  # noqa: F401
+from bcf_api_xml.export import to_xlsx  # noqa: F401
+from bcf_api_xml.export import to_zip  # noqa: F401
+from bcf_api_xml.import_zip import to_json  # noqa: F401
+
+__version__ = "0.4.18"
