f6db7625969a360e215734c648476933ad870624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:05 +0000

    Add package zillion/0.13.1/zillion-0.13.1-py3-none-any.whl

---
+++ packages/zillion/zillion-0.13.1-py3-none-any.whl/zillion-0.13.1.data/scripts/json_to_yaml.py
@@ -0,0 +1,5 @@
+#!python
+
+import sys, yaml, json
+
+print(yaml.dump(json.loads(sys.stdin.read()), indent=2, sort_keys=False))
---
+++ packages/zillion/zillion-0.13.1-py3-none-any.whl/zillion-0.13.1.data/scripts/yaml_to_json.py
@@ -0,0 +1,5 @@
+#!python
+
+import sys, yaml, json
+
+print(json.dumps(yaml.safe_load(sys.stdin.read()), indent=4))
