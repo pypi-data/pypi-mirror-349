e6c79a51bc031f874835004094811f24d7044949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:04 +0000

    Add package math-smpi-sdk/0.1.0/math_smpi_sdk-0.1.0.tar.gz

---
+++ packages/math-smpi-sdk/math_smpi_sdk-0.1.0.tar.gz/math_smpi_sdk-0.1.0/README.md
@@ -0,0 +1,10 @@
+# Math SDK
+
+A simple Python SDK for basic math operations: addition, subtraction, multiplication, and division.
+
+## 📦 Installation
+
+You can install the package using `pip` (after it's published on PyPI):
+
+```bash
+pip install math-sdk
---
+++ packages/math-smpi-sdk/math_smpi_sdk-0.1.0.tar.gz/math_smpi_sdk-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math-smpi-sdk",
+    version="0.1.0",
+    description="A simple SDK for basic math operations.",
+    author="Shubham Kanzariya",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+)
