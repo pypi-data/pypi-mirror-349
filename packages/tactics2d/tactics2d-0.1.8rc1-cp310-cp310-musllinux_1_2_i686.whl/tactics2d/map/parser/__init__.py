ffcd39f0533cf85acba46c59fc44c6b658e75600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:49 +0000

    Add package tactics2d/0.1.8rc1/tactics2d-0.1.8rc1-cp310-cp310-musllinux_1_2_i686.whl

---
+++ packages/tactics2d/tactics2d-0.1.8rc1-cp310-cp310-musllinux_1_2_i686.whl/tactics2d/map/parser/__init__.py
@@ -0,0 +1,13 @@
+##! python3
+# Copyright (C) 2024, Tactics2D Authors. Released under the GNU GPLv3.
+# @File: __init__.py
+# @Description: Initialize the map parser module.
+# @Author: Yueyuan Li
+# @Version: 1.0.0
+
+
+from .parse_gis import GISParser
+from .parse_osm import OSMParser
+from .parse_xodr import XODRParser
+
+__all__ = ["GISParser", "OSMParser", "XODRParser"]
