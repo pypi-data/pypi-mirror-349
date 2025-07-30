b7c5fd6dd62d883c8d40cdbf10ade4d6cb5c9285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:27 +0000

    Add package contacted/0.0.1/contacted-0.0.1.tar.gz

---
+++ packages/contacted/contacted-0.0.1.tar.gz/contacted-0.0.1/README.md
@@ -0,0 +1,20 @@
+# ContactedAI Python SDK
+
+**⚠️ This package is currently under development and not yet functional.**
+
+This package is reserved for the official ContactedAI Python SDK.
+
+## Coming Soon
+
+- Full API integration
+- Easy email sending
+- Template management
+- Complete documentation
+
+## Stay Updated
+
+Visit [contacted.io](https://contacted.io) for updates on the SDK release.
+
+## Contact
+
+For questions about the upcoming SDK, please contact support through our website.
---
+++ packages/contacted/contacted-0.0.1.tar.gz/contacted-0.0.1/contacted/__init__.py
@@ -0,0 +1,17 @@
+"""
+ContactedAI Python SDK
+A Python library for the ContactedAI email API service.
+"""
+
+__version__ = "0.0.1"
+__author__ = "ContactedAI"
+__email__ = "support@contacted.io"
+
+class ContactedAI:
+    """
+    ContactedAI client class.
+    This is a placeholder implementation for package name reservation.
+    """
+    def __init__(self, api_key=None):
+        self.api_key = api_key
+        raise NotImplementedError("This package is under development. Full implementation coming soon.")
