8da8c81ce89894d98a611e0694f9c262fda9fa3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:17:10 +0000

    Add package streetmanager/0.1.2/streetmanager-0.1.2-py3-none-any.whl

---
+++ packages/streetmanager/streetmanager-0.1.2-py3-none-any.whl/streetmanager/__init__.py
@@ -0,0 +1,11 @@
+"""
+Street Manager - A tool for managing street data
+"""
+
+__version__ = "0.1.2"
+
+# Import main modules to make them available at the package level
+from . import work
+from . import geojson
+from . import lookup
+
