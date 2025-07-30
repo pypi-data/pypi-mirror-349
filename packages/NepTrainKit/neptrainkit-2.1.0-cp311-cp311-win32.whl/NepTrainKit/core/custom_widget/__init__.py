23581e613a62098590e95b2a5bcba95649ee24eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:40 +0000

    Add package NepTrainKit/2.1.0/neptrainkit-2.1.0-cp311-cp311-win32.whl

---
+++ packages/NepTrainKit/neptrainkit-2.1.0-cp311-cp311-win32.whl/NepTrainKit/core/custom_widget/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2024/11/28 22:45
+# @Author  : 兵
+# @email    : 1747193328@qq.com
+from .layout import *
+from .label import *
+from .completer import *
+from .dialog import *
+from .input import *
+from .card_widget import *
+from .docker import *
+from .search_widget import *
+from .settingscard import *
