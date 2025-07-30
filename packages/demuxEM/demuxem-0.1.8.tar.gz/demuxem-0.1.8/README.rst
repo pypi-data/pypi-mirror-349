2d9dbb7bd03061482b7ed0793602534fe75ce996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:19 +0000

    Add package demuxEM/0.1.8/demuxem-0.1.8.tar.gz

---
+++ packages/demuxEM/demuxem-0.1.8.tar.gz/demuxem-0.1.8/README.rst
@@ -0,0 +1,24 @@
+==================================================================
+demuxEM for demultiplexing cell/nucleus hashing single-cell data
+==================================================================
+
+|PyPI| |Conda| |Python| |License| |Docs|
+
+.. |PyPI| image:: https://img.shields.io/pypi/v/demuxEM.svg
+   :target: https://pypi.org/project/demuxEM
+
+.. |Conda| image:: https://img.shields.io/conda/v/bioconda/demuxem
+   :target: https://anaconda.org/bioconda/demuxem
+
+.. |Python| image:: https://img.shields.io/pypi/pyversions/demuxEM.svg
+   :target: https://pypi.org/project/demuxEM
+
+.. |Docs| image:: https://readthedocs.org/projects/demuxem/badge/?version=latest
+   :target: https://demuxEM.readthedocs.io
+
+.. |License| image:: https://img.shields.io/github/license/lilab-bcb/demuxEM
+   :target: https://github.com/lilab-bcb/demuxEM/blob/master/LICENSE
+
+demuxEM is the demultiplexing module of Pegasus.
+
+`Read documentation <https://demuxEM.readthedocs.io>`__
---
+++ packages/demuxEM/demuxem-0.1.8.tar.gz/demuxem-0.1.8/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+from codecs import open
+import os
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+requires = [
+    "docopt",
+    "numpy",
+    "pandas>=1.5.0",
+    "scipy",
+    "scikit-learn",
+    "seaborn",
+    "pegasusio>=0.10.0",
+]
+
+setup(
+    name="demuxEM",
+    use_scm_version=True,
+    zip_safe=False,
+    description="demuxEM is the demultiplexing module of Pegasus",
+    long_description=long_description,
+    url="https://github.com/lilab-bcb/demuxEM",
+    author="Yiming Yang, Asma Bankapur, Joshua Gould and Bo Li",
+    author_email="cumulus-support@googlegroups.com",
+    classifiers=[  # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 4 - Beta",
+        "Framework :: Jupyter",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    keywords="demultiplexing cell/nucleus hashing single-cell data",
+    packages=find_packages(),
+    install_requires=requires,
+    setup_requires = ["setuptools_scm"],
+    python_requires="~=3.8",
+    entry_points={"console_scripts": ["demuxEM=demuxEM.__main__:main"]},
+)
