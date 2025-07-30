ca011f926df293ddf8bdf40ad80b5dec521961ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:28 +0000

    Add package SLACplots/1.0.1/slacplots-1.0.1.tar.gz

---
+++ packages/SLACplots/slacplots-1.0.1.tar.gz/slacplots-1.0.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import setuptools
+
+VER = "1.0.1"
+
+reqs = ["matplotlib",
+        ]
+
+setuptools.setup(
+    name="SLACplots",
+    version=VER,
+    author="Daniel Douglas",
+    author_email="dougl215@slac.stanford.edu",
+    description="Plotting utilities for matplotlib in the SLAC style",
+    url="https://github.com/DanielMDouglas/SLACplots",
+    packages=setuptools.find_packages(),
+    install_requires=reqs,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Physics"
+    ],
+    python_requires='>=3.2',
+)
