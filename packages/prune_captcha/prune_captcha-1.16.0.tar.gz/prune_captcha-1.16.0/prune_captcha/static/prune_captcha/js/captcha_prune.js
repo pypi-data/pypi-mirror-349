9843317d61ec61c8d1af8c6cd7c4e355e9244470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:07:48 +0000

    Add package prune_captcha/1.16.0/prune_captcha-1.16.0.tar.gz

---
+++ packages/prune_captcha/prune_captcha-1.16.0.tar.gz/prune_captcha-1.16.0/prune_captcha/static/prune_captcha/js/captcha_prune.js
@@ -0,0 +1,57 @@
+const puzzlePiece = document.querySelector(".puzzle-piece");
+const puzzleContainer = document.querySelector(".puzzle-container");
+const inputPosX = document.querySelector('input[name="pos_x_answer"]');
+const inputPosY = document.querySelector('input[name="pos_y_answer"]');
+
+inputPosX.parentElement.style.display = "none";
+inputPosY.parentElement.style.display = "none";
+
+let isDragging = false;
+let offsetX = 0;
+let offsetY = 0;
+
+const fillInput = (e = undefined) => {
+    const pieceRect = puzzlePiece.getBoundingClientRect();
+    const containerRect = puzzleContainer.getBoundingClientRect();
+    const pieceWidth = puzzlePiece.offsetWidth;
+    const pieceHeight = puzzlePiece.offsetHeight;
+
+    const clientX = e === undefined ? pieceRect.left : e.clientX;
+    const clientY = e === undefined ? pieceRect.top : e.clientY;
+
+    let x = clientX - containerRect.left - offsetX;
+    let y = clientY - containerRect.top - offsetY;
+
+    x = Math.max(0, Math.min(x, containerRect.width - pieceWidth));
+    y = Math.max(0, Math.min(y, containerRect.height - pieceHeight));
+
+    puzzlePiece.style.left = `${x}px`;
+    puzzlePiece.style.top = `${y}px`;
+
+    inputPosX.value = Math.round(x);
+    inputPosY.value = Math.round(y);
+};
+fillInput();
+
+puzzlePiece.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    document.body.style.userSelect = "none";
+
+    const pieceRect = puzzlePiece.getBoundingClientRect();
+
+    offsetX = e.clientX - pieceRect.left;
+    offsetY = e.clientY - pieceRect.top;
+});
+
+puzzleContainer.addEventListener("mousemove", (e) => {
+    if (isDragging) {
+        fillInput(e);
+    }
+});
+
+puzzleContainer.addEventListener("mouseup", (e) => {
+    if (isDragging) {
+        isDragging = false;
+        document.body.style.userSelect = "auto";
+    }
+});
