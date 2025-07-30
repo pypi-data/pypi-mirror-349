f88c92a2b9ef62486654b3d7877c54b4296f7487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:14 +0000

    Add package addition-numpy-pytorch-package/0.1.3/addition_numpy_pytorch_package-0.1.3.tar.gz

---
+++ packages/addition-numpy-pytorch-package/addition_numpy_pytorch_package-0.1.3.tar.gz/addition_numpy_pytorch_package-0.1.3/src/addition_numpy_pytorch_package/addition.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def add(x: int, y: int) -> int:
+    """Adds two integers together.
+
+    This is a simple example function used to demonstrate how to create
+    and structure a Python package.
+
+    Args:
+        x (int): The first number to add.
+        y (int): The second number to add.
+
+    Returns:
+        int: The sum of x and y.
+    """
+    return np.add(x, y)
