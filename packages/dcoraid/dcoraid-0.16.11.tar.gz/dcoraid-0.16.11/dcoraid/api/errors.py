61137e92efb86a880cd26ac3fe925e1d670d68da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:10:49 +0000

    Add package dcoraid/0.16.11/dcoraid-0.16.11.tar.gz

---
+++ packages/dcoraid/dcoraid-0.16.11.tar.gz/dcoraid-0.16.11/dcoraid/api/errors.py
@@ -0,0 +1,53 @@
+class APIError(BaseException):
+    """General CKANAPI error"""
+    pass
+
+
+class APIAuthorizationError(APIError):
+    """User credentials are invalid"""
+    pass
+
+
+class APIBadGatewayError(APIError):
+    """Cannot connect to DCOR server"""
+    pass
+
+
+class APIBadRequest(APIError):
+    """An API command cannot be found"""
+    pass
+
+
+class APIConflictError(APIError):
+    """Invalid payload to DCOR server"""
+    pass
+
+
+class APIGatewayTimeoutError(APIError):
+    """Timeout due to network connection"""
+    pass
+
+
+class APINotFoundError(APIError):
+    """Requested object not found on DCOR"""
+    pass
+
+
+class APIOutdatedError(APIError):
+    """DCOR-Aid is outdated, the server requests a newer version"""
+    pass
+
+
+class NoAPIKeyError(APIError):
+    """DCOR does not have an API token"""
+    pass
+
+
+class NoS3UploadAvailableError(BaseException):
+    """Used for identifying DCOR servers that don't support direct S3 upload"""
+    pass
+
+
+class S3UploadError(BaseException):
+    """raised when an upload to S3 failed"""
+    pass
