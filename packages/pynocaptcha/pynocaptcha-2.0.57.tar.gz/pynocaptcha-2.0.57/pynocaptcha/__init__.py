9b36087c5e3bfb6c651195ca26350c17d2946c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:55 +0000

    Add package pynocaptcha/2.0.57/pynocaptcha-2.0.57.tar.gz

---
+++ packages/pynocaptcha/pynocaptcha-2.0.57.tar.gz/pynocaptcha-2.0.57/pynocaptcha/__init__.py
@@ -0,0 +1,28 @@
+
+
+from .crackers.akamai import AkamaiV2Cracker
+from .crackers.aws import AwsUniversalCracker
+from .crackers.cloudflare import CloudFlareCracker
+from .crackers.datadome import DatadomeCracker
+from .crackers.discord import DiscordCracker
+from .crackers.hcaptcha import HcaptchaCracker
+from .crackers.incapsula import (IncapsulaRbzidCracker,
+                                 IncapsulaReese84Cracker,
+                                 IncapsulaUtmvcCracker)
+from .crackers.kasada import KasadaCdCracker, KasadaCtCracker
+from .crackers.perimeterx import PerimeterxCracker
+from .crackers.recaptcha import (ReCaptchaAppCracker,
+                                 ReCaptchaEnterpriseCracker,
+                                 ReCaptchaSteamCracker,
+                                 ReCaptchaUniversalCracker)
+from .crackers.shape import ShapeV1Cracker, ShapeV2Cracker
+from .crackers.tls import TlsV1Cracker
+from .crackers.utils import parse_client_hints, create_session
+
+__all__ = [
+    'pynocaptcha', 
+    'CloudFlareCracker', 'IncapsulaReese84Cracker', 'IncapsulaUtmvcCracker', 'IncapsulaRbzidCracker', 'HcaptchaCracker', 
+    'AkamaiV2Cracker', 'ReCaptchaUniversalCracker', 'ReCaptchaEnterpriseCracker', 'ReCaptchaSteamCracker',
+    'TlsV1Cracker', 'DiscordCracker', 'ReCaptchaAppCracker', 'AwsUniversalCracker', 'PerimeterxCracker',
+    'KasadaCtCracker', 'KasadaCdCracker', 'DatadomeCracker', 'ShapeV1Cracker', 'ShapeV2Cracker', 'parse_client_hints', 'create_session'
+]
