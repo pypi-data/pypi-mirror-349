ba663911ef90acc01af407787a9508cd5c11968c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:26 +0000

    Add package turbo-turtle/1.2.4/turbo_turtle-1.2.4.tar.gz

---
+++ packages/turbo-turtle/turbo_turtle-1.2.4.tar.gz/turbo_turtle-1.2.4/turbo_turtle/tests/test_main.py
@@ -0,0 +1,16 @@
+from unittest.mock import patch
+
+import pytest
+
+from turbo_turtle import _main
+from turbo_turtle import _settings
+
+
+def test_print_abaqus_path(capsys):
+    """Test the print-abaqus-path subcommand behavior"""
+
+    # Test printing behavior
+    expected_output = f"{_settings._abaqus_python_parent_abspath}\n"
+    _main._print_abaqus_path_location()
+    returned_output = capsys.readouterr()
+    assert expected_output == returned_output.out
