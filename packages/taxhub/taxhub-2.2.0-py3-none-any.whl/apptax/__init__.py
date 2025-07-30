93127dfe6347b9ebe912ecc83e7750abb99023b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:48 +0000

    Add package taxhub/2.2.0/taxhub-2.2.0-py3-none-any.whl

---
+++ packages/taxhub/taxhub-2.2.0-py3-none-any.whl/apptax/__init__.py
@@ -0,0 +1,8 @@
+taxhub_api_routes = [
+    ("apptax.taxonomie.routesbibnoms:adresses", "/bibnoms"),
+    ("apptax.taxonomie.routestaxref:adresses", "/taxref"),
+    ("apptax.taxonomie.routesbibattributs:adresses", "/bibattributs"),
+    ("apptax.taxonomie.routesbiblistes:adresses", "/biblistes"),
+    ("apptax.taxonomie.routestmedias:adresses", "/tmedias"),
+    ("apptax.taxonomie.routesbdcstatuts:adresses", "/bdc_statuts"),
+]
