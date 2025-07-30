1c6dac442aad432e57a1054de03073e87ae503ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:37 +0000

    Add package openhems/0.2.11/openhems-0.2.11.tar.gz

---
+++ packages/openhems/openhems-0.2.11.tar.gz/openhems-0.2.11/src/openhems/modules/network/__init__.py
@@ -0,0 +1,14 @@
+"""
+This module aim to abstract home network of connected devices.
+It is used to know devices and to switch on/off them.
+"""
+
+from .homestate_updater import HomeStateUpdater, HomeStateUpdaterException
+from .network import Network
+from .node import Node, ApplianceConstraints
+from .outnode import OutNode, Switch, FeedbackSwitch, HeatingSystem
+from .inoutnode import InOutNode, PublicPowerGrid, SolarPanel, Battery
+from .feeder import (
+	Feeder, SourceFeeder, RandomFeeder, ConstFeeder,
+	RotationFeeder, FakeSwitchFeeder, StateFeeder, SumFeeder # For fakeNetwork
+)
