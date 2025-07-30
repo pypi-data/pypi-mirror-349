f90d2e6f2ee8763ff89b268b0ed17f2796fb8f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:36:02 +0000

    Add package custom-flet-components/0.1.3/custom_flet_components-0.1.3-py3-none-any.whl

---
+++ packages/custom-flet-components/custom_flet_components-0.1.3-py3-none-any.whl/custom_flet_components/__init__.py
@@ -0,0 +1,7 @@
+from .custom_router import Router
+from .Carousal import CarouselWidget
+from .Carousal2 import CarouselWidget2
+from .CenteredWidget import CenteredLayout
+from .onboard import OnboardingWidget
+from .Shimmer import ShimmerWidget
+from .helper import *
