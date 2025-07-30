5b25acfcb0abaaefe6783315248dfaf5cd9dd72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:07:56 +0000

    Add package rutificador/0.2.26/rutificador-0.2.26.tar.gz

---
+++ packages/rutificador/rutificador-0.2.26.tar.gz/rutificador-0.2.26/rutificador/__init__.py
@@ -0,0 +1,21 @@
+"""
+Rutificador: Una biblioteca Python para validar y formatear RUTs chilenos.
+
+Este módulo proporciona herramientas para trabajar con RUTs chilenos (Rol Único Tributario),
+incluyendo validación, formateo y verificación de dígitos.
+
+Clases:
+    Rut: Representa un RUT chileno, con métodos para validación y formateo.
+    RutDigitoVerificador: Calcula y representa el dígito verificador de un RUT chileno.
+    RutInvalidoError: Excepción personalizada para errores de RUT inválido.
+"""
+
+from typing import List
+
+__version__ = "0.2.26"
+__author__ = "Carlos Ortega González"
+__license__ = "MIT"
+
+from .main import Rut, RutDigitoVerificador, RutInvalidoError
+
+__all__: List[str] = ["Rut", "RutDigitoVerificador", "RutInvalidoError"]
