ff23b0cc14258584a36fe75320bf911e4f413a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:11 +0000

    Add package clovers/0.4.4/clovers-0.4.4.tar.gz

---
+++ packages/clovers/clovers-0.4.4.tar.gz/clovers-0.4.4/clovers/__init__.py
@@ -0,0 +1,7 @@
+"""clovers 0.4"""
+
+__version__ = "0.4"
+__description__ = "高度自定义的聊天平台 Python 异步机器人指令-响应插件框架"
+
+from .clovers import Leaf, Client, LeafClient
+from .core import Result, Event, BaseHandle, Handle, TempHandle, Plugin, Adapter
