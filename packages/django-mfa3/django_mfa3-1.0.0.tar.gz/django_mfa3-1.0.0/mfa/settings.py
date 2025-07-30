dee8d8ee4c672c330e5bacd64ecbda5e9c6cca7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:08:57 +0000

    Add package django-mfa3/1.0.0/django_mfa3-1.0.0.tar.gz

---
+++ packages/django-mfa3/django_mfa3-1.0.0.tar.gz/django_mfa3-1.0.0/mfa/settings.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+
+# The actual domain (e.g. "example.com") that this is hosted on
+# FIDO2 will fail if this is not set correctly
+DOMAIN = settings.MFA_DOMAIN
+
+# Site title that is shown in authenticator apps
+SITE_TITLE = settings.MFA_SITE_TITLE
+
+# Available authentication methods in order of relevance
+METHODS = getattr(settings, 'MFA_METHODS', ['FIDO2', 'TOTP', 'recovery'])
+
+# `valid_window` parameter passed to PyOTP's verify method
+# See https://pyauth.github.io/pyotp/#pyotp.totp.TOTP.verify
+TOTP_VALID_WINDOW = getattr(settings, 'MFA_TOTP_VALID_WINDOW', 0)
+
+# `user_verification` parameter passed to python-fido2
+# See https://www.w3.org/TR/webauthn/#enum-userVerificationRequirement
+FIDO2_USER_VERIFICATION = getattr(settings, 'MFA_FIDO2_USER_VERIFICATION', None)
