6e0294fdc3822481f7c5c394731492b6ec2bb48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:37 +0000

    Add package tsuniverse/0.0.5/tsuniverse-0.0.5.tar.gz

---
+++ packages/tsuniverse/tsuniverse-0.0.5.tar.gz/tsuniverse-0.0.5/tsuniverse/process.py
@@ -0,0 +1,34 @@
+"""The main process function."""
+
+from multiprocessing import Pool
+from typing import Iterator
+
+import pandas as pd
+from timeseriesfeatures.feature import Feature  # type: ignore
+
+from .mutual_information_process import mutual_information_process
+from .pearson_process import pearson_process
+from .spearman_process import spearman_process
+
+
+def process(
+    df: pd.DataFrame,
+    predictands: list[str],
+    max_window: int,
+    max_process_features: int = 10,
+) -> Iterator[list[Feature]]:
+    """Process the dataframe for tsuniverse features."""
+    with Pool() as p:
+        for predictand in predictands:
+            for sub_process in [
+                pearson_process,
+                mutual_information_process,
+                spearman_process,
+            ]:
+                features = list(sub_process(df, predictand, max_window, p))
+                features = sorted(
+                    features,
+                    key=lambda x: abs(x["rank_value"] if "rank_value" in x else 0.0),
+                    reverse=True,
+                )[:max_process_features]
+                yield features
