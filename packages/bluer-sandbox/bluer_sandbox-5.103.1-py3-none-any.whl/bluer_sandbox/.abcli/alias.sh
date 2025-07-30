044cb59911861db513349061043f41e94e10c1d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:08 +0000

    Add package bluer-sandbox/5.103.1/bluer_sandbox-5.103.1-py3-none-any.whl

---
+++ packages/bluer-sandbox/bluer_sandbox-5.103.1-py3-none-any.whl/bluer_sandbox/.abcli/alias.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+alias @assets=bluer_sandbox_assets
+
+alias @docker=bluer_sandbox_docker
+
+alias @offline_llm=bluer_sandbox_offline_llm
+
+alias @notebooks=bluer_sandbox_notebooks
+
+alias @sandbox=bluer_sandbox
