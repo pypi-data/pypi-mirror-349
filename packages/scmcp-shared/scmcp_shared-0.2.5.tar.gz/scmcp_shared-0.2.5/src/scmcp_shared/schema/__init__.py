1f33ef939d17e31545d3f9c0e57363db97d7afc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:07:46 +0000

    Add package scmcp-shared/0.2.5/scmcp_shared-0.2.5.tar.gz

---
+++ packages/scmcp-shared/scmcp_shared-0.2.5.tar.gz/scmcp_shared-0.2.5/src/scmcp_shared/schema/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from pydantic import Field, BaseModel,ConfigDict
+
+
+class AdataModel(BaseModel):
+    """Input schema for the adata tool."""
+    sampleid: str | None = Field(default=None, description="adata sampleid")
+    adtype: str = Field(default="exp", description="The input adata.X data type for preprocess/analysis/plotting")
+
+    model_config = ConfigDict(
+        extra="ignore"
+    )
