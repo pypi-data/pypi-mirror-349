a6ff3ad92b32576bfc738ce79dd2b3c8a3ef32de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:10 +0000

    Add package any-agent/0.15.0/any_agent-0.15.0-py3-none-any.whl

---
+++ packages/any-agent/any_agent-0.15.0-py3-none-any.whl/any_agent/evaluation/evaluate.py
@@ -0,0 +1,44 @@
+from any_agent.config import AgentFramework
+from any_agent.evaluation.evaluation_case import EvaluationCase
+from any_agent.evaluation.evaluators import (
+    evaluate_checkpoint,
+    evaluate_qa_squad,
+)
+from any_agent.evaluation.schemas import TraceEvaluationResult
+from any_agent.logging import logger
+from any_agent.tracing import TracingProcessor
+from any_agent.tracing.trace import AgentTrace
+
+
+def evaluate(
+    evaluation_case: EvaluationCase,
+    trace: AgentTrace,
+    agent_framework: AgentFramework,
+) -> TraceEvaluationResult:
+    processor = TracingProcessor.create(agent_framework)
+    if not processor:
+        msg = f"Processor for {agent_framework} not available."
+        raise ValueError(msg)
+    checkpoint_results = evaluate_checkpoint(
+        model=evaluation_case.llm_judge,
+        trace=trace,
+        checkpoints=evaluation_case.checkpoints,
+        processor=processor,
+    )
+
+    if evaluation_case.ground_truth and trace.final_output:
+        ground_truth_result = evaluate_qa_squad(
+            final_output=trace.final_output,
+            ground_truth_answer=evaluation_case.ground_truth,
+        )
+    else:
+        logger.warning(
+            "No ground truth answer found in the evaluation case or no hypothesis answer in the trace. Skipping direct evaluation."
+        )
+        ground_truth_result = None
+
+    return TraceEvaluationResult(
+        trace=trace,
+        checkpoint_results=checkpoint_results,
+        ground_truth_result=ground_truth_result,
+    )
