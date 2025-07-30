17296ed3816a260df273fdc81f30b99b4fb1284a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:27 +0000

    Add package cfgutils/1.15.0/cfgutils-1.15.0.tar.gz

---
+++ packages/cfgutils/cfgutils-1.15.0.tar.gz/cfgutils-1.15.0/cfgutils/file_formats.py
@@ -0,0 +1,16 @@
+from typing import Union
+from pathlib import Path
+
+import networkx as nx
+import graphviz
+
+
+def save_cfg_as_png(cfg: nx.DiGraph, output_path: Union[Path, str]):
+    output_path = Path(output_path)
+    tmp_path = output_path.with_suffix(".dot")
+    nx.drawing.nx_agraph.write_dot(cfg, str(tmp_path))
+    dot_src = graphviz.Source(open(tmp_path).read(), format="png")
+    dot_src.render(outfile=str(output_path.with_suffix(".png")))
+    tmp_path.with_suffix(".gv").unlink()
+    tmp_path.unlink()
+    return output_path.with_suffix(".png")
---
+++ packages/cfgutils/cfgutils-1.15.0.tar.gz/cfgutils-1.15.0/tests/test_angr_cfg_tools.py
@@ -0,0 +1,69 @@
+import sys
+import unittest
+from pathlib import Path
+
+import networkx as nx
+
+ANGR_AVAILABLE = True
+try:
+    import angr
+except ImportError:
+    ANGR_AVAILABLE = False
+
+if ANGR_AVAILABLE:
+    from cfgutils.angr_utils.ail_graph_conv import binary_to_ail_cfgs
+    from cfgutils.angr_utils.block_matcher import AILBlockMatcher
+    from angr.analyses.decompiler.utils import find_block_by_addr
+
+TEST_FILES = Path(__file__).parent / "data"
+
+
+class TestAngrCFGTools(unittest.TestCase):
+    def setUp(self):
+        if not ANGR_AVAILABLE:
+            self.skipTest("angr is not available")
+
+    def test_cfg_attrs(self):
+        cfgs = binary_to_ail_cfgs(
+            TEST_FILES / "fmt_O0_noinline.o",
+            functions=["fmt"],
+            structuring_opts=False,
+            make_generic=True
+        )
+        cfg: nx.DiGraph = cfgs["fmt"]
+        for node, attr in cfg.nodes(data=True):
+            assert attr is not None
+            original_nodes = attr.get("original_nodes", {})
+            assert original_nodes
+
+    # TODO: re-enable this test later
+    def DISABLE_test_block_matcher(self):
+        cfgs0, p0 = binary_to_ail_cfgs(
+            TEST_FILES / "fmt_O0_noinline.o",
+            functions=["main"],
+            structuring_opts=False,
+            return_project=True
+        )
+        cfgs2, p2 = binary_to_ail_cfgs(
+            TEST_FILES / "fmt_O2_noinline.o",
+            functions=["main"],
+            structuring_opts=False,
+            return_project=True
+        )
+        main_o0 = cfgs0["main"]
+        main_o2 = cfgs2["main"]
+
+        assert main_o0 is not None
+        assert main_o2 is not None
+
+        matcher = AILBlockMatcher(main_o0, main_o2, proj1=p0, proj2=p2)
+        mappings = matcher.mapping
+
+        # TODO: add more assertions
+        o0_blk = find_block_by_addr(main_o0, 0x400862)
+        o2_blk = find_block_by_addr(main_o2, 0x40ce2b)
+        assert mappings[o0_blk] == o2_blk
+
+
+if __name__ == "__main__":
+    unittest.main(argv=sys.argv)
