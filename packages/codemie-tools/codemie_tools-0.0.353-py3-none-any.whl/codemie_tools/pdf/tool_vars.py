c68150c2b5fea9098d8bea621974949d009058de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:27 +0000

    Add package codemie-tools/0.0.353/codemie_tools-0.0.353-py3-none-any.whl

---
+++ packages/codemie-tools/codemie_tools-0.0.353-py3-none-any.whl/codemie_tools/pdf/tool_vars.py
@@ -0,0 +1,7 @@
+from codemie_tools.base.models import ToolMetadata
+
+PDF_TOOL = ToolMetadata(
+    name="pdf_tool",
+    description="A tool for extracting content from PDF documents.",
+    label="PDF Processing Tool",
+)
