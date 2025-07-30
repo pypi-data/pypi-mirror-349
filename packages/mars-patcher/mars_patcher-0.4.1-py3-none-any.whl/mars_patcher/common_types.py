c57ac805ba6cd1dd098b0e80330c3d5928432042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:36 +0000

    Add package mars-patcher/0.4.1/mars_patcher-0.4.1-py3-none-any.whl

---
+++ packages/mars-patcher/mars_patcher-0.4.1-py3-none-any.whl/mars_patcher/common_types.py
@@ -0,0 +1,12 @@
+from typing import Annotated
+
+from typing_extensions import TypeAlias
+
+from mars_patcher.auto_generated_types import Areaid, Typeu8
+
+AreaId: TypeAlias = Areaid
+RoomId: TypeAlias = Typeu8
+
+AreaRoomPair = tuple[AreaId, RoomId]
+
+MinimapId: TypeAlias = Annotated[int, "0 <= value < 10"]
