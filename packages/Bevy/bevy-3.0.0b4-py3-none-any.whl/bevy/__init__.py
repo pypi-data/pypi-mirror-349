ec15a6c6d16768cd2b28e35831823112a738716d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:39 +0000

    Add package Bevy/3.0.0b4/bevy-3.0.0b4-py3-none-any.whl

---
+++ packages/Bevy/bevy-3.0.0b4-py3-none-any.whl/bevy/__init__.py
@@ -0,0 +1,6 @@
+from bevy.containers import get_container
+from bevy.dependencies import dependency
+from bevy.injections import inject
+from bevy.registries import get_registry
+
+__all__ = ["get_registry", "get_container", "dependency", "inject"]
