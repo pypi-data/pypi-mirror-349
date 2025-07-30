919b0899344c04b5b60b71386847339fc7ab6524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:36:39 +0000

    Add package llm-echo/0.3a3/llm_echo-0.3a3.tar.gz

---
+++ packages/llm-echo/llm_echo-0.3a3.tar.gz/llm_echo-0.3a3/tests/test_echo.py
@@ -0,0 +1,130 @@
+import json
+import llm
+import llm.models
+import pytest
+
+
+def test_prompt():
+    model = llm.get_model("echo")
+    response = model.prompt("prompt", system="system")
+    assert json.loads(str(response)) == {
+        "prompt": "prompt",
+        "system": "system",
+        "attachments": [],
+        "stream": True,
+        "previous": [],
+    }
+
+
+def test_prompt_with_option():
+    model = llm.get_model("echo")
+    response = model.prompt("prompt", system="system", example_bool=True)
+    assert json.loads(str(response)) == {
+        "prompt": "prompt",
+        "system": "system",
+        "attachments": [],
+        "stream": True,
+        "previous": [],
+        "options": {"example_bool": True},
+    }
+
+
+def test_conversation():
+    model = llm.get_model("echo")
+    conversation = model.conversation()
+    # str forces the prompt to execute
+    str(conversation.prompt("hi1", system="system"))
+    response = conversation.prompt("hi2")
+    assert json.loads(str(response)) == {
+        "prompt": "hi2",
+        "system": "",
+        "attachments": [],
+        "stream": True,
+        "previous": [{"prompt": "hi1"}],
+    }
+
+
+@pytest.mark.asyncio
+async def test_async_prompt():
+    model = llm.get_async_model("echo")
+    response = await model.prompt("prompt", system="system")
+    text = await response.text()
+    assert json.loads(text) == {
+        "prompt": "prompt",
+        "system": "system",
+        "attachments": [],
+        "stream": True,
+        "previous": [],
+    }
+
+
+def test_prompt_with_tool_calls():
+    def example(input: str) -> str:
+        return f"Example output for {input}"
+
+    def hello() -> str:
+        return "Hello world"
+
+    model = llm.get_model("echo")
+    chain_response = model.chain(
+        json.dumps(
+            {
+                "tool_calls": [
+                    {
+                        "name": "example",
+                        "arguments": {"input": "test"},
+                    },
+                    {"name": "hello"},
+                ],
+                "prompt": "prompt",
+            }
+        ),
+        system="system",
+        tools=[example, hello],
+    )
+    responses = list(chain_response.responses())
+    tool_calls = responses[0].tool_calls()
+    assert tool_calls == [
+        llm.ToolCall(name="example", arguments={"input": "test"}, tool_call_id=None),
+        llm.ToolCall(name="hello", arguments={}, tool_call_id=None),
+    ]
+    assert responses[1].prompt.tool_results == [
+        llm.models.ToolResult(
+            name="example", output="Example output for test", tool_call_id=None
+        ),
+        llm.models.ToolResult(name="hello", output="Hello world", tool_call_id=None),
+    ]
+    assert json.loads(responses[1].text()) == {
+        "prompt": "",
+        "system": "",
+        "attachments": [],
+        "stream": True,
+        "previous": [
+            {
+                "prompt": '{"tool_calls": [{"name": "example", "arguments": {"input": "test"}}, {"name": "hello"}], "prompt": "prompt"}'
+            }
+        ],
+        "tool_results": [
+            {
+                "name": "example",
+                "output": "Example output for test",
+                "tool_call_id": None,
+            },
+            {"name": "hello", "output": "Hello world", "tool_call_id": None},
+        ],
+    }
+
+
+def test_raw():
+    model = llm.get_model("echo")
+    response = model.prompt(
+        json.dumps(
+            {
+                "raw": "this is the raw text",
+                "misc": "Other stuff",
+            }
+        ),
+        system="system",
+    )
+    output = response.text()
+    assert output == "this is the raw text"
