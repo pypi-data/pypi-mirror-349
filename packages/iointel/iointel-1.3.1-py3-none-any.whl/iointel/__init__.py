46786fbd8d4372b41d9d662b87602f34abff0eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:55 +0000

    Add package iointel/1.3.1/iointel-1.3.1-py3-none-any.whl

---
+++ packages/iointel/iointel-1.3.1-py3-none-any.whl/iointel/__init__.py
@@ -0,0 +1,48 @@
+from .src.utilities.magic import (
+    UNUSED as UNUSED,
+)  # this performs some magic to stop prefect mucking with logging setup
+from .src.agents import Agent
+from .src.memory import AsyncMemory
+from .src.workflow import Workflow
+from .src.utilities.runners import run_agents, run_agents_stream
+from .src.agent_methods.tools.wolfram import query_wolfram
+from .src.agent_methods.data_models.datamodels import PersonaConfig
+from .src.utilities.decorators import register_custom_task, register_tool
+
+from .src.code_parsers.pycode_parser import (
+    PythonModule,
+    ClassDefinition,
+    FunctionDefinition,
+    Argument,
+    ImportStatement,
+    PythonCodeGenerator,
+)
+
+__all__ = [
+    ###agents###
+    "Agent",
+    ###memory###
+    "AsyncMemory",
+    ###workflow###
+    "Workflow",
+    ###runners###
+    "run_agents",
+    "run_agents_stream",
+    ###decorators###
+    "register_custom_task",
+    "register_tool",
+    ###personas###
+    "PersonaConfig",
+    ###code parsers###
+    "PythonModule",
+    "ClassDefinition",
+    "FunctionDefinition",
+    "Argument",
+    "ImportStatement",
+    "PythonCodeGenerator",
+    ###tools###
+    "query_wolfram",
+]
+
+
+__version__ = "1.3.1"
