a4ebb91821f36b3cfc7aeeecc2805b6fa3b042dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:28 +0000

    Add package webviz-config-equinor/0.0.0/webviz_config_equinor-0.0.0-py3-none-any.whl

---
+++ packages/webviz-config-equinor/webviz_config_equinor-0.0.0-py3-none-any.whl/build/lib/build/lib/webviz_config_equinor/__init__.py
@@ -0,0 +1,48 @@
+import os
+import glob
+from pkg_resources import get_distribution, DistributionNotFound
+
+from webviz_config import WebvizConfigTheme
+from plotly import colors
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
+
+equinor_theme = WebvizConfigTheme(theme_name="equinor")
+
+equinor_theme.external_stylesheets = [
+    "https://cdn.eds.equinor.com/font/equinor-font.css"
+]
+
+equinor_theme.adjust_csp(
+    {
+        "font-src": ["https://cdn.eds.equinor.com"],
+        "img-src": ["https://cdn.eds.equinor.com"],
+        "style-src": ["https://cdn.eds.equinor.com"],
+    },
+    append=True,
+)
+
+equinor_theme.assets = glob.glob(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "assets", "*")
+)
+
+equinor_theme.plotly_theme = {
+    "layout": {
+        "font": {"family": "Equinor"},
+        "hoverlabel": {"font": {"family": "Equinor"}},
+        "plot_bgcolor": "white",
+        "xaxis": {"exponentformat": "SI"},
+        "yaxis": {"exponentformat": "SI"},
+        "coloraxis": {"colorbar": {"exponentformat": "SI"}},
+        "colorscale": {
+            "diverging": colors.get_colorscale("spectral"),
+            "sequential": colors.get_colorscale("viridis"),
+        },
+        "colorway": colors.qualitative.D3 + colors.qualitative.Light24,
+    }
+}
