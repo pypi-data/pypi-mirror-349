211983dc312d8a5157d1b720606e0ee1a82fd0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:40 +0000

    Add package scikit-digital-health/0.17.9/scikit_digital_health-0.17.9-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/scikit-digital-health/scikit_digital_health-0.17.9-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/skdh/utility/_extensions/__init__.py
@@ -0,0 +1,19 @@
+from .moving_statistics import (
+    moving_mean,
+    moving_sd,
+    moving_skewness,
+    moving_kurtosis,
+    moving_median,
+    moving_max,
+    moving_min,
+)
+
+__all__ = [
+    "moving_mean",
+    "moving_sd",
+    "moving_skewness",
+    "moving_kurtosis",
+    "moving_median",
+    "moving_max",
+    "moving_min",
+]
