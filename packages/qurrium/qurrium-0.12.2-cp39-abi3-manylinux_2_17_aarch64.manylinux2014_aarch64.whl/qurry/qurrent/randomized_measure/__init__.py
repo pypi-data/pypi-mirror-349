a37994de4c2e60dba80bca8b385771c61b6d468d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:27 +0000

    Add package qurrium/0.12.2/qurrium-0.12.2-cp39-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/qurrium/qurrium-0.12.2-cp39-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/qurry/qurrent/randomized_measure/__init__.py
@@ -0,0 +1,8 @@
+"""EntropyMeasureRandomized - The Second Order Renyi Entropy by Randomized Measurement
+(:mod:`qurry.qurrent.randomized_measure`)
+
+"""
+
+from .experiment import EntropyMeasureRandomizedExperiment
+from .analysis import EntropyMeasureRandomizedAnalysis
+from .qurry import EntropyMeasureRandomized
