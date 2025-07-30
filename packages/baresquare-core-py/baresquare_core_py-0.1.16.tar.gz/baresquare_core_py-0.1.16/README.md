befdc123463f5f5c6d2ea265fe6eafb53db25714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:00 +0000

    Add package baresquare-core-py/0.1.16/baresquare_core_py-0.1.16.tar.gz

---
+++ packages/baresquare-core-py/baresquare_core_py-0.1.16.tar.gz/baresquare_core_py-0.1.16/README.md
@@ -0,0 +1,9 @@
+# Baresquare Core Python Library
+
+Core utilities and functionality for Baresquare Python services.
+
+## Features
+
+- Logger 
+- Middleware components for web applications
+- Commonly used functions and classes
---
+++ packages/baresquare-core-py/baresquare_core_py-0.1.16.tar.gz/baresquare_core_py-0.1.16/baresquare_core_py/__init__.py
@@ -0,0 +1,11 @@
+"""Baresquare Core Python utilities.
+
+This package provides core utilities and functionality used across Baresquare Python services.
+"""
+
+from . import exceptions
+from . import middleware
+from . import logger as logger_module
+
+from .logger import setup_logger
+logger = logger_module.logger
