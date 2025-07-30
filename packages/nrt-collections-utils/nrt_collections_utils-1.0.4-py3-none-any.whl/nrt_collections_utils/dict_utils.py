273673bd3875527d10356f7f7da8631d920bca17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:04:59 +0000

    Add package nrt-collections-utils/1.0.4/nrt_collections_utils-1.0.4-py3-none-any.whl

---
+++ packages/nrt-collections-utils/nrt_collections_utils-1.0.4-py3-none-any.whl/nrt_collections_utils/dict_utils.py
@@ -0,0 +1,17 @@
+
+
+class DictUtil:
+
+    @staticmethod
+    def get_value(dict_: dict, path: [str, list], default_value=None):
+
+        if isinstance(path, str):
+            return dict_.get(path, default_value)
+
+        for key in path:
+            if not isinstance(dict_, dict):
+                return default_value
+
+            dict_ = dict_.get(key)
+
+        return dict_
