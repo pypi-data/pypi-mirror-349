7bf5a8cc75ba4f4b1d46cb3073e3cd93fddccf69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:36 +0000

    Add package boxmot/12.0.9/boxmot-12.0.9.tar.gz

---
+++ packages/boxmot/boxmot-12.0.9.tar.gz/boxmot-12.0.9/tests/test_config.py
@@ -0,0 +1,12 @@
+from boxmot import (
+    StrongSort, BotSort, DeepOcSort, OcSort, ByteTrack, BoostTrack, get_tracker_config, create_tracker,
+)
+
+MOTION_N_APPEARANCE_TRACKING_NAMES = ['botsort', 'deepocsort', 'strongsort', 'boosttrack']
+MOTION_ONLY_TRACKING_NAMES = ['ocsort', 'bytetrack']
+
+MOTION_N_APPEARANCE_TRACKING_METHODS=[StrongSort, BotSort, DeepOcSort, BoostTrack]
+MOTION_ONLY_TRACKING_METHODS=[OcSort, ByteTrack]
+
+ALL_TRACKERS = ['botsort', 'deepocsort', 'ocsort', 'bytetrack', 'strongsort', 'boosttrack']
+PER_CLASS_TRACKERS = ['botsort', 'deepocsort', 'ocsort', 'bytetrack']
