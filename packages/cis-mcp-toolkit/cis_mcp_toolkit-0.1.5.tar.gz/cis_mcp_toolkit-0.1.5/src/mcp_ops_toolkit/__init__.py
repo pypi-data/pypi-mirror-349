445eabafe7e7918e3caefb6d8d0336d5a7e02358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:12 +0000

    Add package cis-mcp-toolkit/0.1.5/cis_mcp_toolkit-0.1.5.tar.gz

---
+++ packages/cis-mcp-toolkit/cis_mcp_toolkit-0.1.5.tar.gz/cis_mcp_toolkit-0.1.5/src/mcp_ops_toolkit/__init__.py
@@ -0,0 +1,8 @@
+from . import main
+
+def app():
+    """Main entry point for the package."""
+    main.main()
+
+# # Optionally expose other important items at package level
+# __all__ = ['main', 'server']
