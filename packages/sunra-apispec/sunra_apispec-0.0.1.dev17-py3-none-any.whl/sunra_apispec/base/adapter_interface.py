020e87686ab970861ab715cf1c0d1680bb1c84a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:16:27 +0000

    Add package sunra-apispec/0.0.1.dev17/sunra_apispec-0.0.1.dev17-py3-none-any.whl

---
+++ packages/sunra-apispec/sunra_apispec-0.0.1.dev17-py3-none-any.whl/sunra_apispec/base/adapter_interface.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+
+class ServiceProviderEnum(Enum):
+    FAL = "fal"
+    REPLICATE = "replicate"
+
+
+class IFalAdapter:
+    def __init__(self, data: dict, skip_validation: bool = True):
+        self.data = data
+        self.skip_validation = skip_validation
+
+    def convert_input(self) -> dict:
+        pass
+    
+    def get_fal_model(self) -> str:
+        pass
+
+
+class IReplicateAdapter:
+    def __init__(self, data: dict, skip_validation: bool = True):
+        self.data = data
+        self.skip_validation = skip_validation
+
+    def convert_input(self) -> dict:
+        pass
+    
+    def get_replicate_model(self) -> str:
+        pass
+
+
+BaseAdapter = IFalAdapter | IReplicateAdapter
