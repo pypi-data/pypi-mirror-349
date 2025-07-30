16e98476171c87041b07dee4a2690512585594e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:40 +0000

    Add package labfreed/0.2.11/labfreed-0.2.11.tar.gz

---
+++ packages/labfreed/labfreed-0.2.11.tar.gz/labfreed-0.2.11/labfreed/pac_id/extension.py
@@ -0,0 +1,48 @@
+
+from abc import ABC, abstractproperty
+
+from pydantic import model_validator
+
+from labfreed.labfreed_infrastructure import LabFREED_BaseModel
+
+    
+class ExtensionBase(ABC):
+    name: str
+    type: str
+
+
+    @abstractproperty
+    def data(self) -> str:
+        raise NotImplementedError("Subclasses must implement 'data'")
+    
+    def __str__(self):
+        return f'{self.name}${self.type}/{self.data}'
+    
+
+
+class Extension(LabFREED_BaseModel,ExtensionBase):
+    '''Implementation of Extension for unknown extension types'''
+    name:str
+    type:str
+    data_:str
+    
+    @property
+    def data(self) -> str:
+        return self.data_
+       
+    @staticmethod
+    def create(*, name, type, data):
+        return Extension(name=name, type=type, data=data)
+    
+    @model_validator(mode='before')
+    @classmethod
+    def move_data_field(cls, values):
+        if "data" in values:
+            values["data_"] = values.pop("data")
+        return values
+    
+    model_config = {
+        "extra": "allow",  # Allow extra keys during pre-validation
+    }
+    
+
---
+++ packages/labfreed/labfreed-0.2.11.tar.gz/labfreed-0.2.11/labfreed/well_known_keys/labfreed/well_known_keys.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class WellKnownKeys(Enum):
+    GTIN = '01'
+    BATCH = '10'
+    CONTAINER_CODE = '20'
+    SERIAL = '21'
+    ADDITIONAL_IDENTIFIER = '240'
+    SECONDARY_SERIAL = '250'
+    RUN_ID_ABSOLUTE = 'RNR'
+    SAMPLE_ID = 'SMP'
+    EXPERIMENT_ID = 'EXP'
+    RESULT_ID = 'RST'
+    METHOD_ID = 'MTD'
+    REPORT_ID = 'RPT'
+    TIMESTAMP = 'TS'
+    VERSION = 'V'
