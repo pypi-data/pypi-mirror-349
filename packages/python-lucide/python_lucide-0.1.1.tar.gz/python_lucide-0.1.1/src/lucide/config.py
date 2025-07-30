716ea17fd45d878e49890fd6e7878f9dc7cb839c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:21 +0000

    Add package python-lucide/0.1.1/python_lucide-0.1.1.tar.gz

---
+++ packages/python-lucide/python_lucide-0.1.1.tar.gz/python_lucide-0.1.1/src/lucide/config.py
@@ -0,0 +1,7 @@
+"""Configuration for python-lucide.
+
+This module contains default configuration values used throughout the package.
+"""
+
+# Default Lucide tag to use when building the icon database
+DEFAULT_LUCIDE_TAG = "0.511.0"
