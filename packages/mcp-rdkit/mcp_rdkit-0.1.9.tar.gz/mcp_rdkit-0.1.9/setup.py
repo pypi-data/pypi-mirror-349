8341a2ec232b8542673e8aa6488de28c2d39bd85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:23 +0000

    Add package mcp-rdkit/0.1.9/mcp_rdkit-0.1.9.tar.gz

---
+++ packages/mcp-rdkit/mcp_rdkit-0.1.9.tar.gz/mcp_rdkit-0.1.9/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mcp_rdkit',
+    version='0.1.9',
+    author='Shashank Shekhar Shukla',
+    author_email='shukla20shashankshekhar@gmail.com',
+    description='A package for RDKit integration with MCP',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        "rdkit",
+        "mcp",
+        "pandas",
+        "requests",
+    ],
+    keywords=[
+        "RDKit",
+        "MCP",
+        "cheminformatics",
+        "molecular visualization",
+        "descriptor calculation",
+        "Claude"
+    ],
+    project_urls={
+        "Documentation": "https://github.com/s20ss/mcp_rdkit#readme",
+        "Source": "https://github.com/s20ss/mcp_rdkit",
+        "Issues": "https://github.com/s20ss/mcp_rdkit/issues"
+    },
+)
