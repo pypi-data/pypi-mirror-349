a6bd1e2cc8742ee9227993264ac854feef341686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:34 +0000

    Add package napari-roxas-ai/0.1.0/napari_roxas_ai-0.1.0-py3-none-any.whl

---
+++ packages/napari-roxas-ai/napari_roxas_ai-0.1.0-py3-none-any.whl/napari_roxas_ai/_tests/test_cells_model.py
@@ -0,0 +1,35 @@
+import numpy as np
+import torch
+
+from napari_roxas_ai._segmentation._cells_model import CellsSegmentationModel
+
+
+def test_cells_segmentation_model():
+
+    # Initialize model
+    model = CellsSegmentationModel()
+    print(f"Model running on device : {model.available_device}")
+
+    # Create a synthetic input image
+    test_input = np.random.randn(1, 3, 1024, 1024)
+
+    # Run forward pass (full image inference is too heavy for github actions environment)
+    with torch.no_grad():
+        output = (
+            model(torch.tensor(test_input).to(model.available_device).float())
+            .squeeze(0)
+            .cpu()
+            .numpy()
+        )
+    # Validate output shape
+    assert (
+        output.shape[-2:] == test_input.shape[-2:]
+    ), f"Unexpected output shape: {output.shape}"
+
+    # Check for NaNs
+    assert ~np.isnan(output).any(), "Found NaNs in the output"
+
+    # Ensure values are binaries
+    assert np.isin(output, (0, 1)).all(), "Output is not a binary label"
+
+    print("CellsSegmentationModel test passed!")
