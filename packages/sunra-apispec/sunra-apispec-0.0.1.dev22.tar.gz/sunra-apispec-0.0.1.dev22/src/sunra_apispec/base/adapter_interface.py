334e492e15e557fe6ad893cab48176a3a09c1943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:27 +0000

    Add package sunra-apispec/0.0.1.dev22/sunra-apispec-0.0.1.dev22.tar.gz

---
+++ packages/sunra-apispec/sunra-apispec-0.0.1.dev22.tar.gz/sunra-apispec-0.0.1.dev22/src/sunra_apispec/base/adapter_interface.py
@@ -0,0 +1,52 @@
+from enum import Enum
+
+
+class ServiceProviderEnum(Enum):
+    FAL = "fal"
+    REPLICATE = "replicate"
+    MINIMAX = "minimax"
+
+
+class IFalAdapter:
+    def __init__(self):
+       pass
+
+    def convert_input(self, data: dict, skip_validation: bool = True) -> dict:
+        pass
+    
+    def get_fal_model(self) -> str:
+        pass
+
+    def convert_output(self, data: dict) -> str | list[str]:
+        pass
+
+
+class IReplicateAdapter:
+    def __init__(self):
+        pass
+
+    def convert_input(self, data: dict, skip_validation: bool = True) -> dict:
+        pass
+    
+    def get_replicate_model(self) -> str:
+        pass
+
+
+class IMinimaxAdapter:
+    def __init__(self):
+        pass
+
+    def convert_input(self, data: dict, skip_validation: bool = True) -> dict:
+        pass
+
+    def get_request_url(self) -> str:
+        pass
+
+    def get_status_url(self, task_id: str) -> str:
+        pass
+
+    def get_file_url(self, file_id: str) -> str:
+        pass
+
+
+BaseAdapter = IFalAdapter | IReplicateAdapter | IMinimaxAdapter
