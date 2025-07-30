5932e28dd62432be6b9f051b064e86d70a96968c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:19 +0000

    Add package mbe-mcp-server/0.1.1/mbe_mcp_server-0.1.1.tar.gz

---
+++ packages/mbe-mcp-server/mbe_mcp_server-0.1.1.tar.gz/mbe_mcp_server-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mbe_mcp_server",  # 项目名称
+    version="0.1.1",  # 版本号
+    description="韬略服务对外Mcp服务",  # 简要描述
+    author="yanan.wya",  # 作者名字
+    author_email="wya0556@qq.com",  # 作者邮箱
+    url="",  # 项目
+    packages=find_packages(),  # 自动发现所有包
+    install_requires=[
+        "mcp",  # 所依赖的第三方库，例如 mcp 库
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # 支持的 Python 版本
+)
