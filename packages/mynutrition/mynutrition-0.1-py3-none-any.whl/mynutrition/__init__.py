3601dc1ea0c0de27b4f18de2f4164286c50669be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:37 +0000

    Add package mynutrition/0.1/mynutrition-0.1-py3-none-any.whl

---
+++ packages/mynutrition/mynutrition-0.1-py3-none-any.whl/mynutrition/__init__.py
@@ -0,0 +1,13 @@
+from .core import (
+    search_usda_foods,
+    fetch_multiple_foods,
+    extract_nutrients_df,
+    calculate_tee,
+    compute_target_macros_per_meal,
+    score_menu,
+    plot_radar_chart,
+    estimate_speed_bmi_age,
+    calories_to_exercise_with_distance,
+    filter_foods_by_nutrients,
+    calculate_bmr
+)
