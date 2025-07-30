5f463cbf984d430be1a52057bd134d813dd11714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:35:44 +0000

    Add package multiolib/2.5.1.dev20250521/multiolib-2.5.1.dev20250521-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/multiolib/multiolib-2.5.1.dev20250521-cp311-cp311-manylinux_2_28_x86_64.whl/multiolib/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '2.5.1.dev20250521'
+__commit_hash__ = '4973b1a1cd2386101ac154a8cef63f0da7581b06'
+findlibs_dependencies = ["eckitlib", "eccodeslib", "metkitlib", "atlaslib_ecmwf", "fdb5lib", "mirlib", "fckitlib"]
