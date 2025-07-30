41924310858ea3b4635debaf5df7ddea51d98ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:14 +0000

    Add package simplcal/0.0.1/simplcal-0.0.1.tar.gz

---
+++ packages/simplcal/simplcal-0.0.1.tar.gz/simplcal-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simplcal',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Tanvir',
+    description='A simple calculator package with GUI',
+)
+
