9bdd8fb4a25ef8b8f77fc8f3185d26ab0b0ef376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:45 +0000

    Add package pwnv/0.3.3/pwnv-0.3.3-py3-none-any.whl

---
+++ packages/pwnv/pwnv-0.3.3-py3-none-any.whl/pwnv/cli/utils/guards.py
@@ -0,0 +1,37 @@
+from functools import wraps
+
+
+def _guard(predicate, msg):
+    from pwnv.cli.utils.ui import warn
+
+    def deco(fn):
+        @wraps(fn)
+        def wrapper(*a, **kw):
+            if predicate():
+                return fn(*a, **kw)
+            warn(msg)
+
+        return wrapper
+
+    return deco
+
+
+def config_exists():
+    from pwnv.cli.utils.config import get_config_path
+
+    return _guard(
+        lambda: get_config_path().exists(),
+        "No config. Run [magenta]`pwnv init`[/]. ",
+    )
+
+
+def ctfs_exists():
+    from pwnv.cli.utils.crud import get_ctfs
+
+    return _guard(lambda: bool(get_ctfs()), "No CTFs found.")
+
+
+def challenges_exists():
+    from pwnv.cli.utils.crud import get_challenges
+
+    return _guard(lambda: bool(get_challenges()), "No challenges found.")
