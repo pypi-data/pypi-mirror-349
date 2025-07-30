4cec24b0667a3c892419091a5b438af8bcecf5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:29 +0000

    Add package fpdb/0.0.0.dev2/fpdb-0.0.0.dev2-py3-none-any.whl

---
+++ packages/fpdb/fpdb-0.0.0.dev2-py3-none-any.whl/fpdb/forked_pdb.py
@@ -0,0 +1,15 @@
+import sys
+import pdb
+
+class ForkedPdb(pdb.Pdb):
+    """
+    PDB Subclass for debugging multi-processed code
+    Suggested in: https://stackoverflow.com/questions/4716533/how-to-attach-debugger-to-a-python-subproccess
+    """
+    def interaction(self, *args, **kwargs):
+        _stdin = sys.stdin
+        try:
+            sys.stdin = open('/dev/stdin')
+            pdb.Pdb.interaction(self, *args, **kwargs)
+        finally:
+            sys.stdin = _stdin
