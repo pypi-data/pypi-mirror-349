b39b8e5474fc3f0e908c5ed20a2c8b7767d44f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:34 +0000

    Add package vietnamese-address-parser/0.1.1/vietnamese_address_parser-0.1.1.tar.gz

---
+++ packages/vietnamese-address-parser/vietnamese_address_parser-0.1.1.tar.gz/vietnamese_address_parser-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vietnamese_address_parser',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here
+        
+    ]
+)
