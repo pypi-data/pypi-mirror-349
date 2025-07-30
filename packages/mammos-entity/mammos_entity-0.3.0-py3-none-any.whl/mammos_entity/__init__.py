60005713894ddd5c2ac4b1e1ff856a32b64b14eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:34 +0000

    Add package mammos-entity/0.3.0/mammos_entity-0.3.0-py3-none-any.whl

---
+++ packages/mammos-entity/mammos_entity-0.3.0-py3-none-any.whl/mammos_entity/__init__.py
@@ -0,0 +1,14 @@
+"""
+Package: mammos_entity
+
+Exposes the primary components of the MaMMoS entity package, including
+the `Entity` class for ontology-linked physical quantities, pre-defined
+factory methods for common magnetic entities (Ms, A, Ku, H), and the
+loaded MaMMoS ontology object.
+"""
+
+from mammos_entity.base import Entity
+from mammos_entity.entities import A, BHmax, H, Hc, Ku, Mr, Ms, Tc
+from mammos_entity.onto import mammos_ontology
+
+__all__ = ["Entity", "A", "H", "Ku", "Ms", "Tc", "Hc", "Mr", "BHmax", "mammos_ontology"]
---
+++ packages/mammos-entity/mammos_entity-0.3.0-py3-none-any.whl/mammos_entity/entities.py
@@ -0,0 +1,178 @@
+"""
+Module: entities.py
+
+Provides convenience factory functions for creating common physical property
+entities (such as spontaneous magnetization or external magnetic field)
+using the `Entity` class from `mammos_entity.base`.
+"""
+
+from numpy import typing
+
+from mammos_entity.base import Entity
+
+
+def Ms(value: int | float | typing.ArrayLike = 0, unit: None | str = None) -> Entity:
+    """
+    Create an Entity representing the spontaneous magnetization (Ms).
+
+    Parameters
+    ----------
+    value : float | int | typing.ArrayLike
+        Numeric value corresponding to spontaneous magnetization. It can also be a Numpy
+        array.
+    unit : optional
+        Unit of measure for the value (e.g., 'A/m'). If omitted, the SI unit
+        from the ontology, i.e. A/m, will be inferred.
+
+    Returns
+    -------
+    Entity
+        An `Entity` object labeled "SpontaneousMagnetization".
+    """
+    return Entity("SpontaneousMagnetization", value, unit)
+
+
+def A(value: int | float | typing.ArrayLike = 0, unit: None | str = None) -> Entity:
+    """
+    Create an Entity representing the exchange stiffness constant (A).
+
+    Parameters
+    ----------
+    value : float | int | typing.ArrayLike
+        Numeric value corresponding to exchange stiffness. It can also be a Numpy array.
+    unit : optional
+        Unit of measure for the value (e.g., 'J/m'). If omitted, the SI unit
+        from the ontology, i.e. J/m, will be inferred.
+
+    Returns
+    -------
+    Entity
+        An `Entity` object labeled "ExchangeStiffnessConstant".
+    """
+    return Entity("ExchangeStiffnessConstant", value, unit)
+
+
+def Ku(value: int | float | typing.ArrayLike = 0, unit: None | str = None) -> Entity:
+    """
+    Create an Entity representing the uniaxial anisotropy constant (Ku).
+
+    Parameters
+    ----------
+    value : float | int | typing.ArrayLike
+        Numeric value corresponding to the uniaxial anisotropy constant. It can also be
+        a Numpy array.
+    unit : optional
+        Unit of measure for the value (e.g., 'J/m^3'). If omitted, the SI unit
+        from the ontology, i.e. J/m^3 will be inferred.
+
+    Returns
+    -------
+    Entity
+        An `Entity` object labeled "UniaxialAnisotropyConstant".
+    """
+    return Entity("UniaxialAnisotropyConstant", value, unit)
+
+
+def H(value: int | float | typing.ArrayLike = 0, unit: None | str = None):
+    """
+    Create an Entity representing the external magnetic field (H).
+
+    Parameters
+    ----------
+    value : float | int | typing.ArrayLike
+        Numeric value corresponding to the external magnetic field. It can also be a
+        Numpy array.
+    unit : optional
+        Unit of measure for the value (e.g., 'T' for Tesla). If omitted, the SI unit
+        from the ontology, i.e. T, will be inferred.
+
+    Returns
+    -------
+    Entity
+        An `Entity` object labeled "ExternalMagneticField".
+    """
+    return Entity("ExternalMagneticField", value, unit)
+
+
+def Tc(value: int | float | typing.ArrayLike = 0, unit: None | str = None):
+    """
+    Create an Entity representing the Curie temperature.
+
+    Parameters
+    ----------
+    value : float | int | typing.ArrayLike
+        Numeric value corresponding to the Curie temperature. It can also be a
+        Numpy array.
+    unit : optional
+        Unit of measure for the value (e.g., 'K' for Kelvin). If omitted, the SI unit
+        from the ontology, i.e. K, will be inferred.
+
+    Returns
+    -------
+    Entity
+        An `Entity` object labelled "CurieTemperature".
+    """
+    return Entity("CurieTemperature", value, unit)
+
+
+def Hc(value: int | float | typing.ArrayLike = 0, unit: None | str = None):
+    """
+    Create an Entity representing the external coercive field.
+
+    Parameters
+    ----------
+    value : float | int | typing.ArrayLike
+        Numeric value corresponding to the external coercive field. It can also be a
+        Numpy array.
+    unit : optional
+        Unit of measure for the value (e.g., 'A/m'). If omitted, the SI unit
+        from the ontology, i.e. A/m, will be inferred.
+
+    Returns
+    -------
+    Entity
+        An `Entity` object labelled "CoercivityHcExternal".
+    """
+    return Entity("CoercivityHcExternal", value, unit)
+
+
+def Mr(value: int | float | typing.ArrayLike = 0, unit: None | str = None):
+    """
+    Create an Entity representing the remanent magnetisation.
+
+    Parameters
+    ----------
+    value : float | int | typing.ArrayLike
+        Numeric value corresponding to the remanent magnetisation. It can also be a
+        Numpy array.
+    unit : optional
+        Unit of measure for the value (e.g., 'A/m'). If omitted, the SI unit
+        from the ontology, i.e. A/m, will be inferred.
+
+    Returns
+    -------
+    Entity
+        An `Entity` object labelled "Remanence".
+    """
+    return Entity("Remanence", value, unit)
+
+
+def BHmax(value: int | float | typing.ArrayLike = 0, unit: None | str = None):
+    """
+    Create an Entity representing the maximum energy product of the hysteresis loop.
+
+    Parameters
+    ----------
+    value : float | int | typing.ArrayLike
+        Numeric value corresponding to the maximum energy product. It can also be a
+        Numpy array.
+    unit : optional
+        Unit of measure for the value (e.g., 'J/m3'). If omitted, the SI unit
+        from the ontology, i.e. J/m3, will be inferred.
+
+    Returns
+    -------
+    Entity
+        An `Entity` object labelled "MaximumEnergyProduct".
+    """
+    return Entity("MaximumEnergyProduct", value, unit)
