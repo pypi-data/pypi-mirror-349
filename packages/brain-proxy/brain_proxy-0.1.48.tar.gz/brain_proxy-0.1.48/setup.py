460830bacf7f17da1ae4e7351701f6cbf46ed407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:40 +0000

    Add package brain-proxy/0.1.48/brain_proxy-0.1.48.tar.gz

---
+++ packages/brain-proxy/brain_proxy-0.1.48.tar.gz/brain_proxy-0.1.48/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from brain_proxy.__version__ import __version__
+
+setup(
+    name="brain-proxy",
+    version="0.1.48",
+    description="OpenAI-compatible FastAPI router with Chroma + LangMem memory.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Pablo Schaffner",
+    author_email="pablo@puntorigen.com",
+    url="https://github.com/puntorigen/brain-proxy",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        "fastapi",
+        "openai",
+        "langchain-chroma",
+        "langmem",
+        "tiktoken",
+        "pydantic",
+        "langchain-openai",
+        "numpy<2.0.0",
+        "litellm",
+        "langchain_litellm",
+        "dateparser",
+        "async-promptic",
+        "httpx",
+        "upstash-vector"  # Add Upstash Vector dependency
+    ],
+    include_package_data=True,
+)
