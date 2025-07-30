d19250dd2cabf47b4982ee3f5cb851ca5fac2fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:35:26 +0000

    Add package predictramdataone/1.0.0/predictramdataone-1.0.0.tar.gz

---
+++ packages/predictramdataone/predictramdataone-1.0.0.tar.gz/predictramdataone-1.0.0/predictramdataone/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+from .api import StockAPI
+
+__all__ = ['StockAPI']
+__version__ = '1.0.0'
---
+++ packages/predictramdataone/predictramdataone-1.0.0.tar.gz/predictramdataone-1.0.0/setup.py
@@ -0,0 +1,27 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="predictramdataone",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'predictramdataone': ['data/*.json'],
+    },
+    install_requires=[
+        # No external dependencies for this simple package
+    ],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="API for accessing PredictRam stock data",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/predictramdataone",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
