c0defff5944e11dbcbc2247e3010acac41065bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:03 +0000

    Add package c2dataviewer/1.14.8/c2dataviewer-1.14.8.tar.gz

---
+++ packages/c2dataviewer/c2dataviewer-1.14.8.tar.gz/c2dataviewer-1.14.8/c2dataviewer/control/scope_controller_base.py
@@ -0,0 +1,344 @@
+"""
+C2 DATA VIEWER is distributed subject to a Software License Agreement found
+in the file LICENSE that is included with this distribution.
+SPDX-License-Identifier: EPICS
+"""
+
+import numpy as np
+import pyqtgraph
+import pvaccess as pva
+from ..model import ConnectionState
+import math
+
+class ScopeControllerBase:
+    def __init__(self, widget, model, parameters, warning, channels=[], **kwargs):
+        self._win = widget
+        self.model = model
+        self.parameters = parameters
+        self.data = None
+        # refresh frequency: every 100 ms by default
+        self.refresh = 100
+
+        self.plotting_started = False
+
+        self.timer = pyqtgraph.QtCore.QTimer()
+        self.timer.timeout.connect(self._win.graphicsWidget.update_drawing)
+        self._win.graphicsWidget.set_autoscale(parameters.child('Display', 'Autoscale').value())
+        self._win.graphicsWidget.set_model(self.model)
+
+        self._warning = warning
+        self._warning.warningConfirmButton.clicked.connect(lambda: self.accept_warning())
+
+        self.arrays = np.array([])
+        self.lastArrays = 0
+
+        self.default_trigger = None
+        self.trigger_is_monitor = False
+        self.trigger_auto_scale = False
+
+        self._win.graphicsWidget.set_histogram(parameters.child('Display', 'Histogram').value())
+        single_axis = parameters.child('Display', 'Single axis').value()
+        self._win.graphicsWidget.setup_plot(channels=channels, single_axis=single_axis)
+
+        display_mode = parameters.child('Display', 'Mode').value()
+        if(display_mode != "normal"):
+            self.set_display_mode(display_mode)
+            
+        # timer to update status with statistics data
+        self.status_timer = pyqtgraph.QtCore.QTimer()
+        self.status_timer.timeout.connect(self.update_status)
+        self.status_timer.start(1000)
+
+    def set_display_mode(self, val):
+        self._win.graphicsWidget.set_display_mode(val)
+        self._win.graphicsWidget.setup_plot()
+        # Disable multiaxis for the FFT-type modes. As at the time of the
+        # writing this code pyqtgraph does not support logarithmic scale in multiaxis configuration.
+        if self._win.graphicsWidget.display_mode.is_fft():
+            self.parameters.child("Display").child("Single axis").setReadonly()
+        else:
+            self.parameters.child("Display").child("Single axis").setWritable()
+
+    def set_trigger_pv(self, pvname):
+        if self._win.graphicsWidget.plotting_started:
+            self.notify_warning("Stop plotting first before changing trigger PV")
+            return
+                
+        pvname = pvname.strip()
+        if pvname == '':
+            return
+        
+        proto = 'ca'
+        name = pvname
+        
+        if "://" in name:
+            proto, name = pvname.split("://")
+
+        try:
+            pvfields = self.model.update_trigger(name, proto=proto.lower())
+            child = self.parameters.child("Trigger").child("Time Field")
+            if proto == 'ca':
+                child.hide()
+                self._win.graphicsWidget.trigger.trigger_time_field = 'timeStamp'
+            else:
+                self._win.graphicsWidget.trigger.trigger_time_field = None
+                pvfields.insert(0, 'None')
+                child.show()
+                child.setLimits(pvfields)
+                child.setValue('None')                                
+                                
+        except Exception as e:
+            self.notify_warning("Channel {}://{} timed out. \n{}".format(proto, name, repr(e)))
+
+
+    def default_config(self, **kwargs):
+        buffer_unit = kwargs.get('buffer_unit', 'Samples')
+            
+        max_length = self.parameters.child("Acquisition").child("Buffer (%s)" % buffer_unit).value()
+        if max_length and buffer_unit == 'Samples':
+            self.update_buffer_samples(int(max_length))
+            
+        self._win.graphicsWidget.set_binning(self.parameters.child("Display").child("Num Bins").value())
+
+        refresh = self.parameters.child("Display").child("Refresh").value()
+        if refresh:
+            self.set_freshrate(refresh)
+
+        try:
+            trigger_pv = self.parameters.child("Trigger").child("PV").value()
+            self.set_trigger_pv(trigger_pv)
+
+            trigger_mode = self.parameters.child("Trigger").child("Mode").value()
+            self.set_trigger_mode(trigger_mode)
+        
+            self.trigger_auto_scale = self.parameters.child("Trigger").child("Autoscale Buffer").value()
+        except:
+            pass
+
+        
+    def update_buffer_samples(self, size):
+        """
+        Sets number of samples in buffer
+        
+        :param size  size of buffer in number of samples
+        """
+        self._win.graphicsWidget.update_buffer(size)
+        self.parameters.child("Acquisition").child("Buffer (Samples)").setValue(self._win.graphicsWidget.max_length)
+            
+    def parameter_change(self, params, changes):
+        """
+        
+        :param params:
+        :param changes:
+        :return:
+        """
+        for param, change, data in changes:
+            if change == "value":
+                path = self.parameters.childPath(param)
+                if path is not None:
+                    childName = '.'.join(path)
+                else:
+                    childName = param.name()
+
+                if childName == "Trigger.PV":
+                    if data is None:
+                        return
+                    self.set_trigger_pv(data)
+                elif childName == "Trigger.Mode":
+                    self.set_trigger_mode(data)
+                elif childName == "Trigger.Threshold":
+                    self._win.graphicsWidget.trigger.trigger_level = data
+                elif childName == "Trigger.Data Time Field":
+                    self._win.graphicsWidget.trigger.data_time_field = data
+                elif childName == "Trigger.Time Field":
+                    self._win.graphicsWidget.trigger.trigger_time_field = data
+                elif childName == "Trigger.Autoscale Buffer":
+                    self.trigger_auto_scale = data
+                elif childName == "Acquisition.Buffer (Samples)":
+                    self._win.graphicsWidget.update_buffer(data)
+                elif childName == "Acquisition.Freeze":
+                    self._win.graphicsWidget.is_freeze = data
+                elif childName == "Display.Mode":
+                    self.set_display_mode(data)
+                elif childName == "Display.FFT filter":
+                    self._win.graphicsWidget.set_fft_filter(data)
+                elif childName == "Display.Exp moving avg":
+                    self._win.graphicsWidget.set_average(data)
+                elif childName == "Display.Autoscale":
+                    self._win.graphicsWidget.set_autoscale(data)
+                elif childName == "Display.Single axis":
+                    self._win.graphicsWidget.setup_plot(single_axis=data)
+                elif childName == "Display.Histogram":
+                    self._win.graphicsWidget.set_histogram(data)
+                elif childName == "Display.Num Bins":
+                    self._win.graphicsWidget.set_binning(data)
+                elif childName == "Display.Refresh":
+                    self.set_freshrate(data)
+
+    def set_freshrate(self, value):
+        """
+        Set time to refresh
+            
+        :param value: time interval to plot, in second
+        :return:
+        """
+        plotting_started = self._win.graphicsWidget.plotting_started
+        self.stop_plotting()
+        self.refresh = value*1000.0
+        if plotting_started:
+            self.start_plotting()
+
+
+    def start_plotting(self):
+        """
+            
+        :return:
+        """
+
+        try:
+            self.timer.timeout.disconnect()
+            self.timer.stop()
+        except Exception:
+            pass
+
+        self.stop_trigger()
+        
+        # Setup free run plotting
+        if not self._win.graphicsWidget.trigger_mode():
+            self.timer.timeout.connect(self._win.graphicsWidget.update_drawing)
+            self.timer.start(int(self.refresh))
+        else:
+            self.start_trigger()
+
+        self._win.graphicsWidget.notify_plotting_started(True)
+        
+    def stop_plotting(self):
+        """
+
+        :return:
+        """
+        self._win.graphicsWidget.notify_plotting_started(False)
+
+        self.timer.stop()
+        self.stop_trigger()
+                    
+    def set_trigger_mode(self, value):
+        """
+        Set trigger mode.
+    
+        :param value:
+        :return:
+        """
+        trigger_mode = value != 'none'
+        if trigger_mode != self._win.graphicsWidget.trigger_mode():
+            if self._win.graphicsWidget.plotting_started:
+                action = 'on' if trigger_mode else 'off'
+                self.notify_warning('Stop plotting first before turning trigger %s' % (action))
+                return
+            
+        self._win.graphicsWidget.set_trigger_mode(trigger_mode)
+            
+        trigger_type = None
+        if trigger_mode:
+            self._win.graphicsWidget.trigger.set_trigger_type(value)
+            
+    def start_trigger(self):
+        """
+        Process to start DAQ in trigger mode
+            
+        :return:
+        """
+        if not self.trigger_is_monitor:
+            if self.model.trigger is None:
+                raise Exception('Trigger PV is not set or is invalid')
+            
+
+            
+            self.model.start_trigger(self._win.graphicsWidget.trigger.data_callback)
+            self.trigger_is_monitor = True
+
+    def stop_trigger(self):
+        """
+        Stop trigger mode
+        :return:
+        """
+        self.trigger_is_monitor = False
+        self.model.stop_trigger()
+        
+    def accept_warning(self):
+        """
+            
+        :return:
+        """
+        self._warning.close()
+
+    def notify_warning(self, msg):
+        #close previous warning
+        self.accept_warning()
+            
+        self._warning.warningTextBrowse.setText(msg)
+        self._warning.show()
+
+        
+    def update_status(self):
+        """
+        Update statistics status.
+            
+        :return:
+        """
+        # Update display
+        single_axis_child = self.parameters.child("Display", "Single axis")
+        if single_axis_child.value() != self._win.graphicsWidget.single_axis:
+            single_axis_child.setValue(self._win.graphicsWidget.single_axis)
+
+        # Update statistics
+        with self._win._proc.oneshot():
+            cpu = self._win._proc.cpu_percent(None)
+
+        # TODO algorithm to calculate Array/sec
+        arraysReceived = self._win.graphicsWidget.arraysReceived
+        n = arraysReceived - self.lastArrays
+        self.lastArrays = arraysReceived
+        self.arrays = np.append(self.arrays, n)[-10:]
+
+        for q in self.parameters.child("Statistics").children():
+            if q.name() == 'CPU':
+                q.setValue(cpu)
+            elif q.name() == 'Lost Arrays':
+                q.setValue(self._win.graphicsWidget.lostArrays)
+            elif q.name() == 'Tot. Arrays':
+                q.setValue(self._win.graphicsWidget.arraysReceived)
+            elif q.name() == 'Arrays/Sec':
+                q.setValue(self.arrays.mean())
+            elif q.name() == 'Bytes/Sec':
+                q.setValue(self.arrays.mean() * self._win.graphicsWidget.data_size)
+            elif q.name() == 'Rate':
+                q.setValue(self._win.graphicsWidget.fps)
+
+        try:
+            for q in self.parameters.child("Trigger").children():
+                if q.name() == "Trig Status":
+                    stat_str = 'Disconnected'
+                    if self.trigger_is_monitor:
+                        stat_str = self._win.graphicsWidget.trigger.status()
+                    q.setValue(stat_str)
+                elif q.name() == "Trig Value":
+                    q.setValue(str(self._win.graphicsWidget.trigger.trigger_value))
+        except:
+            pass
+
+        #handle any auto-adjustments
+        if self.trigger_auto_scale and self._win.graphicsWidget.trigger_mode():
+            if self._win.graphicsWidget.trigger.missed_triggers > 0:
+                newsize = self._win.graphicsWidget.trigger.missed_adjust_buffer_size
+
+                # Round up to 3 10's places
+                # ex. 12345 would be rounded to 12400
+                precision = 3
+                exp = math.ceil(math.log10(newsize))
+                roundunit = 10**max(exp - precision, 0)
+                newsize = math.ceil(newsize / roundunit) * roundunit
+
+                self.update_buffer_samples(newsize)
+            
+        
---
+++ packages/c2dataviewer/c2dataviewer-1.14.8.tar.gz/c2dataviewer-1.14.8/test/test_scopecontroller.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+
+"""
+C2 DATA VIEWER is distributed subject to a Software License Agreement found
+in the file LICENSE that is included with this distribution.
+SPDX-License-Identifier: EPICS
+
+Copyright 2021 UChicago Argonne LLC
+ as operator of Argonne National Laboratory
+
+Unit tests for Scope controller
+
+@author: Matic Pogacnik <mpogacnik@anl.gov>
+"""
+import os
+import sys
+import unittest
+import configparser
+import pvaccess as pva
+
+from pyqtgraph.Qt import QtWidgets
+from pyqtgraph.parametertree import Parameter
+
+from c2dataviewer.model import DataSource as DataReceiver
+from c2dataviewer.view import Configure
+from c2dataviewer.scope import ScopeWindow, WarningDialog
+from c2dataviewer.control import ScopeController
+
+
+os.environ["QT_QPA_PLATFORM"] = "offscreen"
+
+
+class TestImageDisplay(unittest.TestCase):
+    """
+    Units tests for the scope controllers.
+    """
+
+    def setUp(self):
+        """
+        Build the environment for each unit test case.
+        This method is called before each test.
+
+        :return:
+        """
+
+        # Create ImageWindow and get the imageWidget instance
+        self.window = ScopeWindow()
+
+        # Build parameter tree
+        configure = Configure(configparser.ConfigParser())
+        parameters = Parameter.create(
+            name="params", type="group", children=configure.parse())
+        self.window.parameterPane.setParameters(parameters, showTop=False)
+
+        # Model to be used
+        model = DataReceiver()
+
+        # Build GUI elements
+        warning = WarningDialog(None)
+
+        self.scope_controller = ScopeController(
+            widget=self.window, model=model, parameters=parameters, WARNING=warning)
+
+    def tearDown(self):
+        """
+        Tear down the environment after each test case.
+        This mentod is called after each test.
+
+        :return:
+        """
+        pass
+
+    def test_update_status(self):
+        """
+        Test update statistics
+        """
+        #make sure that update_status runs without errors
+        self.scope_controller.update_status()
+
+
+    def test_get_fdr(self):
+        """
+        Test if the PV structure is properly parsed and correct fields are
+        extracted to be used as a drop down items on the GUI. This are pva.ScalarType
+        and arrays of pva.ScalarType.
+        """
+        # Mock get for the receiver
+        def mock_get(*_):
+
+            test_struct = {
+                "anInt": pva.INT,
+                "aStruct": {
+                    "aStruct_aFloat": pva.FLOAT,
+                    "aStruct_aDoubleArray": [pva.DOUBLE],
+                },
+                "aFloatArray": [pva.FLOAT],
+                "aStringArray": [pva.STRING],
+                "aStructArray": [
+                    {
+                        "aStructArray_aFloat": pva.FLOAT,
+                        "aStructArray_aDoubleArray": [pva.DOUBLE],
+                    }
+                ],
+                "aString": pva.STRING,
+                "aDoubleArray": [pva.DOUBLE],
+                "aVariant": (),
+                "aBooleanArray": [pva.BOOLEAN],
+            }
+            return pva.PvObject(test_struct)
+        self.scope_controller.model.get = mock_get
+
+        fdr, fdr_scalar, fdr_nonnumeric = self.scope_controller.get_fdr()
+
+        self.assertListEqual(fdr, ['aBooleanArray', 'aDoubleArray',
+                             'aFloatArray', 'aStruct.aStruct_aDoubleArray'])
+        self.assertListEqual(
+            fdr_scalar, ['aString', 'aStruct.aStruct_aFloat', 'anInt'])
+
+        self.assertListEqual(fdr_nonnumeric, ['aStringArray'])
+        
+    def test_buffer_size(self):
+        #pass in data
+        self.scope_controller.monitor_callback({ 'x' : [10]*100})
+        bufval = self.scope_controller.parameters.child("Acquisition").child("Buffer (Samples)").value()
+        self.assertEqual(bufval, 100)
+        
+        #set to object unit
+        self.scope_controller.set_buffer_unit('Objects')
+        bufval = self.scope_controller.parameters.child("Acquisition").child("Buffer (Objects)").value()
+        self.assertEqual(bufval, 1)
+        
