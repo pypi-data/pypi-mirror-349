32a403fa6baf8bf302fa6632b1aad06ac6034abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:02 +0000

    Add package addition-numpy-pytorch-package/0.1.2/addition_numpy_pytorch_package-0.1.2.tar.gz

---
+++ packages/addition-numpy-pytorch-package/addition_numpy_pytorch_package-0.1.2.tar.gz/addition_numpy_pytorch_package-0.1.2/src/addition_numpy_pytorch_package/multiplication.py
@@ -0,0 +1,14 @@
+import torch
+
+
+def matmul(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    """Performs matrix multiplication between two tensors.
+
+    Args:
+        x (torch.Tensor): The first input tensor (matrix).
+        y (torch.Tensor): The second input tensor (matrix).
+
+    Returns:
+        torch.Tensor: The result of the matrix multiplication of x and y.
+    """
+    return x.matmul(y)
