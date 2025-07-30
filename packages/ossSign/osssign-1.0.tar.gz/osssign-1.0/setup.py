5b0e666a25cf1ea83cfbbe631d832273f2b093c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:25 +0000

    Add package ossSign/1.0/osssign-1.0.tar.gz

---
+++ packages/ossSign/osssign-1.0.tar.gz/osssign-1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open(file="README.md", mode="r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ossSign",
+    version="1.0",
+    author="zhangchaolei",
+    author_email="ddt_8888@126.com",
+    description="oss签名",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    python_requires='>=3.10',
+)
