a69090d6013ab43429a5e4d04000f6d0ce6d7341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:19 +0000

    Add package pygameshader/1.0.11/pygameshader-1.0.11-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pygameshader/pygameshader-1.0.11-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/PygameShader/__init__.py
@@ -0,0 +1,15 @@
+"""
+PygameShader library loading
+"""
+from PygameShader.shader import *
+# from PygameShader.shader_gpu import *
+from PygameShader.misc import *
+from PygameShader.Palette import *
+from PygameShader.gaussianBlur5x5 import *
+from PygameShader.Fire import *
+# from PygameShader.Sprites import *
+from PygameShader.RGBConvert import *
+from PygameShader.PygameTools import *
+# from PygameShader.BurstSurface import  *
+from PygameShader.BlendFlags import *
+
