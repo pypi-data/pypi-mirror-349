6b39f33daa0b43bfc7260fbaf17a4c4f78b88da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:02 +0000

    Add package toolregistry/0.4.9/toolregistry-0.4.9-py3-none-any.whl

---
+++ packages/toolregistry/toolregistry-0.4.9-py3-none-any.whl/toolregistry/hub/websearch/__init__.py
@@ -0,0 +1,9 @@
+from .websearch import WebSearchGeneral
+from .websearch_google import WebSearchGoogle
+from .websearch_searxng import WebSearchSearxng
+
+__all__ = [
+    "WebSearchGeneral",
+    "WebSearchGoogle",
+    "WebSearchSearxng",
+]
