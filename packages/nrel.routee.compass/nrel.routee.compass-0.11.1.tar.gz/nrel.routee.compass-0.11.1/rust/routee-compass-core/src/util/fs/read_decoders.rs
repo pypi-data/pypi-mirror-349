1660a2b3f9bc37461fcadff1344e563f7aa353ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:16 +0000

    Add package nrel.routee.compass/0.11.1/nrel.routee.compass-0.11.1.tar.gz

---
+++ packages/nrel.routee.compass/nrel.routee.compass-0.11.1.tar.gz/nrel.routee.compass-0.11.1/rust/routee-compass-core/src/util/fs/read_decoders.rs
@@ -0,0 +1,22 @@
+/// default read decoder for an arbitrary type if the read operation
+/// can be performed via the FromStr trait for type T
+pub fn default<T>(_idx: usize, row: String) -> Result<T, std::io::Error>
+where
+    T: std::str::FromStr<Err = String>,
+{
+    row.parse::<T>().map_err(|e| {
+        let msg = format!("failure decoding row {} due to: {:}", row, e);
+        std::io::Error::new(std::io::ErrorKind::InvalidData, msg)
+    })
+}
+
+pub fn string(_idx: usize, row: String) -> Result<String, std::io::Error> {
+    Ok(row)
+}
+
+pub fn u8(_idx: usize, row: String) -> Result<u8, std::io::Error> {
+    row.parse::<u8>().map_err(|e| {
+        let msg = format!("failure decoding row {} due to: {:}", row, e);
+        std::io::Error::new(std::io::ErrorKind::InvalidData, msg)
+    })
+}
