333c387ac005470f8e61cb168de9a87a4113dfd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:36:24 +0000

    Add package oli-ai/0.3.7/oli_ai-0.3.7.tar.gz

---
+++ packages/oli-ai/oli_ai-0.3.7.tar.gz/oli_ai-0.3.7/src/oli_ai/mnist/mnist.py
@@ -0,0 +1,32 @@
+from keras.datasets import mnist
+from matplotlib import pyplot as plt
+import numpy as np
+from numpy import dot
+from numpy.linalg import norm
+
+def plot_imgs_labels(imgs, labels, rows=2, cols=5):
+    figure = plt.figure(figsize=(10, 3))
+    # plotting images from the training set
+    for i in range(0, rows*cols):
+        plt.subplot(rows, cols, i+1)
+        plt.subplots_adjust(hspace=1, wspace=1)
+        plt.title(f"Label: {labels[i]}")
+        img = imgs[i].reshape((28, 28))
+        plt.imshow(img, cmap='gray')       
+        # Rimuovere le indicazioni numeriche dagli assi
+        plt.xticks([])
+        plt.yticks([])
+
+def plot_imgs(imgs, labels, rows=2, cols=5):
+    figure = plt.figure(figsize=(10, 3))
+    # plotting images from the training set
+    for i in range(0, rows*cols):
+        plt.subplot(rows, cols, i+1)
+        plt.subplots_adjust(hspace=1, wspace=1)
+        #plt.title(f"Label: {labels[i]}")
+        img = imgs[i].reshape((28, 28))
+        plt.imshow(img, cmap='gray')
+        
+        # Rimuovere le indicazioni numeriche dagli assi
+        plt.xticks([])
+        plt.yticks([])
