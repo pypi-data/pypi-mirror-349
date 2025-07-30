a50f7a060f7b3802bb33036923056b2c7f382f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:35 +0000

    Add package django-necormancer/0.1.4/django_necormancer-0.1.4.tar.gz

---
+++ packages/django-necormancer/django_necormancer-0.1.4.tar.gz/django_necormancer-0.1.4/src/cli/utils/__init__.py
@@ -0,0 +1,9 @@
+from .create_project import create_project
+from .setup_package_manager import setup_package_manager
+from .setup_build_tools import setup_build_tools
+
+__all__ = [
+    'create_project',
+    'setup_package_manager',
+    'setup_build_tools'
+]
