6c28b7d35d64d7e5da90aa62c482a5e2bf238dcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:05 +0000

    Add package euclidlib/2025.2/euclidlib-2025.2.tar.gz

---
+++ packages/euclidlib/euclidlib-2025.2.tar.gz/euclidlib-2025.2/euclidlib/photo/__init__.py
@@ -0,0 +1,23 @@
+__all__ = [
+    "angular_power_spectra",
+    "correlation_functions",
+    "bandpowers",
+    "cosebis",
+    "mixing_matrices",
+    "redshift_distributions",
+]
+
+from ._le3_pk_wl import (
+    angular_power_spectra,
+    mixing_matrices,
+)
+
+from ._le3_2pcf_wl import (
+    correlation_functions,
+    bandpowers,
+    cosebis,
+)
+
+from ._phz import (
+    redshift_distributions,
+)
