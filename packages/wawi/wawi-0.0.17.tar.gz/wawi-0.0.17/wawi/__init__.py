374945d6fe434c967076cf3136e462e18229cf52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:13:29 +0000

    Add package wawi/0.0.17/wawi-0.0.17.tar.gz

---
+++ packages/wawi/wawi-0.0.17.tar.gz/wawi-0.0.17/wawi/__init__.py
@@ -0,0 +1,12 @@
+'''
+.. include:: ./../README.md
+'''
+__pdoc__ = {'wawi.ext.abq': False}
+
+__version__ = "0.0.17"
+
+# Other packages
+import numpy as np
+import shutil
+import csv
+from copy import deepcopy
