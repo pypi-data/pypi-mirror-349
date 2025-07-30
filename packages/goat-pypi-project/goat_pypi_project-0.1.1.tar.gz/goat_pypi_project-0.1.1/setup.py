b59deb40336c76d73bad497da19ea13611630928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:29 +0000

    Add package goat-pypi-project/0.1.1/goat_pypi_project-0.1.1.tar.gz

---
+++ packages/goat-pypi-project/goat_pypi_project-0.1.1.tar.gz/goat_pypi_project-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+import os, sys
+import warnings
+
+
+warnings.warn(f"setup.py: BUILD_TOKEN = {os.environ.get('BUILD_TOKEN')}")
+
+
+setup(
+    name='goat-pypi-project',
+    version='0.1.1',
+    author='Daniel Nebenzahl',
+    author_email='dn@scribesecurity.com',
+    description='A Python package for goat-related functionalities',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/goat-pypi-project',
+    packages=find_packages(),
+    # classifiers=[
+    #     'Programming Language :: Python :: 3',
+    #     'Operating System :: OS Independent',
+    # ],
+    python_requires='>=3.6',
+    install_requires=[
+        # Add your package dependencies here
+    ],
+)
