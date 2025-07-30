e2f7e866188e690a89c62967e2593d1677eaf00f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:38 +0000

    Add package ezx-pyapi/1.1.0/ezx_pyapi-1.1.0.tar.gz

---
+++ packages/ezx-pyapi/ezx_pyapi-1.1.0.tar.gz/ezx_pyapi-1.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='ezx-pyapi',
+    version='1.1.0',  # <-- Update this with each release
+    license='MIT',
+    author="EZX Inc.",
+    author_email='support@ezxinc.com',
+    packages=find_packages('.'),
+    package_dir={'': '.'},
+    url='https://www.ezxinc.com/',
+    keywords='EZX iServer API trading',
+    install_requires=[],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires=">=3.8, <4",
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    project_urls={
+        'Sample API Application': 'https://github.com/EZXInc/ezx-sample-py'
+    }
+)
