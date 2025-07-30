c28f97625c55d947b15476a347344ce2d0d40671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:00 +0000

    Add package geosight/0.1/geosight-0.1.tar.gz

---
+++ packages/geosight/geosight-0.1.tar.gz/geosight-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='geosight',
+    version='0.1',
+    author='Abdullah Ghifari',
+    install_requires=[
+        'python-geohash==0.8.5',
+    ],
+    py_modules = ['geosight']
+)
