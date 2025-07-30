d74baef4b645180bcad5107d7c50fb6da8e4f9da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:58 +0000

    Add package raynest/1.1.1/raynest-1.1.1.tar.gz

---
+++ packages/raynest/raynest-1.1.1.tar.gz/raynest-1.1.1/setup.py
@@ -0,0 +1,28 @@
+import os
+import platform
+import numpy
+from setuptools import setup, Extension, find_packages
+from Cython.Build import cythonize
+
+WINDOWS = platform.system().lower() == "windows"
+
+# set extension
+libraries = [] if WINDOWS else ["m"]
+ext_modules = [Extension("raynest.parameter",
+                         sources=[os.path.join("raynest", "parameter.pyx")],
+                         include_dirs=['raynest', numpy.get_include()],
+                         libraries=libraries,
+                         extra_compile_args=["-O3","-ffast-math","-ftree-vectorize"])]
+ext_modules = cythonize(ext_modules)
+
+setup(url="https://github.com/wdpozzo/raynest",  # Replace with your package URL
+      packages=find_packages(),
+      classifiers=[
+                'Development Status :: 4 - Beta',
+                'License :: OSI Approved :: MIT License',
+                'Programming Language :: Python :: 3.9'
+                'Programming Language :: Python :: 3.10'
+                ],
+      python_requires='>=3.9',
+      ext_modules=ext_modules)
+
