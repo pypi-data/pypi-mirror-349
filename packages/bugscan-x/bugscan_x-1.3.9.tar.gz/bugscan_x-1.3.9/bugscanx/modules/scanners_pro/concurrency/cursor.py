ad94f36a88e221344fdb01314ca3b2eb46b68db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:14:41 +0000

    Add package bugscan-x/1.3.9/bugscan_x-1.3.9.tar.gz

---
+++ packages/bugscan-x/bugscan_x-1.3.9.tar.gz/bugscan_x-1.3.9/bugscanx/modules/scanners_pro/concurrency/cursor.py
@@ -0,0 +1,7 @@
+class CursorManager:
+    def __enter__(self):
+        print('\033[?25l', end='', flush=True)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('\033[?25h', end='', flush=True)
