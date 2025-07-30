5c633e2e6f692d7d8568e66cbfbd491046359294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:46 +0000

    Add package domible/0.1.18/domible-0.1.18.tar.gz

---
+++ packages/domible/domible-0.1.18.tar.gz/domible-0.1.18/src/domible/builders/__init__.py
@@ -0,0 +1,8 @@
+""" domible/src/domible/builders/__init__.py """
+
+from domible.builders.tableBuilder import RowBuilder, TableBuilder, build_table_from_dicts
+from domible.builders.navBuilder import NavBuilder 
+from domible.builders.elementFromObject import element_from_object  
+from domible.builders.formBuilder import ToggleDetailsButton, default_toggle_details_button  
+
+# end of file
