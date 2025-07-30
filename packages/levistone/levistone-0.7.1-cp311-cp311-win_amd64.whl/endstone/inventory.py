6b65e912d72e912986511ca36d33a58d6280414f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:35 +0000

    Add package levistone/0.7.1/levistone-0.7.1-cp311-cp311-win_amd64.whl

---
+++ packages/levistone/levistone-0.7.1-cp311-cp311-win_amd64.whl/endstone/inventory.py
@@ -0,0 +1,3 @@
+from endstone._internal.endstone_python import Inventory, ItemMeta, ItemStack, MapMeta, PlayerInventory
+
+__all__ = ["ItemStack", "ItemMeta", "Inventory", "MapMeta", "PlayerInventory"]
