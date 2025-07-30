367e74042e6d2df08b7b7dd13f39fdf384248c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:26 +0000

    Add package auto-sklearn2/1.0.0/auto_sklearn2-1.0.0-py3-none-any.whl

---
+++ packages/auto-sklearn2/auto_sklearn2-1.0.0-py3-none-any.whl/auto_sklearn2/__init__.py
@@ -0,0 +1,9 @@
+"""
+Auto-Sklearn2 - A Python 3.11+ compatible version of auto-sklearn
+"""
+
+from .classifier import AutoSklearnClassifier
+from .regressor import AutoSklearnRegressor
+
+__version__ = "1.0.0"
+__all__ = ["AutoSklearnClassifier", "AutoSklearnRegressor"]
