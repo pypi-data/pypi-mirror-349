b58b4e5c0d0496af3a13979c5d4fde13a1b15b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:37 +0000

    Add package feasytools/0.0.30/feasytools-0.0.30-py3-none-any.whl

---
+++ packages/feasytools/feasytools-0.0.30-py3-none-any.whl/feasytools/perf.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+from functools import wraps
+import time
+
+
+class _perf:
+    RES = defaultdict(lambda: 0.0)
+
+    def __call__(self, func):
+        @wraps(func)
+        def func_wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            execution_time = end_time - start_time
+            _perf.RES[func.__name__] += execution_time
+            return result
+
+        return func_wrapper
+
+
+FEasyTimer = _perf()
