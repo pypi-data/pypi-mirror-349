5a84a8fce3b1e36f9176f397c6bcc47a7ffb78a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:54 +0000

    Add package progrich/0.1.0/progrich-0.1.0-py3-none-any.whl

---
+++ packages/progrich/progrich-0.1.0-py3-none-any.whl/progrich/__init__.py
@@ -0,0 +1,15 @@
+from . import columns, fmt
+from .manager import ManagedWidget, Manager
+from .pbar import ProgressBar
+from .spinner import Spinner
+from .widget import Widget
+
+__all__ = [
+    "fmt",
+    "columns",
+    "Widget",
+    "Manager",
+    "ManagedWidget",
+    "ProgressBar",
+    "Spinner",
+]
