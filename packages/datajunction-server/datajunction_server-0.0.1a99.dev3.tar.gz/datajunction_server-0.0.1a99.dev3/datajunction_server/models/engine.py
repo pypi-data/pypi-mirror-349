1fe2de5a9ceda1ae2463e695319611db40042f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:53 +0000

    Add package datajunction-server/0.0.1a99.dev3/datajunction_server-0.0.1a99.dev3.tar.gz

---
+++ packages/datajunction-server/datajunction_server-0.0.1a99.dev3.tar.gz/datajunction_server-0.0.1a99.dev3/datajunction_server/models/engine.py
@@ -0,0 +1,42 @@
+"""
+Models for columns.
+"""
+
+from typing import Optional
+
+from pydantic.main import BaseModel
+
+from datajunction_server.enum import StrEnum
+
+
+class Dialect(StrEnum):
+    """
+    SQL dialect
+    """
+
+    SPARK = "spark"
+    TRINO = "trino"
+    DRUID = "druid"
+
+
+class EngineInfo(BaseModel):
+    """
+    Class for engine creation
+    """
+
+    name: str
+    version: str
+    uri: Optional[str]
+    dialect: Optional[Dialect]
+
+    class Config:
+        orm_mode = True
+
+
+class EngineRef(BaseModel):
+    """
+    Basic reference to an engine
+    """
+
+    name: str
+    version: str
