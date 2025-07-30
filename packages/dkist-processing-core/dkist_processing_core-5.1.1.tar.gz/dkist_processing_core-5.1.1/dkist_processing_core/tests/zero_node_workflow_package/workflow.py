948168f4bffe01e919b7b20a966f87d67d98856b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:10:49 +0000

    Add package dkist-processing-core/5.1.1/dkist_processing_core-5.1.1.tar.gz

---
+++ packages/dkist-processing-core/dkist_processing_core-5.1.1.tar.gz/dkist_processing_core-5.1.1/dkist_processing_core/tests/zero_node_workflow_package/workflow.py
@@ -0,0 +1,9 @@
+"""Workflow containing zero nodes."""
+from dkist_processing_core import Workflow
+
+zero_node_workflow = Workflow(
+    input_data="test-data",
+    output_data="zero-node",
+    category="core",
+    workflow_package=__package__,
+)
