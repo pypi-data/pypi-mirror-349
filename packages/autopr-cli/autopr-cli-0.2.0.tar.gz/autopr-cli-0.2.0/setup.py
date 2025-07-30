db53ec7fb736f85f8e6b2b1b6517054e5f7c3f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:33 +0000

    Add package autopr-cli/0.2.0/autopr-cli-0.2.0.tar.gz

---
+++ packages/autopr-cli/autopr-cli-0.2.0.tar.gz/autopr-cli-0.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autopr-cli',
+    version='0.2.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'aipr=run_cli:main',
+        ],
+    },
+    install_requires=[],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A CLI tool to automate PR creation and listing.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/leaopedro/autopr',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
