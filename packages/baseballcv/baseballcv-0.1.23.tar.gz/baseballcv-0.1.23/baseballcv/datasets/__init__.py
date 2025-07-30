43d620f1e3d9113604ade63b97b81fa41693fed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:10 +0000

    Add package baseballcv/0.1.23/baseballcv-0.1.23.tar.gz

---
+++ packages/baseballcv/baseballcv-0.1.23.tar.gz/baseballcv-0.1.23/baseballcv/datasets/__init__.py
@@ -0,0 +1,8 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+from .processing.datasets_processor import DataProcessor
+from .formats.datasets_coco_detection import CocoDetectionDataset
+from .formats.datasets_jsonl_detection import JSONLDetection
+
+__all__ = ['DataProcessor', 'CocoDetectionDataset', 'JSONLDetection']
