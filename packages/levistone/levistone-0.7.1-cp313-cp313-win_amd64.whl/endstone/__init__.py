8c7797d872c23d301b225023b1e4831e97bdef6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:37 +0000

    Add package levistone/0.7.1/levistone-0.7.1-cp313-cp313-win_amd64.whl

---
+++ packages/levistone/levistone-0.7.1-cp313-cp313-win_amd64.whl/endstone/__init__.py
@@ -0,0 +1,16 @@
+from endstone._internal.endstone_python import ColorFormat, GameMode, Logger, OfflinePlayer, Player, Server, Skin
+from endstone._internal.version import __version__
+
+__minecraft_version__ = "1.21.70"
+
+__all__ = [
+    "__version__",
+    "__minecraft_version__",
+    "ColorFormat",
+    "GameMode",
+    "Logger",
+    "OfflinePlayer",
+    "Player",
+    "Server",
+    "Skin",
+]
---
+++ packages/levistone/levistone-0.7.1-cp313-cp313-win_amd64.whl/endstone/form.py
@@ -0,0 +1,13 @@
+from endstone._internal.endstone_python import (
+    ActionForm,
+    Dropdown,
+    Label,
+    MessageForm,
+    ModalForm,
+    Slider,
+    StepSlider,
+    TextInput,
+    Toggle,
+)
+
+__all__ = ["ActionForm", "MessageForm", "ModalForm", "Dropdown", "Label", "Slider", "StepSlider", "TextInput", "Toggle"]
