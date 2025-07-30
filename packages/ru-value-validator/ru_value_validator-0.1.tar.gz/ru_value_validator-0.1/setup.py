cb89bfb487aee54d17a52484e78bbec4d1b5eb78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:43 +0000

    Add package ru-value-validator/0.1/ru_value_validator-0.1.tar.gz

---
+++ packages/ru-value-validator/ru_value_validator-0.1.tar.gz/ru_value_validator-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ru-value-validator",
+    version="0.1",
+    packages=find_packages(),
+    description="Валидация для российских идентификаторов",
+    author="Кудрин Артем",
+    author_email="yorkwilsa@gmail.com",
+    python_requires='>=3.6'
+)
---
+++ packages/ru-value-validator/ru_value_validator-0.1.tar.gz/ru_value_validator-0.1/validators/__init__.py
@@ -0,0 +1,4 @@
+from .OgrnValidator import OgrnValidator
+from .SnilsValidator import SnilsValidator
+
+__all__ = ['OgrnValidator', 'SnilsValidator']
