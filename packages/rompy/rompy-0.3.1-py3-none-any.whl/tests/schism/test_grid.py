0211bd1834233c68de3e89a051e1cdf5ad6ad1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:51 +0000

    Add package rompy/0.3.1/rompy-0.3.1-py3-none-any.whl

---
+++ packages/rompy/rompy-0.3.1-py3-none-any.whl/tests/schism/test_grid.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+from importlib.metadata import entry_points
+import pytest
+
+pytest.importorskip("rompy.schism")
+
+from rompy.core.data import DataBlob
+from rompy.core.grid import BaseGrid
+from rompy.schism import SCHISMGrid
+from rompy.schism.grid import WWMBNDGR3Generator
+
+here = Path(__file__).parent
+
+eps = entry_points(group="rompy.config")
+
+@pytest.mark.skipif("schism" not in eps.names, reason="requires SCHISM")
+def test_SCHISMGrid2D(tmpdir):
+    hgrid = DataBlob(source=here / "test_data/hgrid.gr3")
+    # drag = DataBlob(source=here / "test_data/drag.gr3")
+    # rough = DataBlob(source=here / "test_data/rough.gr3")
+    # manning = DataBlob(source=here / "test_data/manning.gr3")
+    # hgridll = DataBlob(source=here / "test_data/hgrid.ll")
+    # diffmin = DataBlob(source=here / "test_data/diffmin.gr3")
+    # diffmax = DataBlob(source=here / "test_data/diffmax.gr3")
+    # hgrid_WWM = DataBlob(source=here / "test_data/hgrid_WWM.gr3")
+    # wwmbnd = DataBlob(source=here / "test_data/wwmbnd.gr3")
+
+    grid = SCHISMGrid(
+        hgrid=hgrid,
+        drag=1,
+        # rough=rough,
+        # manning=1.2,
+        # hgridll=hgridll,
+        # diffmin=diffmin,
+        # diffmax=diffmax,
+        # hgrid_WWM=hgrid_WWM,
+        # wwmbnd=wwmbnd,
+    )
+
+    # assert grid.is_3d == False
+    # # assert grid.drag == drag
+    # # assert grid.rough == rough
+    # assert grid.manning == manning
+    # assert grid.hgridll == hgridll
+    # assert grid.diffmin == diffmin
+    # assert grid.diffmax == diffmax
+    # assert grid.hgrid_WWM == hgrid_WWM
+    # assert grid.wwmbnd == wwmbnd
+
+    assert grid.validate_rough_drag_manning(grid) == grid
+    # assert that the gr3 file for each of the above is in the staging dir
+    staging_dir = Path(tmpdir)
+    ret = grid.get(staging_dir)
+
+    assert staging_dir.joinpath("hgrid.gr3").exists()
+    assert staging_dir.joinpath("hgrid.ll").exists()
+    assert staging_dir.joinpath("hgrid.ll").is_symlink()
+    assert staging_dir.joinpath("hgrid_WWM.gr3").is_symlink()
+    assert staging_dir.joinpath("diffmin.gr3").exists()
+    assert staging_dir.joinpath("diffmax.gr3").exists()
+    assert staging_dir.joinpath("tvd.prop").exists()
+    assert staging_dir.joinpath("vgrid.in").exists()
+
+
+# def test_generate_wwmbnd():
+#     hgrid = "test_data/hgrid.gr3"
+#     wwmbnd = WWMBNDGR3Generator(hgrid=hgrid)
+#     wwmbnd.get("./")
+#
+#     # assert contents of wwmbnd.gr3 and wwmbnd_ref.gr3 are the same
+#     with open("wwmbnd.gr3", "r") as f:
+#         wwmbnd_lines = f.readlines()
+#     with open("wwmbnd_ref.gr3", "r") as f:
+#         wwmbnd_ref_lines = f.readlines()
+#     assert wwmbnd_lines == wwmbnd_ref_lines
---
+++ packages/rompy/rompy-0.3.1-py3-none-any.whl/tests/schism/test_schism_data.py
@@ -0,0 +1,115 @@
+import os
+from pathlib import Path
+
+import pytest
+
+pytest.importorskip("rompy.schism")
+import xarray as xr
+
+from rompy.core.data import DataBlob
+from rompy.core.time import TimeRange
+from rompy.core.source import SourceFile, SourceIntake
+from rompy.schism import SCHISMGrid
+from rompy.schism.data import (
+    SCHISMDataBoundary,
+    SCHISMDataOcean,
+    SCHISMDataSflux,
+    SCHISMDataTides,
+    SfluxAir,
+    TidalDataset,
+)
+
+HERE = Path(__file__).parent
+DATAMESH_TOKEN = os.environ.get("DATAMESH_TOKEN")
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+@pytest.fixture
+def grid2d():
+    return SCHISMGrid(hgrid=DataBlob(source=HERE / "test_data/hgrid.gr3"), drag=1)
+
+
+@pytest.fixture
+def grid_atmos_source():
+    return SourceIntake(
+        dataset_id="era5",
+        catalog_uri=HERE / ".." / "data" / "catalog.yaml",
+    )
+
+
+@pytest.fixture
+def hycom_bnd():
+    hycomdata = HERE / "test_data" / "hycom.nc"
+    if not hycomdata.exists():
+        from utils import download_hycom
+
+        logging.info("Hycom test data not found, downloading...")
+        logging.info("This may take a while...only has to be done once.")
+        download_hycom(dest=HERE / "test_data", hgrid=HERE / "test_data" / "hgrid.gr3")
+    return SCHISMDataBoundary(
+        id="hycom",
+        source=SourceFile(
+            uri=HERE / "test_data" / "hycom.nc",
+        ),
+        variable="surf_el",
+        coords={"t": "time", "y": "ylat", "x": "xlon", "z": "depth"},
+    )
+
+
+def test_atmos(tmp_path, grid_atmos_source):
+    data = SCHISMDataSflux(
+        air_1=SfluxAir(
+            id="air_1",
+            source=grid_atmos_source,
+            uwind_name="u10",
+            vwind_name="v10",
+            filter={
+                "sort": {"coords": ["latitude"]},
+                "crop": {
+                    "time": slice("2023-01-01", "2023-01-02"),
+                    "latitude": slice(0, 20),
+                    "longitude": slice(0, 20),
+                },
+            },
+        )
+    )
+    data.get(tmp_path)
+
+
+def test_oceandataboundary(tmp_path, grid2d, hycom_bnd):
+    hycom_bnd.get(tmp_path, grid2d)
+    with xr.open_dataset(tmp_path / "hycom.th.nc") as bnd:
+        assert "one" in bnd.dims
+        assert "time" in bnd.dims
+        assert "nOpenBndNodes" in bnd.dims
+        assert "nLevels" in bnd.dims
+        assert "nComponents" in bnd.dims
+        assert len(bnd.nOpenBndNodes) == len(grid2d.ocean_boundary()[0])
+
+
+def test_oceandata(tmp_path, grid2d, hycom_bnd):
+    oceandata = SCHISMDataOcean(elev2D=hycom_bnd)
+    oceandata.get(tmp_path, grid2d)
+
+
+def test_tidal_boundary(tmp_path, grid2d):
+    if not (HERE / "test_data" / "tpxo9-neaus" / "h_m2s2n2.nc").exists():
+        from utils import untar_file
+
+        untar_file(HERE / "test_data" / "tpxo9-neaus.tar.gz", HERE / "test_data/")
+    from utils import untar_file
+
+    tides = SCHISMDataTides(
+        tidal_data=TidalDataset(
+            elevations=HERE / "test_data" / "tpxo9-neaus" / "h_m2s2n2.nc",
+            velocities=HERE / "test_data" / "tpxo9-neaus" / "u_m2s2n2.nc",
+        ),
+        constituents=["M2", "S2", "N2"],
+    )
+    tides.get(
+        destdir=tmp_path,
+        grid=grid2d,
+        time=TimeRange(start="2023-01-01", end="2023-01-02", dt=3600),
+    )
