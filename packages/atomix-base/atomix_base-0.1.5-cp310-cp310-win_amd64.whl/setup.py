653ab15aac561386a1ae36cdd457a44fc0852244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:02 +0000

    Add package atomix-base/0.1.5/atomix_base-0.1.5-cp310-cp310-win_amd64.whl

---
+++ packages/atomix-base/atomix_base-0.1.5-cp310-cp310-win_amd64.whl/setup.py
@@ -0,0 +1,30 @@
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
+
+__version__ = "0.1.5"
+
+ext_modules = [
+    Pybind11Extension(
+        "atomix_base",
+        ["src/main.cpp"],
+        define_macros=[("VERSION_INFO", __version__)],
+        cxx_std=20,
+        # extra_compile_args=['/O2'],
+    ),
+]
+
+setup(
+    name="atomix_base",
+    version=__version__,
+    author="electroglyph",
+    author_email="admin@terminoid.com",
+    url="https://github.com/electroglyph/pyatomix",
+    description="atomics for pyatomix",
+    long_description="",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+    python_requires=">=3.7",
+    packages=[""],
+    package_data={"": ["atomix_base.pyi"]},
+)
