12ea4bcd2a7662c04fd70d9fc94fb2dde34c555c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:24 +0000

    Add package AIModeler/0.9/aimodeler-0.9-py3-none-any.whl

---
+++ packages/AIModeler/aimodeler-0.9-py3-none-any.whl/aimodeler/__init__.py
@@ -0,0 +1,7 @@
+from . import document_parsing
+from .document_parsing import extract_text
+
+from . import text_chunking
+from .text_chunking import chunk_text
+
+__all__ = ["extract_text", "chunk_text"]
