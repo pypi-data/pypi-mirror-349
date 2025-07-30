db212e9230f89a23e4f0a770698f0367a2ed8800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:07:43 +0000

    Add package faststream/0.5.42/faststream-0.5.42-py3-none-any.whl

---
+++ packages/faststream/faststream-0.5.42-py3-none-any.whl/faststream/redis/__init__.py
@@ -0,0 +1,29 @@
+try:
+    from faststream.testing.app import TestApp
+
+    from .annotations import Redis, RedisMessage
+    from .broker.broker import RedisBroker
+    from .response import RedisResponse
+    from .router import RedisPublisher, RedisRoute, RedisRouter
+    from .schemas import ListSub, PubSub, StreamSub
+    from .testing import TestRedisBroker
+
+except ImportError as e:
+    from faststream.exceptions import INSTALL_FASTSTREAM_REDIS
+
+    raise ImportError(INSTALL_FASTSTREAM_REDIS) from e
+
+__all__ = (
+    "ListSub",
+    "PubSub",
+    "Redis",
+    "RedisBroker",
+    "RedisMessage",
+    "RedisPublisher",
+    "RedisResponse",
+    "RedisRoute",
+    "RedisRouter",
+    "StreamSub",
+    "TestApp",
+    "TestRedisBroker",
+)
