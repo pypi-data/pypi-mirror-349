0e36070adff8c7359d680a3dbee4cfe08cca8a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:04:59 +0000

    Add package shepherd-core/2025.5.3/shepherd_core-2025.5.3-py3-none-any.whl

---
+++ packages/shepherd-core/shepherd_core-2025.5.3-py3-none-any.whl/shepherd_core/commons.py
@@ -0,0 +1,8 @@
+"""Container for commonly shared constants."""
+
+SAMPLERATE_SPS_DEFAULT: int = 100_000
+
+UID_NAME: str = "SHEPHERD_NODE_ID"
+UID_SIZE: int = 2
+
+TESTBED_SERVER_URI: str = "http://127.0.0.1:8000/shepherd"
