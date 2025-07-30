a4a3d422fa42c065e3c3322f60739906b6f2d51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:14 +0000

    Add package evapro/0.1.3/evapro-0.1.3.tar.gz

---
+++ packages/evapro/evapro-0.1.3.tar.gz/evapro-0.1.3/evapro/__init__.py
@@ -0,0 +1,11 @@
+"""Evapro - Automated add projects to annoeva Workflow Monitoring System
+"""
+
+from .scripts.evaproject import main
+
+__version__ = "0.1.3"
+__author__ = "Zan Yuan <yfinddream@gmail.com>"
+__license__ = "MIT"
+
+__all__ = ["main"]
+
