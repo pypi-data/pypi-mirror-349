c5b9d25cb8530b46e50d1b1b38a895dfd22b755e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:32 +0000

    Add package fastlink/0.1.4/fastlink-0.1.4-py3-none-any.whl

---
+++ packages/fastlink/fastlink-0.1.4-py3-none-any.whl/fastlink/__init__.py
@@ -0,0 +1,11 @@
+from fastlink.base.client import FastLink
+from fastlink.google.provider import GoogleSSO
+from fastlink.telegram.provider import TelegramSSO
+from fastlink.yandex.provider import YandexSSO
+
+__all__ = [
+    "FastLink",
+    "GoogleSSO",
+    "TelegramSSO",
+    "YandexSSO",
+]
---
+++ packages/fastlink/fastlink-0.1.4-py3-none-any.whl/fastlink/jwt/manager.py
@@ -0,0 +1,58 @@
+import datetime
+from typing import TYPE_CHECKING
+
+import jwt
+from jwt import InvalidTokenError as JWTInvalidTokenError
+
+from fastlink.exceptions import InvalidTokenTypeError, TokenError
+from fastlink.jwt.schemas import JWTConfig, JWTPayload
+
+if TYPE_CHECKING:
+    from collections.abc import MutableMapping
+
+
+class JWTManager:
+    def __init__(self, *config: JWTConfig) -> None:
+        self.config: MutableMapping[str, JWTConfig] = {t.type: t for t in config}
+
+    def create(self, token_type: str, payload: JWTPayload) -> str:
+        config = self.config[token_type]
+        now = datetime.datetime.now(datetime.UTC)
+        claims = dict(
+            iss=config.issuer,
+            typ=token_type,
+            iat=now,
+            **payload.model_dump(exclude_none=True),
+        )
+        if config.expires_in is not None:
+            claims["exp"] = now + config.expires_in
+        return jwt.encode(
+            claims,
+            config.private_key,
+            algorithm=config.algorithm,
+        )
+
+    def validate(
+        self,
+        token_type: str,
+        token: str,
+    ) -> JWTPayload:
+        params = self.config[token_type]
+        try:
+            decoded = jwt.decode(
+                token,
+                params.public_key,
+                algorithms=[params.algorithm],
+                issuer=params.issuer,
+            )
+        except JWTInvalidTokenError as e:
+            raise TokenError from e
+        if decoded["typ"] != token_type:
+            raise InvalidTokenTypeError
+        return JWTPayload.model_validate(decoded)
+
+    def get_lifetime(self, token_type: str) -> int | None:
+        expires_in = self.config[token_type].expires_in
+        if expires_in is None:
+            return None
+        return int(expires_in.total_seconds())
