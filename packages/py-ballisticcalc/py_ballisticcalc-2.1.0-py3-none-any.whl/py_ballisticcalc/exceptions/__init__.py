f4611f74e901787e414e7e8cbb79b6715a2153ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:43 +0000

    Add package py-ballisticcalc/2.1.0/py_ballisticcalc-2.1.0-py3-none-any.whl

---
+++ packages/py-ballisticcalc/py_ballisticcalc-2.1.0-py3-none-any.whl/py_ballisticcalc/exceptions/__init__.py
@@ -0,0 +1,12 @@
+"""py_ballisticcalc exception types"""
+
+from py_ballisticcalc.exceptions.exceptions import (
+    UnitTypeError, UnitConversionError, UnitAliasError, ZeroFindingError, RangeError)
+
+__all__ = (
+    'UnitTypeError',
+    'UnitConversionError',
+    'UnitAliasError',
+    'ZeroFindingError',
+    'RangeError',
+)
