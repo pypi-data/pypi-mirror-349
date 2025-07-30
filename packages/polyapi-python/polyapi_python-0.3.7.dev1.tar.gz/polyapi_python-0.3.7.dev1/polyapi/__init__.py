9b068ce7209877b0e0518bb904d1042f69d88a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:10:27 +0000

    Add package polyapi-python/0.3.7.dev1/polyapi_python-0.3.7.dev1.tar.gz

---
+++ packages/polyapi-python/polyapi_python-0.3.7.dev1.tar.gz/polyapi_python-0.3.7.dev1/polyapi/__init__.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import truststore
+from typing import Dict, Any
+truststore.inject_into_ssl()
+from .cli import CLI_COMMANDS
+
+__all__ = ["poly"]
+
+
+if len(sys.argv) > 1 and sys.argv[1] not in CLI_COMMANDS:
+    currdir = os.path.dirname(os.path.abspath(__file__))
+    if not os.path.isdir(os.path.join(currdir, "poly")):
+        print("No 'poly' found. Please run 'python3 -m polyapi generate' to generate the 'poly' library for your tenant.")
+        sys.exit(1)
+
+
+polyCustom: Dict[str, Any] = {
+    "executionId": None,
+    "executionApiKey": None,
+    "responseStatusCode": 200,
+    "responseContentType": None,
+}
