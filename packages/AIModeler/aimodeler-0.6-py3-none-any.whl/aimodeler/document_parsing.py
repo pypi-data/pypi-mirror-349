b79292e554578c0dd73efbb7dd0f1235bb3001f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:16:12 +0000

    Add package AIModeler/0.6/aimodeler-0.6-py3-none-any.whl

---
+++ packages/AIModeler/aimodeler-0.6-py3-none-any.whl/aimodeler/document_parsing.py
@@ -0,0 +1,15 @@
+import os
+import fitz  # PyMuPDF
+
+PDF_FOLDER = "pdfs"
+
+# Function to extract text from PDFs
+def extract_text(pdf_folder):
+    filenames, texts = [], []
+    for filename in os.listdir(pdf_folder):
+        if filename.endswith(".pdf"):
+            document = fitz.open(os.path.join(pdf_folder, filename))
+            text = "\n".join(page.get_text() for page in document)
+            texts.append(text)
+            filenames.append(filename)
+    return filenames, texts
