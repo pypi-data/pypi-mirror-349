61a021d642c6a776c7fee2df1a06bad1bf72ed3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:22 +0000

    Add package sit-timeline-logging/0.0.5/sit_timeline_logging-0.0.5.tar.gz

---
+++ packages/sit-timeline-logging/sit_timeline_logging-0.0.5.tar.gz/sit_timeline_logging-0.0.5/README.md
@@ -0,0 +1,12 @@
+## Pre-requisite
+- Python >= 3.8
+- virtualenv
+
+## Create environments (venv) 
+```
+git clone https://git.dev.somapait.com/ai-chatbot/project-template/ai-api-python.git
+cd ai-api-python
+python -m virtualenv venv
+.\venv\scripts\activate
+pip install -r requirements.txt
+```
---
+++ packages/sit-timeline-logging/sit_timeline_logging-0.0.5.tar.gz/sit_timeline_logging-0.0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="sit-timeline-logging",
+    version="0.0.5",
+    author="anusorn.l",
+    author_email="anusorn.l@somapait.com",
+    description="SIT timeline logging package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    package_data={
+        "app.resource": ["*.json"],  # ใช้ * ถ้า resource อยู่ใน path แบบ module
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+     ],
+ )
