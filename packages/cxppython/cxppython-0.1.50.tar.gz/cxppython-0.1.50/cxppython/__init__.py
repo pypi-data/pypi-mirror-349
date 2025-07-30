c3ee21623a5a9babfdeb423a92639a465cbf9a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:23 +0000

    Add package cxppython/0.1.50/cxppython-0.1.50.tar.gz

---
+++ packages/cxppython/cxppython-0.1.50.tar.gz/cxppython-0.1.50/cxppython/__init__.py
@@ -0,0 +1,12 @@
+from .utils.cxplogging import logging
+from .utils.easy_imports import *
+from .utils.database import db
+from .utils.database import MysqlDB
+debug()
+
+database_dict={}
+def add_db(name,database):
+    database_dict[name]=database
+def get_db(name):
+    return database_dict[name]
+
