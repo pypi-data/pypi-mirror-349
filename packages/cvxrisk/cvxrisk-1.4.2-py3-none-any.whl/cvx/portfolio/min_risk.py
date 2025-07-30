58d5b0aaf836cbf928a5ce3ca19b834e73ecb48a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:36:40 +0000

    Add package cvxrisk/1.4.2/cvxrisk-1.4.2-py3-none-any.whl

---
+++ packages/cvxrisk/cvxrisk-1.4.2-py3-none-any.whl/cvx/portfolio/min_risk.py
@@ -0,0 +1,36 @@
+#    Copyright 2023 Stanford University Convex Optimization Group
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+from __future__ import annotations
+
+import cvxpy as cp
+
+
+def minrisk_problem(riskmodel, weights, **kwargs):
+    """
+    Create a minimum risk portfolio optimization problem.
+
+    Args:
+        riskmodel: Risk model that implements the Model interface
+        weights: CVXPY variable representing portfolio weights
+        **kwargs: Additional keyword arguments to pass to the risk model
+
+    Returns:
+        cp.Problem: A CVXPY problem that minimizes the risk subject to constraints
+    """
+    problem = cp.Problem(
+        cp.Minimize(riskmodel.estimate(weights, **kwargs)),
+        [cp.sum(weights) == 1.0, weights >= 0] + riskmodel.constraints(weights, **kwargs),
+    )
+
+    return problem
