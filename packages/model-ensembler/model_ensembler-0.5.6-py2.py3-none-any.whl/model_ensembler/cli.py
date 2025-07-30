49cc1b5beaf580d61fc1b425d57965448c89c488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:31 +0000

    Add package model-ensembler/0.5.6/model_ensembler-0.5.6-py2.py3-none-any.whl

---
+++ packages/model-ensembler/model_ensembler-0.5.6-py2.py3-none-any.whl/model_ensembler/cli.py
@@ -0,0 +1,123 @@
+import argparse
+import logging
+import os
+import re
+
+from .config import EnsembleConfig
+from .batcher import BatchExecutor
+# TODO: logging and parse_args should be in utils
+from .utils import Arguments, background_fork, setup_logging
+
+"""CLI entry module
+
+This module contains all package entry_points
+"""
+
+
+def parse_indexes(argv):
+    """ Method for ensuring a CSV string of integers
+
+    Returns:
+        list: List of integer values
+    Raises:
+        argparse.ArgumentTypeError
+    """
+    if re.match(r'^([0-9]+,)*[0-9]+$', argv):
+        return [int(v) for v in argv.split(",")]
+    raise argparse.ArgumentTypeError("{} is not a CSV delimited integer list "
+                                     "of indexes".format(argv))
+
+
+def parse_extra_vars(arg):
+    """ Method for processing extra var arguments
+
+    Returns:
+        tuple: name and value for the argument to be overridden
+    Raises:
+        argparse.ArgumentTypeError
+    """
+
+    arg_match = re.match(r'^([^=]+)=(.+)$', arg)
+    if arg_match:
+        return arg_match.groups()
+    raise argparse.ArgumentTypeError("Argument does not match "
+                                      "name=value format: {}".format(arg))
+
+
+def parse_args():
+    """Parse command line parameters
+
+    Returns:
+        Arguments: The immutable instance from ``.utils``
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--daemon",
+                        help="Daemonise the ensembler", default=False,
+                        action="store_true")
+
+    # TODO: Need to validate the argument selections/group certain commands
+    parser.add_argument("-v", "--verbose",
+                        help="Log verbosely", default=False,
+                        action="store_true")
+    parser.add_argument("-c", "--no-checks",
+                        help="Do not run check commands", default=False,
+                        action="store_true")
+    parser.add_argument("-s", "--no-submission",
+                        help="Do not try to submit job, just log the step",
+                        default=False, action="store_true")
+    parser.add_argument("-p", "--pickup",
+                        help="Continue a previous set of runs by picking up "
+                        "existing directories rather, than assuming to create "
+                        "them; for example if ensemble has previously failed",
+                        default=False, action="store_true")
+    parser.add_argument("-l", "--shell",
+                        help="Allows the user to specify the shell passed to "
+                             "subprocess execs.",
+                        default="/bin/bash", type=str)
+
+    # FIXME: These should not be applied in multi-batch ensembles
+    parser.add_argument("-k", "--skips",
+                        help="Number of run entries to skip", default=0,
+                        type=int)
+
+    parser.add_argument("-i", "--indexes",
+                        help="Specify which indexes to run",
+                        type=parse_indexes)
+
+    parser.add_argument("-ct", "--check-timeout", default=30, type=int)
+    parser.add_argument("-st", "--submit-timeout", default=20, type=int)
+    parser.add_argument("-rt", "--running-timeout", default=60, type=int)
+    parser.add_argument("-et", "--error-timeout", default=120, type=int)
+
+    parser.add_argument("-ms", "--max-stagger", default=1, type=int)
+
+    parser.add_argument("-x", "--extra-vars", dest="extra", nargs="*",
+                        default=[], type=parse_extra_vars)
+
+    parser.add_argument("configuration")
+    parser.add_argument("backend", default="slurm", choices=("slurm", "dummy"),
+                        nargs="?")
+
+    # Prefer retaining immutable Arguments()
+    # by not using the instance as a namespace
+    return Arguments(**vars(parser.parse_args()))
+
+
+def main():
+    """CLI entry point
+    """
+    args = parse_args()
+
+    if args.daemon:
+        background_fork(True)
+
+    setup_logging("{}".format(os.path.basename(args.configuration)),
+                  verbose=args.verbose)
+
+    logging.info("Model Ensemble Runner")
+
+    config = EnsembleConfig(args.configuration)
+    # TODO: get_batch_executor
+    BatchExecutor(config,
+                  args.backend,
+                  dict(args.extra)).run()
---
+++ packages/model-ensembler/model_ensembler-0.5.6-py2.py3-none-any.whl/model_ensembler/cluster/slurm.py
@@ -0,0 +1,108 @@
+import asyncio
+import collections
+import logging
+import os
+import random
+import re
+import time
+
+from pprint import pformat
+
+from model_ensembler.tasks.utils import execute_command
+from model_ensembler.cluster import Job, job_lock
+from model_ensembler.utils import Arguments
+
+START_STATES = ("COMPLETING", "PENDING", "RESV_DEL_HOLD", "RUNNING",
+                "SUSPENDED", "CONFIGURING", "REQUEUE_FED", "REQUEUE_HOLD",
+                "REQUEUED", "RESIZING", "REVOKED", "SIGNALED", "STOPPED",
+                "FAILED")
+FINISH_STATES = ("COMPLETED", "FAILED", "CANCELLED", "OUT_OF_MEMORY",
+                 "DEADLINE", "NODE_FAIL", "PREEMPTED", "TIMEOUT")
+
+
+async def find_id(job_id):
+    """Method to find SLURM job by ID
+
+    This method provides an interface to the squeue SLURM queue utility to
+    identify a job and return it along with it's state
+
+    Args:
+        job_id (int): SLURM job identifier
+
+    Returns:
+        jobs (list): job objects including name and state
+    """
+    job = None
+    args = Arguments()
+
+    while not job:
+        try:
+            res = await execute_command("sacct -XnP -j {} "
+                                        "-o jobname,state,start,end".
+                                        format(job_id))
+            output = res.stdout.decode().strip()
+            (name, state, started, finished) = output.split("|")
+        except ValueError:
+            logging.warning("Could not retrieve job from list")
+            await asyncio.sleep(args.check_timeout)
+        else:
+            job = Job(
+                name=name,
+                state=state.split()[0],
+                started=started == "Unknown",
+                finished=finished == "Unknown"
+            )
+
+    logging.debug("SLURM find result name: {}".format(job.name))
+    return job
+
+
+async def current_jobs(ctx, match):
+    filtered_jobs = None
+
+    # Ensure we account for empty lists
+    while not filtered_jobs and filtered_jobs is None:
+        try:
+            res = await execute_command("squeue -o \"%j,%T\" -h -p {}".
+                                        format(ctx.cluster),
+                                        cwd=ctx.dir)
+            output = res.stdout.decode()
+        except Exception as e:
+            logging.warning("Could not retrieve list: {}".format(e))
+        else:
+            jobs = []
+            for line in output.split():
+                fields = line.strip().split(",")
+                jobs.append({"name": fields[0], "job_state": fields[1]})
+
+            filtered_jobs = [{"name": j['name'], "state": j["job_state"]}
+                         for j in jobs
+                         if j['name'].startswith(match)
+                         and j['job_state'] in START_STATES]
+
+            logging.debug("SLURM JOBS result: {}".
+                          format(pformat(filtered_jobs)))
+
+    return filtered_jobs
+
+
+async def submit_job(ctx, script=None):
+    r_sbatch_id = re.compile(r'Submitted batch job (\d+)$')
+    args = Arguments()
+
+    # Don't smash the scheduler immediately, it appears to have the potential
+    # to cause problems. 
+    sleep_for = random.randint(0, args.max_stagger)
+    logging.debug("Sleeping for {} seconds before submission".format(sleep_for))
+    await asyncio.sleep(sleep_for)
+    res = await execute_command("sbatch --no-requeue {}".format(script),
+                                cwd=ctx.dir)
+    output = res.stdout.decode()
+
+    sbatch_match = r_sbatch_id.match(output)
+    if sbatch_match:
+        job_id = sbatch_match.group(1)
+        logging.info("Submitted job with ID {}".format(job_id))
+
+        return int(job_id)
+    return None
