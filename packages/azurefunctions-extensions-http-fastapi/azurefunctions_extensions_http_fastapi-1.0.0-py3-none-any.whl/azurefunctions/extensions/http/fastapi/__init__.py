63a78d89fdc88de1e480108c4885d316bafa6bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:16 +0000

    Add package azurefunctions-extensions-http-fastapi/1.0.0/azurefunctions_extensions_http_fastapi-1.0.0-py3-none-any.whl

---
+++ packages/azurefunctions-extensions-http-fastapi/azurefunctions_extensions_http_fastapi-1.0.0-py3-none-any.whl/azurefunctions/extensions/http/fastapi/__init__.py
@@ -0,0 +1,31 @@
+from fastapi import Request, Response
+from fastapi.responses import (
+    FileResponse,
+    HTMLResponse,
+    JSONResponse,
+    ORJSONResponse,
+    PlainTextResponse,
+    RedirectResponse,
+    StreamingResponse,
+    UJSONResponse,
+)
+
+from .web import RequestSynchronizer, WebApp, WebServer
+
+__all__ = [
+    "WebServer",
+    "WebApp",
+    "Request",
+    "Response",
+    "RequestSynchronizer",
+    "StreamingResponse",
+    "HTMLResponse",
+    "PlainTextResponse",
+    "RedirectResponse",
+    "JSONResponse",
+    "UJSONResponse",
+    "ORJSONResponse",
+    "FileResponse",
+]
+
+__version__ = '1.0.0'
