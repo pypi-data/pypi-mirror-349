045fef26a5a71dcf1b71f0d0778534379a95fbc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:46 +0000

    Add package solax-py-library/1.0.0.2508/solax_py_library-1.0.0.2508.tar.gz

---
+++ packages/solax-py-library/solax_py_library-1.0.0.2508.tar.gz/solax_py_library-1.0.0.2508/solax_py_library/smart_scene/core/condition/base.py
@@ -0,0 +1,15 @@
+class BaseCondition(object):
+    condition_type = None
+
+    def __init__(self, update_value_function, **kwargs):
+        self.value = {}
+        if not callable(update_value_function):
+            raise ValueError("update_value_function must be callable")
+        self.update_value_function = update_value_function
+
+    def update_value(self):
+        self.update_value_function()
+        print(self.value)
+
+    def meet_func(self, data, ctx):
+        ...
