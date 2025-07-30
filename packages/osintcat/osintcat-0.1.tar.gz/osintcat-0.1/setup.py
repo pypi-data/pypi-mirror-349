c491439b952abf7bd43b537abc5213b56d7c3e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:39 +0000

    Add package osintcat/0.1/osintcat-0.1.tar.gz

---
+++ packages/osintcat/osintcat-0.1.tar.gz/osintcat-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="osintcat",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["requests"],
+    author="flux",
+    description="python package for osintcat.ru",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/stormylol/osintcat",
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/osintcat/osintcat-0.1.tar.gz/osintcat-0.1/tests/test.py
@@ -0,0 +1,4 @@
+from osintcat import osintcat
+
+result = osintcat(cat_id="", module="breach", query="example@gmail.com")
+print(result)
