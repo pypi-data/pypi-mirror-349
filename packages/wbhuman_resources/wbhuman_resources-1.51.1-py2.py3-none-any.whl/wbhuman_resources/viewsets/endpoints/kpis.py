0d5bb4d29a1d92737e7572868abde95527f24b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:35 +0000

    Add package wbhuman_resources/1.51.1/wbhuman_resources-1.51.1-py2.py3-none-any.whl

---
+++ packages/wbhuman_resources/wbhuman_resources-1.51.1-py2.py3-none-any.whl/wbhuman_resources/viewsets/endpoints/kpis.py
@@ -0,0 +1,59 @@
+from rest_framework.reverse import reverse
+from wbcore.metadata.configs.endpoints import EndpointViewConfig
+
+from wbhuman_resources.models import KPI
+
+
+class KPIEndpointConfig(EndpointViewConfig):
+    def get_endpoint(self, **kwargs):
+        return super().get_endpoint()
+
+    def get_instance_endpoint(self, **kwargs):
+        if self.instance:
+            if not KPI.is_administrator(self.request.user):
+                return None
+        return super().get_instance_endpoint()
+
+    def get_create_endpoint(self, **kwargs):
+        if KPI.is_administrator(self.request.user):
+            return super().get_instance_endpoint()
+        return None
+
+    def get_delete_endpoint(self, **kwargs):
+        if KPI.is_administrator(self.request.user):
+            return super().get_delete_endpoint()
+        return None
+
+
+class KPIEvaluationEndpointConfig(EndpointViewConfig):
+    def get_endpoint(self, **kwargs):
+        return reverse(
+            "wbhuman_resources:kpi-evaluation-list",
+            args=[self.view.kwargs["kpi_id"]],
+            request=self.request,
+        )
+
+    def get_instance_endpoint(self, **kwargs):
+        if self.instance:
+            return None
+        return super().get_instance_endpoint()
+
+    def get_create_endpoint(self, **kwargs):
+        return None
+
+    def get_delete_endpoint(self, **kwargs):
+        return None
+
+
+class EvaluationGraphEndpointConfig(EndpointViewConfig):
+    def get_list_endpoint(self, **kwargs):
+        return reverse(
+            "wbhuman_resources:kpi-evaluationgraph-list",
+            args=[self.view.kwargs["kpi_id"]],
+            request=self.request,
+        )
+
+
+class KPIEvaluationPandasEndpointConfig(EndpointViewConfig):
+    def get_list_endpoint(self, **kwargs):
+        return reverse("wbhuman_resources:kpievaluationpandas-list", [], request=self.request)
