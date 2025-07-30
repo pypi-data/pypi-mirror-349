7b2b34d3d64d48cfac56fb399c66c55b72e45722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:35 +0000

    Add package universal-mcp-hubspot/0.1.6/universal_mcp_hubspot-0.1.6-py3-none-any.whl

---
+++ packages/universal-mcp-hubspot/universal_mcp_hubspot-0.1.6-py3-none-any.whl/universal_mcp_hubspot/app.py
@@ -0,0 +1,19 @@
+from typing import Any
+from universal_mcp.applications import APIApplication
+from universal_mcp.integrations import Integration
+from .api_segments.crm_api import CrmApi
+from .api_segments.marketing_api import MarketingApi
+
+class HubspotApp(APIApplication):
+
+    def __init__(self, integration: Integration=None, **kwargs) -> None:
+        super().__init__(name='hubspot', integration=integration, **kwargs)
+        self.base_url = 'https://api.hubapi.com'
+        self.crm = CrmApi(self)
+        self.marketing = MarketingApi(self)
+
+    def list_tools(self):
+        all_tools = []
+        all_tools.extend(self.crm.list_tools())
+        all_tools.extend(self.marketing.list_tools())
+        return all_tools
