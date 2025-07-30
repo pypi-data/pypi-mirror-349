9bdba01b74c60f5b3ffd48f825abd4a89786df9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:16:04 +0000

    Add package polars/1.30.0/polars-1.30.0.tar.gz

---
+++ packages/polars/polars-1.30.0.tar.gz/polars-1.30.0/crates/polars-plan/src/dsl/functions/business.rs
@@ -0,0 +1,20 @@
+use super::*;
+
+#[cfg(feature = "dtype-date")]
+pub fn business_day_count(
+    start: Expr,
+    end: Expr,
+    week_mask: [bool; 7],
+    holidays: Vec<i32>,
+) -> Expr {
+    let input = vec![start, end];
+
+    Expr::Function {
+        input,
+        function: FunctionExpr::Business(BusinessFunction::BusinessDayCount {
+            week_mask,
+            holidays,
+        }),
+        options: FunctionOptions::elementwise().with_flags(|f| f | FunctionFlags::ALLOW_RENAME),
+    }
+}
