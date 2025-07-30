e5490e3585332f5369876211277486a52fcf39a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:54 +0000

    Add package wks/0.4.3/wks-0.4.3-py3-none-any.whl

---
+++ packages/wks/wks-0.4.3-py3-none-any.whl/workspace/command_build.py
@@ -0,0 +1,33 @@
+import subprocess
+import os
+from workspace import env, git, message, cmake
+
+name = "build"
+help = """Runs the build"""
+usage = ""
+min_args = 0
+
+
+def run(args):
+    result = 1
+    message.bright("* Building")
+
+    if not os.path.exists("build"):
+        os.makedirs("build")
+
+    if subprocess.getoutput("which ninja") != "":
+        cmd = "cd build; cmake -G Ninja ..; ninja %s" % " ".join(args)
+        result = os.system(cmd)
+    else:
+        print(message.warn("Ninja was not found, you can consider: apt-get install ninja-build"))
+        if input("Continue with Makefiles? (y/n) ") == "y":
+            cmd = "cd build; cmake ..; make %s" % " ".join(args)
+            result = os.system(cmd)
+
+    if result != 0:
+        message.bright(message.error("\n! Errors while building, read log for more details\n"))
+        exit(1)
+    else:
+        message.bright(message.success("\nBuild was successful\n"))
+
+    
