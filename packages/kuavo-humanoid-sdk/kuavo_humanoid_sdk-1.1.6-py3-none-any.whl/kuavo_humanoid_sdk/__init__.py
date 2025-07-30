9b9b861707d2b743a6693b0de7176e751b696754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:37 +0000

    Add package kuavo-humanoid-sdk/1.1.6/kuavo_humanoid_sdk-1.1.6-py3-none-any.whl

---
+++ packages/kuavo-humanoid-sdk/kuavo_humanoid_sdk-1.1.6-py3-none-any.whl/kuavo_humanoid_sdk/__init__.py
@@ -0,0 +1,4 @@
+from .msg import *
+from .interfaces import *
+from .kuavo import *
+from kuavo_humanoid_sdk.common.logger import SDKLogger, disable_sdk_logging
---
+++ packages/kuavo-humanoid-sdk/kuavo_humanoid_sdk-1.1.6-py3-none-any.whl/kuavo_humanoid_sdk/msg/motion_capture_ik/srv/__init__.py
@@ -0,0 +1,4 @@
+from ._changeArmCtrlMode import *
+from ._changeArmCtrlModeKuavo import *
+from ._fkSrv import *
+from ._twoArmHandPoseCmdSrv import *
