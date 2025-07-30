5f796e230ded55ec8e3913b7e8309f912ccf0674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:13:33 +0000

    Add package tables-io/1.0.0/tables_io-1.0.0-py3-none-any.whl

---
+++ packages/tables-io/tables_io-1.0.0-py3-none-any.whl/tables_io/cli.py
@@ -0,0 +1,64 @@
+"""cli for tables_io.convert"""
+
+from functools import partial
+from typing import Any
+
+import click
+
+from . import types
+import tables_io
+
+
+class PartialOption:
+    """Wraps click.option with partial arguments for convenient reuse"""
+
+    def __init__(self, *param_decls: Any, **kwargs: Any) -> None:
+        self._partial = partial(
+            click.option, *param_decls, cls=partial(click.Option), **kwargs
+        )
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        return self._partial(*args, **kwargs)
+
+
+input = PartialOption(
+    "--input",
+    type=click.Path(),
+    help=f"input filename; suffix should be one of {list(types.FILE_FORMAT_SUFFIXS.keys())}",
+)
+
+output = PartialOption(
+    "--output",
+    type=click.Path(),
+    help=f"output filename; suffix should be one of {list(types.FILE_FORMAT_SUFFIXS.keys())}",
+)
+
+
+@click.group()
+@click.version_option(tables_io._version)
+def cli() -> None:
+    """tables_io utility scripts"""
+
+
+@cli.command()
+@input()
+@output()
+def convert(input, output):
+    """Convert a file with tabular data from one format to another"""
+
+    input_fname = input
+    output_fname = output
+    output_format = output_fname.split(".")[-1]
+
+    print(f"Converting {input_fname} to {output_fname}")
+
+    suffixes = types.FILE_FORMAT_SUFFIXS
+    suffix = suffixes[output_format]
+
+    t_in = tables_io.read(input_fname)
+    t_out = tables_io.convert(t_in, suffix)
+    _written = tables_io.write(t_out, output_fname)
+
+    print("Done converting file")
+
+    return 0
