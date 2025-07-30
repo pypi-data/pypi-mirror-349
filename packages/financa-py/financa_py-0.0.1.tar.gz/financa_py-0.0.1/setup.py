45865ef9ed3ea30b1864116b620dc8bc71040317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:57 +0000

    Add package financa-py/0.0.1/financa_py-0.0.1.tar.gz

---
+++ packages/financa-py/financa_py-0.0.1.tar.gz/financa_py-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+
+setup(name='financa_py',
+    version='0.0.1',
+    license='MIT License',
+    author='Douglas Faria',
+    long_description='readme',
+    long_description_content_type="text/markdown",
+    author_email='douglasfariasil14@gmail.com',
+    keywords='financa_py',
+    description=u'este é meu primeiro projeto no pypi',
+    packages=['financa_py'],
+    )
