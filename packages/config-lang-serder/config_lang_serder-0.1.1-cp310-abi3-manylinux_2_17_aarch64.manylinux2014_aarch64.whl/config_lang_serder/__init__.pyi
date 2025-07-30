cb991fdca939dcb5343c9b5ee12f7ece204af626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:43 +0000

    Add package config-lang-serder/0.1.1/config_lang_serder-0.1.1-cp310-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/config-lang-serder/config_lang_serder-0.1.1-cp310-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/config_lang_serder/__init__.pyi
@@ -0,0 +1,16 @@
+from typing import Dict
+
+def read(path: str) -> Dict:
+    """
+    A General method to read a file and return its contents as a Python dict based on the file extension.
+    :param path: the path to the file
+    :return: a python dict
+    """
+
+def write(dict: Dict, path: str) -> None:
+    """
+    A General method to write a Python dict to a file based on the file extension
+    :param dict: a python dict
+    :param path: the path to the file
+    :return: None
+    """
