d3caf39ac13e3dc260ad2cb2661513f3b8d6972c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:25 +0000

    Add package mtmai/0.7.24/mtmai-0.7.24.tar.gz

---
+++ packages/mtmai/mtmai-0.7.24.tar.gz/mtmai-0.7.24/mtmai/context/worker_context.py
@@ -0,0 +1,29 @@
+from mtmai.worker.dispatcher.dispatcher import DispatcherClient
+
+
+class WorkerContext:
+    _worker_id: str = None
+    _registered_workflow_names: list[str] = []
+    _labels: dict[str, str | int] = {}
+
+    def __init__(
+        self,
+        labels: dict[str, str | int],
+        client: DispatcherClient,
+    ):
+        self._labels = labels
+        self.client = client
+
+    def labels(self):
+        return self._labels
+
+    def upsert_labels(self, labels: dict[str, str | int]):
+        self.client.upsert_worker_labels(self._worker_id, labels)
+        self._labels.update(labels)
+
+    async def async_upsert_labels(self, labels: dict[str, str | int]):
+        await self.client.async_upsert_worker_labels(self._worker_id, labels)
+        self._labels.update(labels)
+
+    def id(self):
+        return self._worker_id
