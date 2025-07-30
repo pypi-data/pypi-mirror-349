a5475cb8555f22455096069d7b2edc31d6f260be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:31 +0000

    Add package utlib/0.3.2/utlib-0.3.2.tar.gz

---
+++ packages/utlib/utlib-0.3.2.tar.gz/utlib-0.3.2/utlib/__init__.py
@@ -0,0 +1,11 @@
+from .numbers_utils import digit_sum
+from .numbers_utils import average
+from .strings_utils import is_palindrome
+from .strings_utils import vowels
+from .strings_utils import remove_vowels
+from .strings_utils import count_words
+from .files_utils import get_size
+
+__version__ = '0.1.0'
+__all__ = ['digit_sum', 'average',
+           'is_palindrome', 'vowels', 'remove_vowels', 'count_words', 'get_size']
