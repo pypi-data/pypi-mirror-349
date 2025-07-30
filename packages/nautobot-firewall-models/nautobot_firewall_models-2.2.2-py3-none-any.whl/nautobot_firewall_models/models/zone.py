f676883a4987557df2c100e04d32338f2963428d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:24 +0000

    Add package nautobot-firewall-models/2.2.2/nautobot_firewall_models-2.2.2-py3-none-any.whl

---
+++ packages/nautobot-firewall-models/nautobot_firewall_models-2.2.2-py3-none-any.whl/nautobot_firewall_models/models/zone.py
@@ -0,0 +1,50 @@
+"""Models for the Firewall app."""
+# pylint: disable=duplicate-code
+
+from django.db import models
+from nautobot.core.models.generics import PrimaryModel
+from nautobot.extras.models import StatusField
+from nautobot.extras.utils import extras_features
+
+from nautobot_firewall_models.utils import get_default_status
+
+###########################
+# Core Models
+###########################
+
+
+@extras_features(
+    "custom_fields",
+    "custom_links",
+    "custom_validators",
+    "export_templates",
+    "graphql",
+    "relationships",
+    "statuses",
+    "webhooks",
+)
+class Zone(PrimaryModel):
+    """Zones common on firewalls and are typically seen as representations of area (e.g. DMZ trust untrust)."""
+
+    description = models.CharField(
+        max_length=200,
+        blank=True,
+    )
+    name = models.CharField(max_length=100, unique=True, help_text="Name of the zone (e.g. trust)")
+    vrfs = models.ManyToManyField(to="ipam.VRF", blank=True, related_name="zones")
+    interfaces = models.ManyToManyField(to="dcim.Interface", blank=True, related_name="zones")
+    status = StatusField(
+        on_delete=models.PROTECT,
+        related_name="%(app_label)s_%(class)s_related",  # e.g. dcim_device_related
+        default=get_default_status,
+    )
+
+    class Meta:
+        """Meta class."""
+
+        ordering = ["name"]
+        verbose_name_plural = "Zones"
+
+    def __str__(self):
+        """Stringify instance."""
+        return self.name
