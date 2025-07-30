8ae678647249ffd2d1e95e5f7088ce7b4b6fd49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:24 +0000

    Add package flexkit/0.1.1/flexkit-0.1.1-py3-none-any.whl

---
+++ packages/flexkit/flexkit-0.1.1-py3-none-any.whl/flexkit/tools/__init__.py
@@ -0,0 +1,5 @@
+from .get_favicon_url import get_favicon_url, test_get_favicon_url
+from .download_file import download_file, test_download_file
+
+
+__all__ = ["get_favicon_url", "test_get_favicon_url", "download_file", "test_download_file"]
