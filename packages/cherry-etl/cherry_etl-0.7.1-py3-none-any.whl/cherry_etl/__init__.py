16adc26bc4a14c1da5e2d7a7d49145a9b38428ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:19 +0000

    Add package cherry-etl/0.7.1/cherry_etl-0.7.1-py3-none-any.whl

---
+++ packages/cherry-etl/cherry_etl-0.7.1-py3-none-any.whl/cherry_etl/__init__.py
@@ -0,0 +1,4 @@
+from . import config, datasets, utils
+from .pipeline import run_pipeline
+
+__all__ = ["config", "run_pipeline", "datasets", "utils"]
---
+++ packages/cherry-etl/cherry_etl-0.7.1-py3-none-any.whl/cherry_etl/datasets/svm/__init__.py
@@ -0,0 +1,4 @@
+from .instructions import make_pipeline as make_instructions_pipeline
+from .instructions_and_logs import make_pipeline as make_instructions_and_logs_pipeline
+
+__all__ = ["make_instructions_pipeline", "make_instructions_and_logs_pipeline"]
