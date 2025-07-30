5ede5cc7dc6610fec7d3c98a654e6075874a7da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:15 +0000

    Add package supermag-api/0.0.3/supermag_api-0.0.3.tar.gz

---
+++ packages/supermag-api/supermag_api-0.0.3.tar.gz/supermag_api-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='supermag_api',
+    version='0.0.3',
+    description='Python software to fetch SuperMag data',
+    author='Sandy Antunes',
+    author_email='sandy.antunes@jhuapl.edu',
+    url='https://github.com/elwinter/supermag_api',
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.8',
+    install_requires=[
+        'certifi',
+        'pandas',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD 3-Clause',
+        'Programming Language :: Python :: 3',
+    ],
+)
