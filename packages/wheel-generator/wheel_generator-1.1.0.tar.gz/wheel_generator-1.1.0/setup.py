45edfa3a7862bf497b077de7db6eec3531474069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:15 +0000

    Add package wheel-generator/1.1.0/wheel_generator-1.1.0.tar.gz

---
+++ packages/wheel-generator/wheel_generator-1.1.0.tar.gz/wheel_generator-1.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wheel_generator',
+    version='1.1.0',
+    packages=find_packages(),
+    description='Blender addon to generate wheels and gears',
+    author='Your Name',
+    author_email='youremail@example.com',
+    url='https://github.com/A-D-H-I/wheel_generator',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
